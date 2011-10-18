45693e1837c947904ed3f9f3a59b10b1e6bf1de2
Ali Kaafarani <kaafarani.ali@gmail.com>
2011-10-18 13:42:59 +0200

    Use norwegian names in URL routes

--- project/urls.py
+++ project/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^$', 'home.views.index'),
-    url(r'^home/', include('home.urls')),
-    url(r'^training/', include('training.urls')),
-    url(r'^member/', include('member.urls')),
-    url(r'^about/', include('about.urls')),
+    url(r'^hjem/', include('home.urls')),
+    url(r'^kurs/', include('training.urls')),
+    url(r'^medlem/', include('member.urls')),
+    url(r'^om/', include('about.urls')),
 )
