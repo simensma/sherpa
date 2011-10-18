41dda283dd976775065589411bbf170b5d36494c
Ali Kaafarani <kaafarani.ali@gmail.com>
2011-10-18 11:06:29 +0200

    Specify urls module relatively

--- settings.py
+++ settings.py
@@ -86,7 +86,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'sherpa3.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
