6237b2599896df48232063810a11226aa6590670
Ali Kaafarani <kaafarani.ali@gmail.com>
2011-10-18 09:59:10 +0200

    Add initial apps

--- urls.py
+++ urls.py
@@ -1,17 +1,8 @@
 from django.conf.urls.defaults import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
-
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'sherpa3.views.home', name='home'),
-    # url(r'^sherpa3/', include('sherpa3.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'main.views.index'),
+    url(r'^training/', include('training.urls')),
+    url(r'^member/', include('member.urls')),
+    url(r'^about/', include('about.urls')),
 )
