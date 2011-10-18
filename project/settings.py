746a5049ecc5142f182039cb9a935a5fa82d547c
Ali Kaafarani <kaafarani.ali@gmail.com>
2011-10-18 13:37:34 +0200

    Add preliminary templates

--- project/settings.py
+++ project/settings.py
@@ -89,9 +89,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'urls'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    "/home/murray/jobb/dnt/web/sherpa3/templates"
 )
 
 INSTALLED_APPS = (
