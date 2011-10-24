faad356181312fb528d3988639a4c27aa58c7e1b
Ali Kaafarani <kaafarani.ali@gmail.com>
2011-10-24 12:18:01 +0200

    Include all home urls

--- project/urls.py
+++ project/urls.py
@@ -1,8 +1,7 @@
 from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('',
-    url(r'^$', 'home.views.index'),
-    url(r'^hjem/', include('home.urls')),
+    url(r'^$', include('home.urls')),
     url(r'^kurs/', include('training.urls')),
     url(r'^medlem/', include('member.urls')),
     url(r'^om/', include('about.urls')),
