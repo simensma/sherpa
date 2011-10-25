b4870d45ecaa377db724a75e89180ba22190b998
Ali Kaafarani <kaafarani.ali@gmail.com>
2011-10-25 09:18:21 +0200

    Move page-specific functions into separate file

--- project/admin/urls.py
+++ project/admin/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('admin.views',
     url(r'^$', 'index'),
+
     url(r'^side/$', 'page_list'),
     url(r'^side/rediger/(?P<page>\d*)/$', 'page_edit'),
     url(r'^side/slett/(?P<page>\d*)/$', 'page_delete'),
---
+++ project/admin/views_page.py
@@ -0,0 +1,48 @@
+from django.core.urlresolvers import reverse
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.http import HttpResponseRedirect
+from page.models import Page
+
+def page_list(request, error=None):
+    pages = Page.objects.all()
+    context = {'pages': pages, 'error': error}
+    return render_to_response('admin/page/list.html', context, context_instance=RequestContext(request))
+
+def page_edit(request, page):
+    if(request.method == 'GET'):
+        try:
+            page = Page.objects.get(pk=page)
+            context = {'page': page}
+            return render_to_response('admin/page/edit.html', context,
+              context_instance=RequestContext(request))
+        except (KeyError, Page.DoesNotExist):
+            return page_list(request, error="This page does not exist.")
+    elif(request.method == 'POST'):
+        try:
+            page = Page.objects.get(pk=page)
+            page.content = request.POST['content']
+            page.slug = request.POST['slug']
+            page.save()
+            return HttpResponseRedirect(reverse('admin.views.page_list'))
+        except (KeyError, Page.DoesNotExist):
+            page = Page(content=request.POST['content'], slug=request.POST['slug'])
+            context = {'page': page, 'error': "Whoops, looks like you tried to edit a non-existing thing."}
+            return render_to_response('admin/page/edit.html', context,
+              context_instance=RequestContext(request))
+
+def page_new(request):
+    if(request.method == 'GET'):
+        return render_to_response('admin/page/new.html', context_instance=RequestContext(request))
+    elif(request.method == 'POST'):
+        page = Page(content=request.POST['content'], slug=request.POST['slug'])
+        page.save()
+        return HttpResponseRedirect(reverse('admin.views.page_list'))
+
+def page_delete(request, page):
+    try:
+        page = Page.objects.get(pk=page)
+        page.delete()
+        return HttpResponseRedirect(reverse('admin.views.page_list'))
+    except (KeyError, Page.DoesNotExist):
+        return page_list(request, error="The page you tried to delete does not exist.")
