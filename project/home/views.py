320aa8bd7c03a5dd2f7e1a8d560fcd17c06c7e44
Ali Kaafarani <kaafarani.ali@gmail.com>
2011-10-26 11:21:56 +0200

    Allow no slug for frontpage

--- project/home/views.py
+++ project/home/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
+from page.models import Page
 
 def index(request):
-    return render_to_response('home.html', context_instance=RequestContext(request))
+    page = get_object_or_404(Page, slug="")
+    context = {'page': page}
+    return render_to_response('page/page.html', context, context_instance=RequestContext(request))
