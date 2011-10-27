fc8da2cb0fd809ac7064f0205fe1cd3fb7dd75ec
Ali Kaafarani <kaafarani.ali@gmail.com>
2011-10-27 16:24:15 +0200

    Add middleware for analytics processing

---
+++ project/middleware.py
@@ -0,0 +1,4 @@
+class Analytics():
+    def process_request(self, request):
+        # Save pageview, events etc
+        return None
--- project/settings.py
+++ project/settings.py
@@ -85,6 +85,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'middleware.Analytics',
 )
 
 ROOT_URLCONF = 'urls'
