af98eced2283bbccb2e1ad8bb881dab8c1b9f5f5
Ali Kaafarani <kaafarani.ali@gmail.com>
2011-10-27 16:24:56 +0200

    Add Person model that references User

--- project/home/models.py
+++ project/home/models.py
@@ -1,6 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Menu(models.Model):
     name = models.CharField(max_length=50)
     page = models.ForeignKey('page.Page')
     position = models.IntegerField()
+
+class Person(models.Model):
+    user = models.ForeignKey(User)
+    # Much more person data
