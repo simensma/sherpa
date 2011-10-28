4754bcf2894706f90cb329d93d706642de23b9ce
Ali Kaafarani <kaafarani.ali@gmail.com>
2011-10-28 14:56:05 +0200

    Forward frontpage request to page view

--- project/home/views.py
+++ project/home/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from page.models import Page
+from page.views import page
 
 def index(request):
-    page = get_object_or_404(Page, slug="")
-    context = {'page': page}
-    return render_to_response('page/page.html', context, context_instance=RequestContext(request))
+    return page(request=request, slug="")
