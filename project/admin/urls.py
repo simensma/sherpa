c381af4df6d0399f6a27e8e44055bc52bb448654
Ali Kaafarani <kaafarani.ali@gmail.com>
2011-10-31 14:23:53 +0100

    Add descriptive comments

--- project/admin/urls.py
+++ project/admin/urls.py
@@ -3,11 +3,13 @@ from django.conf.urls.defaults import patterns, include, url
 urlpatterns = patterns('admin.views',
     url(r'^$', 'index'),
 
+    # Sider
     url(r'^side/$', 'page_list'),
     url(r'^side/ny/$', 'page_new'),
     url(r'^side/rediger/(?P<page>\d*)/$', 'page_edit'),
     url(r'^side/slett/(?P<page>\d*)/$', 'page_delete'),
 
+    # Menyer
     url(r'^meny/$', 'menu_list'),
     url(r'^meny/rediger/$', 'menu_edit'),
 )
