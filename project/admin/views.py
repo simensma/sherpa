c5a13864734f59f7a307d7777b1b8b0ee87c84c1
Ali Kaafarani <kaafarani.ali@gmail.com>
2011-11-02 15:58:57 +0100

    Rename variant view to page_variant

--- project/admin/views.py
+++ project/admin/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from admin.views_page import *
 from admin.views_menu import *
 from admin.views_page_version import *
-from admin.views_variant import *
+from admin.views_page_variant import *
 
 def index(request):
     return render(request, 'admin/admin.html')
