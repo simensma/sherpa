66a84743496ab23698fb34c6f8f64ecb9d90c245
Ali Kaafarani <kaafarani.ali@gmail.com>
2011-11-03 09:02:55 +0100

    Base menus on pages, not page versions

--- project/admin/views_menu.py
+++ project/admin/views_menu.py
@@ -2,11 +2,11 @@ from django.core.urlresolvers import reverse
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from home.models import Menu
-from page.models import PageVersion
+from page.models import Page
 
 def menu_list(request, error=None):
-    menupages = PageVersion.objects.filter(menu__isnull=False).filter(active=True)
-    otherpages = PageVersion.objects.filter(menu__isnull=True).filter(active=True)
+    menupages = Page.objects.filter(menu__isnull=False).filter(active=True)
+    otherpages = Page.objects.filter(menu__isnull=True).filter(active=True)
     context = {'menupages': menupages, 'otherpages': otherpages, 'error': error}
     return render(request, 'admin/menu/list.html', context)
 
@@ -16,7 +16,7 @@ def menu_edit(request):
         for id in request.POST:
             # csrfmiddlewaretoken is the only unrelated post field
             if(id != 'csrfmiddlewaretoken'):
-                version = PageVersion.objects.get(pk=id)
+                version = Page.objects.get(pk=id)
                 # Save with the name of the version content now, but this should be manually entered
                 m = Menu(name=version.content.content, version=version, position=1)
                 m.save()
--- project/home/models.py
+++ project/home/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 class Menu(models.Model):
     name = models.CharField(max_length=50)
-    version = models.ForeignKey('page.PageVersion')
+    page = models.ForeignKey('page.Page')
     position = models.IntegerField()
