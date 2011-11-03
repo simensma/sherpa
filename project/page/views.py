53a85cfd2e1d7483df561895bea44c850557e220
Ali Kaafarani <kaafarani.ali@gmail.com>
2011-11-03 18:15:35 +0100

    Handle variants for frontpage

--- project/page/views.py
+++ project/page/views.py
@@ -16,18 +16,19 @@ def page(request, slug):
     page = Page.objects.get(slug=slug)
     version = PageVersion.objects.filter(page=page).get(active=True)
     try:
-        variant = PageVariant.objects.get(version=version)
-        return HttpResponseRedirect(reverse('page.views.page', args=[slug]) + "?" + variantParameter + "=" + variant.slug)
+        variants = PageVariant.objects.filter(version=version)
+        # Logic for when several segments match
+        variant = variants[0]
+        if(slug == ""):
+            # Special case for when the frontpage has variants
+            return HttpResponseRedirect(reverse('home.views.index') + "?" + variantParameter + "=" + variant.slug)
+        else:
+            return HttpResponseRedirect(reverse('page.views.page', args=[slug]) + "?" + variantParameter + "=" + variant.slug)
     except (KeyError, PageVariant.DoesNotExist):
         context = {'version': version, 'content': version.content}
         return render(request, 'page/page.html', context)
 
 def render_variant(request, pageslug, variantslug):
-    variants = PageVariant.objects.filter(slug=variantslug).order_by('priority')
-    context = {}
-    for variant in variants:
-        if(True): # If segment matches
-            context = {'version': variant.pageVersion, 'content': variant.content}
-            return render(request, 'page/page.html', context)
-        # Logic for when several segments match
-    # Error
+    variant = PageVariant.objects.filter(version__page__slug=pageslug).get(slug=variantslug)
+    context = {'version': variant.version, 'content': variant.content}
+    return render(request, 'page/page.html', context)
