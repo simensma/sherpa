6431376222820f7033a5cdd44240f47b7803e911
Ali Kaafarani <kaafarani.ali@gmail.com>
2011-11-03 09:44:53 +0100

    Display all SQL queries during development

--- project/context_processors.py
+++ project/context_processors.py
@@ -1,4 +1,8 @@
 from home.models import Menu
+from django.db import connection
 
 def main_menu(request):
     return {'main_menu': Menu.objects.all()}
+
+def sql_queries(request):
+    return {'sql_queries': connection.queries}
--- project/settings.py
+++ project/settings.py
@@ -144,7 +144,8 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "django.contrib.messages.context_processors.messages",
 
     # custom processors
-    "context_processors.main_menu"
+    "context_processors.main_menu",
+    "context_processors.sql_queries"
 )
 
 AUTH_PROFILE_MODULE='auth.Profile'
