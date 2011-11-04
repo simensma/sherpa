fc9382d52c019edbd18e77830aa09be5ee0831f1
Ali Kaafarani <kaafarani.ali@gmail.com>
2011-11-04 16:18:42 +0100

    Don't process requests to static files

--- project/middleware.py
+++ project/middleware.py
@@ -2,6 +2,11 @@ from analytics.models import Visitor
 
 class Analytics():
     def process_request(self, request):
+        # Don't process requests to static files
+        statics = ['favicon.ico', 'robots.txt']
+        if request.get_full_path()[1:] in statics:
+            return None
+
         # If this is a new user, create a new Visitor
         # Todo: Logic around auth
         if not 'visitor' in request.session:
