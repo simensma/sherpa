66bd548ab4fb3fe8cdd9eb4c953999791bd2360f
Ali Kaafarani <kaafarani.ali@gmail.com>
2011-11-04 13:56:40 +0100

    Create a new Visitor upon request

--- project/middleware.py
+++ project/middleware.py
@@ -1,4 +1,13 @@
+from analytics.models import Visitor
+
 class Analytics():
     def process_request(self, request):
+        # If this is a new user, create a new Visitor
+        # Todo: Logic around auth
+        if not 'visitor' in request.session:
+            v = Visitor()
+            v.save()
+            request.session['visitor'] = v.id
+
         # Save pageview, events etc
         return None
