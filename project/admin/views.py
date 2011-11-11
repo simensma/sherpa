ac370d82252c3e07bbe8e1c6e88dbc28a0fc4906
Ali Kaafarani <kaafarani.ali@gmail.com>
2011-11-11 14:39:42 +0100

    Remove separate version view file

--- project/admin/views.py
+++ project/admin/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render
 from admin.views_page import *
 from admin.views_menu import *
 from admin.views_page_variant import *
-#from admin.views_page_version import *
 
 def index(request):
     return render(request, 'admin/admin.html')
