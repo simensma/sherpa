0ffd3f8b0b761441b43875dce9f9ec38d2a19eda
Ali Kaafarani <kaafarani.ali@gmail.com>
2011-11-28 10:25:18 +0100

    Remove unnecessary dropdown functionality

--- static/js/admin.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  $("form#new_variant_form").animate({height: "toggle"}, 0);
-});
-
-$(function() {
-  $("a#new_variant").click(function(event) {
-    $("form#new_variant_form").animate({height: "toggle"}, 400);
-  });
-});
