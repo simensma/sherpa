26583412cd3f1f9efd35958d77b3601aa96034ee
Ali Kaafarani <kaafarani.ali@gmail.com>
2011-12-06 16:19:33 +0100

    Sort layout items by order

--- project/page/views_widgets.py
+++ project/page/views_widgets.py
@@ -8,20 +8,25 @@ def parse_content(request, version):
     max_columns = 3 # The highest number of columns we'll have in a layout
     # Potential optimization: Use a manager to perform a single query with joins
     layouts = Layout.objects.filter(version=version).order_by('order')
-    # TODO sort contents and widgets
     for layout in layouts:
         del layout.columns[:]
         layout.columns = []
         for i in range(max_columns):
             layout.columns.append([])
-        contents = HTMLContent.objects.filter(layout=layout).order_by('order')
-        for content in contents:
-            layout.columns[content.column].append({'type': 'html', 'content': content.content})
-        widgets = Widget.objects.filter(layout=layout).order_by('order')
-        for widget_object in widgets:
-            widget = json.loads(widget_object.widget)
-            layout.columns[widget_object.column].append({'type': 'widget', 'content':
-              parse_widget(widget)})
+        # Fetch all items and sort them afterwards
+        contents = HTMLContent.objects.filter(layout=layout)
+        widgets = Widget.objects.filter(layout=layout)
+        list = []
+        list.extend(contents)
+        list.extend(widgets)
+        list.sort(key=lambda item: item.order)
+        for item in list:
+            if isinstance(item, HTMLContent):
+                layout.columns[item.column].append({'type': 'html', 'content': item.content})
+            elif isinstance(item, Widget):
+                widget = json.loads(item.widget)
+                layout.columns[item.column].append({'type': 'widget', 'content':
+                  parse_widget(widget)})
     context = {'layouts': layouts}
     return render(request, "page/page.html", context)
 
