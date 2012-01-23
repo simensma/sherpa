5dcb20faa6ed4263806440ba4139883162a797a4
Ali Kaafarani <ali@kvikshaug.no>
2012-01-23 15:26:58 +0100

    Order imports alphabetically

--- project/admin/views.py
+++ project/admin/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 
-from admin.views_page import *
-from admin.views_menu import *
-from admin.views_page_variant import *
 from admin.views_analytics import *
+from admin.views_menu import *
+from admin.views_page import *
+from admin.views_page_variant import *
 
 def index(request):
     return render(request, 'admin/admin.html')
