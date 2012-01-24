49a8004d5fa1cca1c4d548cd0b5d54c31d300139
Ali Kaafarani <ali@kvikshaug.no>
2012-01-24 08:59:58 +0100

    Use consistent commenting in urls file

--- project/admin/urls.py
+++ project/admin/urls.py
@@ -3,19 +3,19 @@ from django.conf.urls.defaults import patterns, include, url
 urlpatterns = patterns('admin.views',
     url(r'^$', 'index'),
 
-    # Sider
+    # Pages
     url(r'^artikkel/$', 'page_list'),
     url(r'^artikkel/ny/$', 'page_new'),
     url(r'^artikkel/rediger/(?P<page>\d+)/$', 'page_edit'),
     url(r'^artikkel/slett/(?P<page>\d+)/$', 'page_delete'),
 
-    # Varianter
+    # Variants
     url(r'^artikkel/variant/ny/(?P<page>\d+)/$', 'variant_new'),
     url(r'^artikkel/variant/rediger/(?P<version>\d+)/$', 'variant_edit'),
     url(r'^artikkel/variant/bytt/(?P<page>\d+)/(?P<pri1>\d+)/(?P<pri2>\d+)/$', 'variant_swap'),
     #url(r'^artikkel/variant/slett/(?P<variant>\d*)/$', 'variant_delete'),
 
-    # Versjoner
+    # Versions
     url(r'^artikkel/versjon/ny/(?P<variant>\d+)/$', 'version_new'),
     url(r'^artikkel/versjon/rediger/(?P<version>\d+)/$', 'version_edit'),
     url(r'^artikkel/versjon/aktiver/(?P<version>\d+)/$', 'version_activate'),
@@ -24,18 +24,16 @@ urlpatterns = patterns('admin.views',
     url(r'^artikkel/versjon/layout/flytt-opp/(?P<layout>\d+)/$', 'layout_move_up'),
     url(r'^artikkel/versjon/layout/slett/(?P<layout>\d+)/$', 'layout_delete'),
 
-    # Creating widgets
+    # Widgets
     url(r'^artikkel/widget/opprett/quote/(?P<version>\d+)/$', 'version_add_widget_quote'),
-
-    # Remove widget
     url(r'^artikkel/widget/slett/(?P<widget>\d+)/$', 'widget_delete'),
 
-    # AJAX-updates to content and widgets
+    # Content
     url(r'^artikkel/innhold/opprett/(?P<layout>\d+)/(?P<column>\d+)/(?P<order>\d+)/$', 'content_create'),
     url(r'^artikkel/innhold/oppdater/(?P<content>\d+)/$', 'content_update'),
     url(r'^artikkel/innhold/slett/(?P<content>\d+)/$', 'content_delete'),
 
-    # Menyer
+    # Menus
     url(r'^meny/$', 'menu_list'),
     url(r'^meny/ny/(?P<page>\d+)/$', 'menu_add'),
     url(r'^meny/bytt/(?P<pos1>\d+)/(?P<pos2>\d+)/$', 'menu_swap'),
