8ac0068bebd8d5d4e3680fbe8b973cd8f3edb481
Ali Kaafarani <ali@kvikshaug.no>
2012-01-30 15:03:58 +0100

    Import all cms views

--- project/admin/views.py
+++ project/admin/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 
 from project.admin.analytics.views import *
+from project.admin.cms.views.layout import *
+from project.admin.cms.views.content import *
 from project.admin.cms.views.menu import *
 from project.admin.cms.views.page import *
 from project.admin.cms.views.variant import *
+from project.admin.cms.views.version import *
 from project.admin.cms.views.widgets import *
 
 def index(request):
