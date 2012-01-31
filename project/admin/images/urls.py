fe63f31db06fcee61e9a721b0c07e720abece211
Ali Kaafarani <ali@kvikshaug.no>
2012-01-31 13:53:05 +0100

    List images along with subalbums

--- project/admin/images/urls.py
+++ project/admin/images/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls.defaults import patterns, include, url
 urlpatterns = patterns('admin.images.views',
     url(r'^$', 'dashboard', {'album': None}),
     url(r'^album/(?P<album>\d+)/$', 'dashboard'),
+    url(r'^bilde/(?P<image>\d+)/$', 'image'),
 )
--- project/admin/images/views.py
+++ project/admin/images/views.py
@@ -1,17 +1,22 @@
 #from django.core.urlresolvers import reverse
 #from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from admin.models import Album
+from admin.models import Image, Album
 
 def dashboard(request, album):
     albums = Album.objects.filter(parent=album)
     parents = []
+    images = None
     if album is not None:
+        images = Image.objects.filter(album=album)
         album = Album.objects.get(id=album)
         parents.append(album)
         while(album.parent != None):
             album = Album.objects.get(id=album.parent.id)
             parents.insert(0, album)
 
-    context = {'albums': albums, 'albumpath': parents}
+    context = {'albums': albums, 'albumpath': parents, 'images': images}
     return render(request, 'admin/images/dashboard.html', context)
+
+def image(request, image):
+    return None
