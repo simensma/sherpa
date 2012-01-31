2ad00abc329267f55fdff7d942802ea298e51a0d
Ali Kaafarani <ali@kvikshaug.no>
2012-01-31 10:07:48 +0100

    Remove leftover comment field

--- project/admin/models.py
+++ project/admin/models.py
@@ -4,7 +4,6 @@ class Image(models.Model):
     hash = models.CharField(max_length=40)
     # Todo: Loads of metadata
     album = models.ForeignKey('admin.Album')
-    #page = models.ForeignKey('page.Page', unique=True)
     position = models.IntegerField(unique=True)
 
 class Album(models.Model):
