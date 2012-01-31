16cb217678a775f8bd5c93c8636cac37cbe5c668
Ali Kaafarani <ali@kvikshaug.no>
2012-01-31 10:35:14 +0100

    Add more metadata to image gallery model

--- project/admin/models.py
+++ project/admin/models.py
@@ -2,11 +2,20 @@ from django.db import models
 
 class Image(models.Model):
     hash = models.CharField(max_length=40)
-    # Todo: Loads of metadata
     album = models.ForeignKey('admin.Album')
-    position = models.IntegerField(unique=True)
+    photographer = models.CharField(max_length=200)
+    credits = models.CharField(max_length=20)
+    photographer_contact = models.CharField(max_length=200)
+    uploaded = models.DateTimeField(auto_now_add=True)
+    uploader = models.ForeignKey('users.Profile')
+    height = models.IntegerField()
+    width = models.IntegerField()
 
 class Album(models.Model):
     name = models.CharField(max_length=200)
     parent = models.ForeignKey('admin.Album', null=True)
     # Todo: Author, or some other sort of affiliation?
+
+class Keyword(models.Model):
+    image = models.ForeignKey('admin.Image')
+    name = models.CharField(max_length=200)
