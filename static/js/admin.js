ce9707d5e57eda716926e923ec0b96693bee33a0
Ali Kaafarani <ali@kvikshaug.no>
2012-01-31 17:14:03 +0100

    Add dialog for adding new album

--- static/js/admin.js
+++ static/js/admin.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
+    $("div#add-album").dialog({
+        title: 'Legg til album',
+        modal: true,
+        autoOpen: false,
+        width: "80%"
+    }).hide();
     $("div#archive-gallery li.add.album a").click(function(event) {
-        // add album
+        $("div#add-album").dialog('open');
     });
 
     $("div#archive-gallery li.add.image a").click(function(event) {
