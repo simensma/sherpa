750e3f4d3266f1a55f98a85402c087c3f7332227
Ali Kaafarani <ali@kvikshaug.no>
2012-01-31 12:50:45 +0100

    Display path for current album

--- project/admin/images/views.py
+++ project/admin/images/views.py
@@ -5,5 +5,13 @@ from admin.models import Album
 
 def dashboard(request, album):
     albums = Album.objects.filter(parent=album)
-    context = {'albums': albums, 'parents': parents}
+    parents = []
+    if album is not None:
+        album = Album.objects.get(id=album)
+        parents.append(album)
+        while(album.parent != None):
+            album = Album.objects.get(id=album.parent.id)
+            parents.insert(0, album)
+
+    context = {'albums': albums, 'albumpath': parents}
     return render(request, 'admin/images/dashboard.html', context)
