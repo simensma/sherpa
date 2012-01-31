b02d23c86bf877b0642280b3787ca9b7c03cd320
Ali Kaafarani <ali@kvikshaug.no>
2012-01-31 17:07:50 +0100

    Add js hooks to add album/image clicks

--- static/js/admin.js
+++ static/js/admin.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
+    $("div#archive-gallery li.add.album a").click(function(event) {
+        // add album
+    });
 
+    $("div#archive-gallery li.add.image a").click(function(event) {
+        // add image
+    });
 });
