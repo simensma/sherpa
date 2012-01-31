4ef322d765b29d976d7a7a986b985056fb81a590
Ali Kaafarani <ali@kvikshaug.no>
2012-01-31 15:46:40 +0100

    Display album path for images

--- project/admin/images/views.py
+++ project/admin/images/views.py
@@ -9,16 +9,20 @@ def albums(request, album):
     images = None
     if album is not None:
         images = Image.objects.filter(album=album)
-        album = Album.objects.get(id=album)
-        parents.append(album)
-        while(album.parent != None):
-            album = Album.objects.get(id=album.parent.id)
-            parents.insert(0, album)
-
+        parents = list_parents(Album.objects.get(id=album))
     context = {'albums': albums, 'albumpath': parents, 'images': images}
     return render(request, 'admin/images/albums.html', context)
 
 def image(request, image):
     image = Image.objects.get(id=image)
-    context = {'image': image}
+    parents = list_parents(image.album)
+    context = {'image': image, 'albumpath': parents}
     return render(request, 'admin/images/image.html', context)
+
+def list_parents(album):
+    parents = []
+    parents.append(album)
+    while(album.parent != None):
+        album = Album.objects.get(id=album.parent.id)
+        parents.insert(0, album)
+    return parents
