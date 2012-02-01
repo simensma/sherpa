75262bca6e664fd9eb4d8fac91baaac8fc398d11
Ali Kaafarani <ali@kvikshaug.no>
2012-02-01 17:33:55 +0100

    Add title field to page model

---
+++ project/page/migrations/0002_auto__add_field_page_title.py
@@ -0,0 +1,84 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Adding field 'Page.title'
+        db.add_column('page_page', 'title', self.gf('django.db.models.fields.CharField')(default='Side uten tittel, fra migrering', max_length=200), keep_default=False)
+
+
+    def backwards(self, orm):
+        
+        # Deleting field 'Page.title'
+        db.delete_column('page_page', 'title')
+
+
+    models = {
+        'analytics.segment': {
+            'Meta': {'object_name': 'Segment'},
+            'description': ('django.db.models.fields.TextField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '200'})
+        },
+        'page.block': {
+            'Meta': {'object_name': 'Block'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'order': ('django.db.models.fields.IntegerField', [], {}),
+            'template': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'version': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['page.PageVersion']"})
+        },
+        'page.htmlcontent': {
+            'Meta': {'object_name': 'HTMLContent'},
+            'block': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['page.Block']"}),
+            'column': ('django.db.models.fields.IntegerField', [], {}),
+            'content': ('django.db.models.fields.TextField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'order': ('django.db.models.fields.IntegerField', [], {})
+        },
+        'page.menu': {
+            'Meta': {'object_name': 'Menu'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'page': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['page.Page']", 'unique': 'True'}),
+            'position': ('django.db.models.fields.IntegerField', [], {'unique': 'True'})
+        },
+        'page.page': {
+            'Meta': {'object_name': 'Page'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'pub_date': ('django.db.models.fields.DateTimeField', [], {'null': 'True'}),
+            'published': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'slug': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '50'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '200'})
+        },
+        'page.pagevariant': {
+            'Meta': {'object_name': 'PageVariant'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'page': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['page.Page']"}),
+            'priority': ('django.db.models.fields.IntegerField', [], {}),
+            'segment': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['analytics.Segment']", 'null': 'True'}),
+            'slug': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'page.pageversion': {
+            'Meta': {'object_name': 'PageVersion'},
+            'active': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'variant': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['page.PageVariant']"}),
+            'version': ('django.db.models.fields.IntegerField', [], {})
+        },
+        'page.widget': {
+            'Meta': {'object_name': 'Widget'},
+            'block': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['page.Block']"}),
+            'column': ('django.db.models.fields.IntegerField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'order': ('django.db.models.fields.IntegerField', [], {}),
+            'widget': ('django.db.models.fields.TextField', [], {})
+        }
+    }
+
+    complete_apps = ['page']
--- project/page/models.py
+++ project/page/models.py
@@ -6,6 +6,7 @@ class Menu(models.Model):
     position = models.IntegerField(unique=True)
 
 class Page(models.Model):
+    title = models.CharField(max_length=200)
     slug = models.CharField(max_length=50, unique=True)
     published = models.BooleanField()
     pub_date = models.DateTimeField(null=True)
