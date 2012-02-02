ec8a26bc4c0968a7e8d2e41c86c1653570e049fb
Ali Kaafarani <ali@kvikshaug.no>
2012-02-02 11:28:31 +0100

    Add link for creating new article

--- project/admin/articles/views.py
+++ project/admin/articles/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 def list(request):
     context = {}
     return render(request, 'admin/articles/list.html', context)
+
+def new(request):
+    context = {}
+    return render(request, 'admin/articles/new.html', context)
