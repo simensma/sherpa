059217cb584039b0df74790cf6ee2e0eb772b665
Ali Kaafarani <ali@kvikshaug.no>
2012-02-02 15:39:38 +0100

    Have new article redirect to edit action

--- project/admin/articles/views.py
+++ project/admin/articles/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
 
 def list(request):
     context = {}
     return render(request, 'admin/articles/list.html', context)
 
 def new(request):
+    return HttpResponseRedirect(reverse('admin.articles.views.edit'))
+
+def edit(request):
     context = {}
-    return render(request, 'admin/articles/new.html', context)
+    return render(request, 'admin/articles/edit.html', context)
