32f8f5bb25bc5bfdb6b4441a132bfcc88fe133f3
Ali Kaafarani <ali@kvikshaug.no>
2012-02-05 23:28:51 +0100

    Remove unnecessary imports

--- project/admin/views.py
+++ project/admin/views.py
@@ -1,13 +1,4 @@
 from django.shortcuts import render
 
-from project.admin.analytics.views import *
-from project.admin.cms.views.block import *
-from project.admin.cms.views.content import *
-from project.admin.cms.views.menu import *
-from project.admin.cms.views.page import *
-from project.admin.cms.views.variant import *
-from project.admin.cms.views.version import *
-from project.admin.cms.views.widget import *
-
 def index(request):
     return render(request, 'admin/dashboard.html')
