b1bf3dad0a6e806816d90eb3d0343b9d17a69e7f
Ali Kaafarani <ali@kvikshaug.no>
2012-02-07 15:01:36 +0100

    Add save button, save content

--- static/js/admin-editor.js
+++ static/js/admin-editor.js
@@ -1,3 +1,10 @@
+/* Always include CSRF-token in AJAX requests */
+$.ajaxSetup({
+    beforeSend: function(xhr, settings) {
+        xhr.setRequestHeader("X-CSRFToken", $("input[name='csrfmiddlewaretoken']").val());
+    }
+});
+
 $(document).ready(function() {
     $(".block").sortable({ disabled: true });
     $("#toolbar div.tab").hide().first().show();
@@ -10,4 +17,24 @@ $(document).ready(function() {
     // Draggable will set position relative, so make sure it is fixed before the user drags it
     $("#toolbar").css('position', 'fixed');
     $(".cms-content").attr('contenteditable', 'true');
+
+    // Saving
+    $("#toolbar #tabs a.save").click(function() {
+        saveContent();
+    });
+
 });
+
+function saveContent() {
+    $(".cms-content").each(function(c) {
+        $.ajax({
+            url: '/sherpa/cms/innhold/oppdater/' + $(this).attr('data-id') + '/',
+            type: 'POST',
+            data: "content=" + encodeURIComponent($(this).html())
+        }).done(function(string) {
+        }).fail(function(string) {
+            // Todo: Error handling
+        }).always(function(string) {
+        });
+    });
+}
