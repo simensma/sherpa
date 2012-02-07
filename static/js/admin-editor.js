ce4505df4014f9ad7c8821ff06f6d8c24063bdc5
Ali Kaafarani <ali@kvikshaug.no>
2012-02-07 17:48:49 +0100

    Enable drag-drop sorting of rows and columns

--- static/js/admin-editor.js
+++ static/js/admin-editor.js
@@ -6,23 +6,59 @@ $.ajaxSetup({
 });
 
 $(document).ready(function() {
-    $(".block").sortable({ disabled: true });
+    // Hide/show chosen toolbar tab
     $("#toolbar div.tab").hide().first().show();
     $("#toolbar li").click(function() {
         $("#toolbar div.tab").hide();
         $($("#toolbar div.tab")[$(this).index()]).show();
     });
-
+    // Make toolbar draggable
     $("#toolbar").draggable();
     // Draggable will set position relative, so make sure it is fixed before the user drags it
     $("#toolbar").css('position', 'fixed');
-    $(".cms-content").attr('contenteditable', 'true');
 
-    // Saving
+    // Edit mode - formatting, move vertically/horizontally
+    $("#cms-container").sortable({ disabled: true });
+    $(".cms-row").sortable({ disabled: true });
+    $("#toolbar #tabs input.formatting").click(function() {
+        disableSort($("#cms-container"));
+        disableSort($(".cms-row"));
+        $(".cms-content").attr('contenteditable', 'true');
+    });
+    $("#toolbar #tabs input.vertical").click(function() {
+        enableSort($("#cms-container"), 'vertical');
+        disableSort($(".cms-row"));
+        $(".cms-content").attr('contenteditable', 'false');
+    });
+    $("#toolbar #tabs input.horizontal").click(function() {
+        disableSort($("#cms-container"));
+        enableSort($(".cms-row"), 'horizontal');
+        $(".cms-content").attr('contenteditable', 'false');
+    });
+
+    function disableSort(element) {
+        element.sortable('disable');
+        element.children().off('mouseenter');
+        element.children().off('mouseleave');
+    }
+
+    function enableSort(element, alignment) {
+        element.sortable('enable');
+        element.children().on('mouseenter', function() {
+            $(this).addClass('moveable ' + alignment);
+        });
+        element.children().on('mouseleave', function() {
+            $(this).removeClass('moveable ' + alignment);
+        });
+    }
+
+    // Save all content
     $("#toolbar #tabs a.save").click(function() {
         saveContent();
     });
 
+    // Allow content editing of content elements
+    $(".cms-content").attr('contenteditable', 'true');
 });
 
 function saveContent() {
