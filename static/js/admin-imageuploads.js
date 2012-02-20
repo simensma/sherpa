68e8452084476ad42905b5cea31dedc18cd6c026
Ali Kaafarani <ali@kvikshaug.no>
2012-02-20 11:49:31 +0100

    Don't use the jquery form plugin
    There seems to be a time-delay bug when the 'complete' event should be
    called, and instead of tracking down the root cause, implement the
    upload functionality manually.

--- static/js/admin-imageuploads.js
+++ static/js/admin-imageuploads.js
@@ -1,13 +1,3 @@
 $(document).ready(function() {
 
-    $("#image-uploader form").ajaxForm({
-        beforeSubmit: function() {
-            $("div.well").hide();
-            $("input[type='submit']").prop('disabled', true);
-        },
-        complete: function(response) {
-            $(document.body).html(response.responseText);
-        }
-    });
-
 });
