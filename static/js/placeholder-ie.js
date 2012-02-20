6cb3868c632ac1bb2d5b67216e503f15a51fb83b
Ali Kaafarani <ali@kvikshaug.no>
2012-02-20 20:10:19 +0100

    Don't submit placeholder values

--- static/js/placeholder-ie.js
+++ static/js/placeholder-ie.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
 
     /* IE doesn't support the placeholder attribute, so simulate it. */
     $("input[placeholder]").each(function() {
+        // Note: This doesn't account for that rare case where the user actually
+        // wants to input exactly what's in the placeholder.
         $(this).data('placeholder', $(this).attr('placeholder'));
         $(this).val($(this).data('placeholder'));
         $(this).removeAttr('placeholder');
@@ -13,6 +15,12 @@ $(document).ready(function() {
         $(this).focusout(function() {
             if($(this).val() == "") {
                 $(this).val($(this).data('placeholder'));
+            }
+        });
+        var input = $(this);
+        $(this).parents("form").submit(function() {
+            if(input.val() == input.data('placeholder')) {
+                input.val("");
             }
         });
     });
