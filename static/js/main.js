4fed10d771f6594102fe204b4d325a6de948842a
Ali Kaafarani <ali@kvikshaug.no>
2012-02-21 10:53:55 +0100

    Fine-tune layout and add some more template content

--- static/js/main.js
+++ static/js/main.js
@@ -3,4 +3,9 @@ $(document).ready(function() {
     /* For any close button, close its parent alert */
     $("a.close").click(function() { $(this).parent().hide(); });
 
+    /* Scale article image sidebars according to the image width */
+    $("article .image-right").each(function() {
+        $(this).width($(this).children("img").width());
+    });
+
 });
