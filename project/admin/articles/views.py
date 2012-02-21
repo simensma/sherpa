0f01b66ad7bb823b0e27d1b19ccb5227918dd3bd
Ali Kaafarani <ali@kvikshaug.no>
2012-02-21 16:43:49 +0100

    Create new article upon POST

--- project/admin/articles/views.py
+++ project/admin/articles/views.py
@@ -2,13 +2,19 @@ from django.shortcuts import render
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 
+from articles.models import Article
+
 def list(request):
     context = {}
     return render(request, 'admin/articles/list.html', context)
 
 def new(request):
-    return HttpResponseRedirect(reverse('admin.articles.views.edit'))
+    article = Article(title=request.POST['title'], published=False, pub_date=None,
+      publisher=request.user.get_profile())
+    article.save()
+    return HttpResponseRedirect(reverse('admin.articles.views.edit', args=[article.id]))
 
-def edit(request):
-    context = {}
+def edit(request, article):
+    article = Article.objects.get(id=article)
+    context = {'article': article}
     return render(request, 'admin/articles/edit.html', context)
