3523d71b1917bfb85e0bd29f98f7a54a1a67ae70
Ali Kaafarani <ali@kvikshaug.no>
2012-02-22 18:52:35 +0100

    List articles in article-list view

--- project/admin/articles/views.py
+++ project/admin/articles/views.py
@@ -3,9 +3,11 @@ from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 
 from articles.models import Article
+from page.models import Version
 
 def list(request):
-    context = {}
+    versions = Version.objects.filter(variant__article__isnull=False, variant__segment__isnull=True, active=True)
+    context = {'versions': versions}
     return render(request, 'admin/articles/list.html', context)
 
 def new(request):
