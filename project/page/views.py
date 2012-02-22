bb9f2ac60e27ea2cba305d7f82938df4c3dc1775
Ali Kaafarani <ali@kvikshaug.no>
2012-02-22 15:23:21 +0100

    Use GET param to request variant by id

--- project/page/views.py
+++ project/page/views.py
@@ -7,34 +7,34 @@ from string import split
 
 from page.views_widgets import *
 
-def page(request, slugs):
-    for pair in slug_combinations(slugs):
-        page = Page.objects.filter(slug=pair['pageslug'])
-        if len(page) != 0:
-            page = page[0]
-            matched_variant = match_user(request, page)
-            if(pair['variantslug'] == ''):
-                if matched_variant is not None:
-                    # Render the matched variant
-                    if(pair['pageslug'] == ""): args = []
-                    else: args = [pair['pageslug']]
-                    return HttpResponseRedirect("%s%s/" % (reverse('page.views.page', args=args), matched_variant.slug))
-                else:
-                    default = PageVariant.objects.get(page=page, segment__isnull=True)
-                    version = PageVersion.objects.get(variant=default)
-                    pageview = Pageview(request=request.session['request'],
-                      variant=default, active_version=version,
-                      requested_segment=None, matched_segment=None)
-                    pageview.save()
-                    return parse_content(request, version)
-            else:
-                variant = PageVariant.objects.get(page=page, slug=pair['variantslug'])
-                version = PageVersion.objects.get(variant=variant)
-                pageview = Pageview(request=request.session['request'],
-                  variant=variant, active_version=version,
-                  requested_segment=variant.segment, matched_segment=matched_variant.segment)
-                pageview.save()
+variant_key = 'var'
+
+def page(request, slug):
+    page = Page.objects.get(slug=slug)
+    matched_variant = match_user(request, page)
+    requested_variant = request.GET.get(variant_key)
+    if(requested_variant == None):
+        if(matched_variant == None):
+            # No variant requested, and no variant matched. The default, simple view for a page.
+            default_variant = PageVariant.objects.get(page=page, segment__isnull=True)
+            version = PageVersion.objects.get(variant=default_variant, active=True)
+            save_pageview(request, default_variant, version, None, None)
             return parse_content(request, version)
+        else:
+            # No variant requested, but the page has variants and a special one matched.
+            return HttpResponseRedirect("%s?%s=%s" %
+                (reverse('page.views.page', args=[slug]), variant_key, matched_variant.id))
+    else:
+        # A specific variant was requested. Show it regardless of which variant matches the user,
+        # but do log what actually matched.
+        requested_variant = PageVariant.objects.get(id=requested_variant)
+        version = PageVersion.objects.get(variant=requested_variant, active=True)
+        save_pageview(request, requested_variant, version, requested_variant.segment, matched_variant.segment)
+        return parse_content(request, version)
+
+def save_pageview(request, variant, version, requested_segment, matched_segment):
+    pageview = Pageview(request=request.session['request'], variant=variant,
+        active_version=version, requested_segment=requested_segment, matched_segment=matched_segment)
 
 def match_user(request, page):
     variants = PageVariant.objects.filter(page=page, segment__isnull=False).order_by('priority')
@@ -43,17 +43,3 @@ def match_user(request, page):
         if(variant.segment.match(request, visitor)):
             return variant
     return None
-
-def slug_combinations(slugs):
-    combinations = []
-    slugs = split(slugs, '/')
-    for i in range(len(slugs), -1, -1):
-        pageslug = ""
-        variantslug = ""
-        for j in range(i):
-            pageslug += '/' + slugs[j]
-        for j in range(i, len(slugs)):
-            variantslug += '/' + slugs[j]
-        pair = {'pageslug': pageslug[1:], 'variantslug': variantslug[1:]}
-        combinations.append(pair)
-    return combinations
