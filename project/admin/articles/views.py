6b44a27904b7f3674dae49260253357a857d7a98
Ali Kaafarani <ali@kvikshaug.no>
2012-02-23 13:42:14 +0100

    Require login for article editing

--- project/admin/articles/views.py
+++ project/admin/articles/views.py
@@ -1,21 +1,25 @@
 from django.shortcuts import render
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
 
 from articles.models import Article
 from page.models import Version
 
+@login_required
 def list(request):
     versions = Version.objects.filter(variant__article__isnull=False, variant__segment__isnull=True, active=True)
     context = {'versions': versions}
     return render(request, 'admin/articles/list.html', context)
 
+@login_required
 def new(request):
     article = Article(title=request.POST['title'], published=False, pub_date=None,
       publisher=request.user.get_profile())
     article.save()
     return HttpResponseRedirect(reverse('admin.articles.views.edit', args=[article.id]))
 
+@login_required
 def edit(request, article):
     article = Article.objects.get(id=article)
     context = {'article': article}
