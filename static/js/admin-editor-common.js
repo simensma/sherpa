b4396e29b451ee1679b9ff6406f62e9410f07b39
Ali Kaafarani <ali@kvikshaug.no>
2012-02-24 14:42:48 +0100

    Enable toolbar button functionality

--- static/js/admin-editor-common.js
+++ static/js/admin-editor-common.js
@@ -14,4 +14,47 @@ $(document).ready(function() {
     // Draggable will set position relative, so make sure it is fixed before the user drags it
     $("#toolbar").css('position', 'fixed');
 
+    /* Toolbar buttons */
+
+    $("#toolbar a.header").click(function() {
+        document.execCommand('formatblock', false, 'h1');
+    });
+    $("#toolbar a.lede").click(function() {
+        $(":focus").toggleClass('lede');
+    });
+    $("#toolbar a.body").click(function() {
+        document.execCommand('formatblock', false, 'p');
+    });
+    $("#toolbar a.bold").click(function(event) {
+        document.execCommand('bold');
+    });
+    $("#toolbar a.italic").click(function(event) {
+        document.execCommand('italic');
+    });
+    $("#toolbar a.underline").click(function(event) {
+        document.execCommand('underline');
+    });
+    $("#toolbar a.ol").click(function(event) {
+        document.execCommand('insertorderedlist');
+    });
+    $("#toolbar a.ul").click(function(event) {
+        document.execCommand('insertunorderedlist');
+    });
+    $("#toolbar a.anchor").click(function(event) {
+        document.execCommand('createLink', false, 'TODO');
+    });
+    $("#toolbar a.left").click(function(event) {
+        document.execCommand('justifyleft');
+    });
+    $("#toolbar a.center").click(function(event) {
+        document.execCommand('justifycenter');
+    });
+    $("#toolbar a.right").click(function(event) {
+        document.execCommand('justifyright');
+    });
+    $("#toolbar a.full").click(function(event) {
+        document.execCommand('justifyfull');
+    });
+
+
 });
