45b3ceb2e837f5c7e8a52dde54021af68641ef44
Ali Kaafarani <ali@kvikshaug.no>
2012-02-24 09:40:18 +0100

    Send version content to the article edit template

--- project/admin/articles/urls.py
+++ project/admin/articles/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls.defaults import patterns, include, url
 urlpatterns = patterns('admin.articles.views',
     url(r'^$', 'list'),
     url(r'^ny/$', 'new'),
-    url(r'^rediger/(?P<article>\d+)/$', 'edit'),
+    url(r'^rediger/(?P<version>\d+)/$', 'edit'),
 )
--- project/admin/articles/views.py
+++ project/admin/articles/views.py
@@ -4,7 +4,9 @@ from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 
 from articles.models import Article
-from page.models import Version
+from page.models import Variant, Version, Row, Column, Content
+
+from admin.cms.views.widget import parse_widget
 
 @login_required
 def list(request):
@@ -17,10 +19,24 @@ def new(request):
     article = Article(title=request.POST['title'], published=False, pub_date=None,
       publisher=request.user.get_profile())
     article.save()
-    return HttpResponseRedirect(reverse('admin.articles.views.edit', args=[article.id]))
+    variant = Variant(page=None, article=article, name='default', segment=None, priority=1, publisher=request.user.get_profile())
+    variant.save()
+    version = Version(variant=variant, version=1, publisher=request.user.get_profile(), active=True)
+    version.save()
+    return HttpResponseRedirect(reverse('admin.articles.views.edit', args=[version.id]))
 
 @login_required
-def edit(request, article):
-    article = Article.objects.get(id=article)
-    context = {'article': article}
+def edit(request, version):
+    version = Version.objects.get(id=version)
+    rows = Row.objects.filter(version=version).order_by('order')
+    for row in rows:
+        columns = Column.objects.filter(row=row).order_by('order')
+        for column in columns:
+            contents = Content.objects.filter(column=column).order_by('order')
+            for content in contents:
+                if content.type == 'w':
+                    content.widget = parse_widget(json.loads(content.content))
+            column.contents = contents
+        row.columns = columns
+    context = {'rows': rows}
     return render(request, 'admin/articles/edit.html', context)
