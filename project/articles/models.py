add6961ce4c97220dda2289b7ba1cfd99da0f3b8
Ali Kaafarani <ali@kvikshaug.no>
2012-02-28 13:28:30 +0100

    Perform deep delete upon deleting article

--- project/articles/models.py
+++ project/articles/models.py
@@ -1,7 +1,15 @@
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
 from django.db import models
+
+from page.models import Variant
 
 class Article(models.Model):
     title = models.CharField(max_length=200)
     published = models.BooleanField()
     pub_date = models.DateTimeField(null=True)
     publisher = models.ForeignKey('user.Profile')
+
+@receiver(post_delete, sender=Article)
+def delete_article(sender, **kwargs):
+    Variant.objects.filter(article=kwargs['instance']).delete()
