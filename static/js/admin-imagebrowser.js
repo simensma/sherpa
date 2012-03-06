174368a0d0c5368373df4fe1f9ca8205fb4e9d0d
Ali Kaafarani <ali@kvikshaug.no>
2012-03-06 10:48:11 +0100

    Allow deleting albums and images simultaneously

--- static/js/admin-imagebrowser.js
+++ static/js/admin-imagebrowser.js
@@ -1,11 +1,8 @@
 $(document).ready(function() {
 
+    var dialog;
     $("button.details-button").click(function() {
-        if(selected == "album") {
-            $(".album-details.dialog").dialog('open');
-        } else if(selected == "image") {
-            $(".image-details.dialog").dialog('open');
-        }
+        dialog.dialog('open');
     });
 
     $(".album-details form").submit(function() {
@@ -25,33 +22,39 @@ $(document).ready(function() {
         serializeTags();
     });
 
-    var selected;
     $("button.context-button").attr('disabled', true);
     function toggleMultiedit() {
-        if($("#archive-gallery li.selected").length > 0) {
-            $("button.context-button").removeAttr('disabled');
+        var albums = $("#archive-gallery li.album.selected").length > 0;
+        var images = $("#archive-gallery li.image.selected").length > 0;
+        if(albums && images) {
+            $("table.action-buttons button.delete-button").removeAttr('disabled');
+            $("table.action-buttons button.details-button").attr('disabled', true);
+            $("div.delete-dialog p").text('Er du helt sikker på at du vil slette alle albumene og bildene du har merket, for godt? Alle underalbum og bilder i merkede album vil også bli slettet for godt.');
+            $("table.action-buttons button.delete-button").html('<i class="icon-remove"></i> Slett album og bilder');
+        } else if(albums) {
+            $("table.action-buttons button.context-button").removeAttr('disabled');
+            $("div.delete-dialog p").text('Er du helt sikker på at du vil slette alle albumene du har merket, for godt? Alle underalbum og bilder i albumet vil også bli slettet for godt.');
+            $("table.action-buttons button.details-button").html('<i class="icon-pencil"></i> Endre albumdetaljer');
+            $("table.action-buttons button.delete-button").html('<i class="icon-remove"></i> Slett album');
+            dialog = $(".album-details.dialog");
+        } else if(images) {
+            $("table.action-buttons button.context-button").removeAttr('disabled');
+            $("div.delete-dialog p").text('Er du helt sikker på at du vil slette alle bildene du har merket, for godt?');
+            $("table.action-buttons button.details-button").html('<i class="icon-pencil"></i> Endre bildedetaljer');
+            $("table.action-buttons button.delete-button").html('<i class="icon-remove"></i> Slett bilder');
+            dialog = $(".image-details.dialog");
         } else {
-            $("button.context-button").attr('disabled', true);
+            $("table.action-buttons button.context-button").attr('disabled', true);
         }
     }
 
     $("#archive-gallery li.album button.mark").click(function() {
-        $("#archive-gallery li.image.selected").removeClass('selected');
         $(this).parent("li").toggleClass('selected');
-        $("div.delete-dialog p").text('Er du helt sikker på at du vil slette alle albumene du har merket, for godt? Alle underalbum og bilder i albumet vil også bli slettet for godt.');
-        $("table.action-buttons button.details-button").html('<i class="icon-pencil"></i> Endre albumdetaljer');
-        $("table.action-buttons button.delete-button").html('<i class="icon-remove"></i> Slett album');
-        selected = 'album';
         toggleMultiedit();
     });
 
     $("#archive-gallery li.image button.mark").click(function() {
-        $("#archive-gallery li.album.selected").removeClass('selected');
         $(this).parent("li").toggleClass('selected');
-        $("div.delete-dialog p").text('Er du helt sikker på at du vil slette alle bildene du har merket, for godt?');
-        $("table.action-buttons button.details-button").html('<i class="icon-pencil"></i> Endre bildedetaljer');
-        $("table.action-buttons button.delete-button").html('<i class="icon-remove"></i> Slett bilder');
-        selected = 'image';
         toggleMultiedit();
     });
 
