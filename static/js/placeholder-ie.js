67c08ebb3ba76bf4a87aa43bd8389cb619897e2e
Ali Kaafarani <ali@kvikshaug.no>
2012-03-13 16:24:17 +0100

    Allow IE-users to input exact placeholder value

--- static/js/placeholder-ie.js
+++ static/js/placeholder-ie.js
@@ -2,24 +2,25 @@ $(document).ready(function() {
 
     /* IE doesn't support the placeholder attribute, so simulate it. */
     $("input[placeholder]").each(function() {
-        // Note: This doesn't account for that rare case where the user actually
-        // wants to input exactly what's in the placeholder.
+        $(this).data('holding', true); // true when the value is actually the placeholder
         $(this).data('placeholder', $(this).attr('placeholder'));
         $(this).val($(this).data('placeholder'));
         $(this).removeAttr('placeholder');
         $(this).focus(function() {
-            if($(this).val() == $(this).data('placeholder')) {
+            if($(this).data('holding')) {
+                $(this).data('holding', false);
                 $(this).val("");
             }
         });
         $(this).focusout(function() {
             if($(this).val() == "") {
+                $(this).data('holding', true);
                 $(this).val($(this).data('placeholder'));
             }
         });
         var input = $(this);
         $(this).parents("form").submit(function() {
-            if(input.val() == input.data('placeholder')) {
+            if(input.data('holding')) {
                 input.val("");
             }
         });
