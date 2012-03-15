4fbc413b45de7f701d9ec749636bc505ad4e85a8
Ali Kaafarani <ali@kvikshaug.no>
2012-03-15 14:14:23 +0100

    Remove unused import

--- project/enrollment/views.py
+++ project/enrollment/views.py
@@ -1,7 +1,6 @@
 # encoding: utf-8
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
-from django.contrib.auth.decorators import login_required
 
 from user.models import Zipcode
 
