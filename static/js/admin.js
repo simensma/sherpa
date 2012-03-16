30ccd1acddaade62af5cca9a8480dd3063d39532
Ali Kaafarani <ali@kvikshaug.no>
2012-03-16 15:32:11 +0100

    Move dialog enabling to main js

--- static/js/admin.js
+++ static/js/admin.js
@@ -1,33 +1,10 @@
 $(document).ready(function() {
 
-    /* Enable all dialogs */
-    $(".dialog").enableDialog();
-    $(".dialog-button").enableDialogButton();
-
     /* When creating a page, slugify the title as URL */
     $("#add-page-dialog input[name='title']").keyup(function() {
         $("#add-page-dialog input[name='slug']").val(slugify($(this).val()));
     })
 });
-
-$.fn.enableDialog = function() {
-    return this.each(function() {
-        $(this).dialog({
-            title: $(this).attr('data-title'),
-            modal: true,
-            autoOpen: false,
-            width: $(this).attr('data-width')
-        }).hide();
-    });
-}
-
-$.fn.enableDialogButton = function() {
-    return this.each(function() {
-        $(this).click(function(event) {
-            $($(this).attr('data-dialog')).dialog('open');
-        });
-    });
-}
 
 function slugify(string) {
     string = string.toLowerCase().trim();
