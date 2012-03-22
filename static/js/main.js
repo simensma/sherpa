e0eb1fbf2714810753bc73f78f108a0c0f9a97df
Ali Kaafarani <ali@kvikshaug.no>
2012-03-22 17:18:21 +0100

    Remove obsolete article code

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,3 @@
 $(document).ready(function() {
 
-    /* Scale article image sidebars according to the image width */
-    $("article .image-right").each(function() {
-        $(this).width($(this).children("img").width());
-    });
-
 });
