e7c57c0d22e93276fad1c7daeaca88f0cf108017
Ali Kaafarani <ali@kvikshaug.no>
2012-03-27 16:43:32 +0200

    Add editor widget for articles

--- project/page/views_widgets.py
+++ project/page/views_widgets.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 
 from page.models import Row, Column, Content
+from articles.models import Article
 
 import json
 
@@ -25,3 +26,9 @@ def parse_widget(widget):
         return {'template': 'widgets/quote/display.html', 'quote': widget['quote'], 'author': widget['author']}
     elif(widget['widget'] == "promo"):
         return {'template': 'widgets/promo/display.html'}
+    elif(widget['widget'] == "editor"):
+        article = Article.objects.get(id=widget['article'])
+        return {'template': 'widgets/editor/display.html',
+            'author': article.publisher.user.get_full_name(),
+            'email': "TBD",
+            'publishdate': article.pub_date}
