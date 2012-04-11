82bc48d0fe73f7e992545ec375c015c1ca5d3a0c
Ali Kaafarani <ali@kvikshaug.no>
2012-04-11 15:05:05 +0200

    Separate showFolder from updating contents

--- static/js/admin/editor-image.js
+++ static/js/admin/editor-image.js
@@ -21,53 +21,55 @@ $(document).ready(function() {
 });
 
 function showFolder(album) {
+    var ajaxLoader = $('<img class="ajaxloader" src="/static/img/ajax-loader-small.gif" alt="Laster, vennligst vent...">');
     var list = $("div#dialog-change-image div#imagearchive div#contentlist");
-    var images = $("div#dialog-change-image div#imagearchive ul#images");
     list.contents().remove();
-    images.children().remove();
-    list.append('<img class="ajaxloader" src="/static/img/ajax-loader-small.gif" alt="Laster, vennligst vent...">');
+    list.append(ajaxLoader);
+    $("div#dialog-change-image div#imagearchive ul#images").children().remove();
     $.ajax({
         url: '/sherpa/bildearkiv/innhold/' + album,
         type: 'POST'
-    }).done(function(result) {
-        result = JSON.parse(result);
-
-        // Add albums
-        for(var i=0; i<result.albums.length; i++) {
-            var item = $('<div class="album"><a href="javascript:undefined" data-id="' + result.albums[i].id + '/"><img src="/static/img/icons/folder.png" alt="Album" class="album"> ' + result.albums[i].name + '</a></div><div style="clear: both;"></div>');
-            item.find("a").click(function() {
-                showFolder($(this).attr('data-id'));
-            });
-            $("div#dialog-change-image div#imagearchive div#contentlist").append(item);
-        }
-
-        // Add images
-        for(var i=0; i<result.images.length; i++) {
-            var item = $('<li data-path="' + result.images[i].key + '.' + result.images[i].extension + '" data-description="' + result.images[i].description + '"><p><img src="http://cdn.turistforeningen.no/images/' + result.images[i].key + '-150.' + result.images[i].extension + '" alt="Thumbnail"></p>' + result.images[i].width + ' x ' + result.images[i].height + '<br>' + result.images[i].photographer + '</li>');
-            item.click(function() {
-                $("div#dialog-change-image input[name='url']").val("http://cdn.turistforeningen.no/images/" + $(this).attr('data-path'));
-                $("div#dialog-change-image input[name='alt']").val($(this).attr('data-description'));
-                $("div#dialog-change-image button.insert-image").click();
-            });
-            images.append(item);
-        }
-
-        // Add breadcrumbs
-        bcList.children().remove();
-        bcList.append(bcRoot);
-        bcRoot.find("a").click(function() {
-            showFolder('');
-        });
-        for(var i=0; i<result.parents.length; i++) {
-            var item = $('<li><a href="javascript:undefined" data-id="' + result.parents[i].id + '/">' + result.parents[i].name + '</a></li>');
-            item.find("a").click(function() {
-                showFolder($(this).attr('data-id'));
-            });
-            bcList.append($('<span class="divider">→</span>')).append(item);
-        }
-    }).fail(function(result) {
+    }).done(updateContents).fail(function(result) {
         $(document.body).html(result.responseText);
     }).always(function(result) {
-        list.find("img.ajaxloader").remove();
+        ajaxLoader.remove();
     });
+}
+
+function updateContents(result) {
+    result = JSON.parse(result);
+
+    // Add breadcrumbs
+    bcList.children().remove();
+    bcList.append(bcRoot);
+    bcRoot.find("a").click(function() {
+        showFolder('');
+    });
+    for(var i=0; i<result.parents.length; i++) {
+        var item = $('<li><a href="javascript:undefined" data-id="' + result.parents[i].id + '/">' + result.parents[i].name + '</a></li>');
+        item.find("a").click(function() {
+            showFolder($(this).attr('data-id'));
+        });
+        bcList.append($('<span class="divider">→</span>')).append(item);
+    }
+
+    // Add albums
+    for(var i=0; i<result.albums.length; i++) {
+        var item = $('<div class="album"><a href="javascript:undefined" data-id="' + result.albums[i].id + '/"><img src="/static/img/icons/folder.png" alt="Album" class="album"> ' + result.albums[i].name + '</a></div><div style="clear: both;"></div>');
+        item.find("a").click(function() {
+            showFolder($(this).attr('data-id'));
+        });
+        $("div#dialog-change-image div#imagearchive div#contentlist").append(item);
+    }
+
+    // Add images
+    for(var i=0; i<result.images.length; i++) {
+        var item = $('<li data-path="' + result.images[i].key + '.' + result.images[i].extension + '" data-description="' + result.images[i].description + '"><p><img src="http://cdn.turistforeningen.no/images/' + result.images[i].key + '-150.' + result.images[i].extension + '" alt="Thumbnail"></p>' + result.images[i].width + ' x ' + result.images[i].height + '<br>' + result.images[i].photographer + '</li>');
+        item.click(function() {
+            $("div#dialog-change-image input[name='url']").val("http://cdn.turistforeningen.no/images/" + $(this).attr('data-path'));
+            $("div#dialog-change-image input[name='alt']").val($(this).attr('data-description'));
+            $("div#dialog-change-image button.insert-image").click();
+        });
+        $("div#dialog-change-image div#imagearchive ul#images").append(item);
+    }
 }
