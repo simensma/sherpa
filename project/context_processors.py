d9d81685bb3ea8d6ba05a86f4adea147ee27ede6
Ali Kaafarani <ali@kvikshaug.no>
2012-04-16 15:54:54 +0200

    Add a final slash to the menu URL if missing

--- project/context_processors.py
+++ project/context_processors.py
@@ -6,6 +6,7 @@ def menus(request):
     menus = Menu.objects.all().order_by('order')
     for menu in menus:
         url = re.sub('https?:\/\/', '', menu.url) # Strip protocol
+        url = re.sub('[^\/]$', '/', url) # Add final slash if missing
         if request.get_host() + request.path == url:
             menu.active = True
             break
