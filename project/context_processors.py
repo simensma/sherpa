52e8c6528c5fab34c7006e389fa2054e3641fdb5
Ali Kaafarani <ali@kvikshaug.no>
2012-04-16 15:36:54 +0200

    Separate stripping and comparison logic

--- project/context_processors.py
+++ project/context_processors.py
@@ -5,7 +5,8 @@ import re
 def menus(request):
     menus = Menu.objects.all().order_by('order')
     for menu in menus:
-        if request.get_host() + request.path == re.sub('http:\/\/', '', menu.url):
+        url = re.sub('http:\/\/', '', menu.url) # Strip protocol
+        if request.get_host() + request.path == url:
             menu.active = True
             break
     return {'menus': menus}
