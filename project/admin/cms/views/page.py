d45c2f5718df936609a317b3a40fc4b6f70d5756
Ali Kaafarani <ali@kvikshaug.no>
2012-04-20 11:19:21 +0200

    Add sidebar template

--- project/admin/cms/views/page.py
+++ project/admin/cms/views/page.py
@@ -40,6 +40,40 @@ def new(request):
         for i in range(len(contents)):
             content = Content(column=column, content=contents[i]['content'], type=contents[i]['type'], order=i)
             content.save()
+    elif request.POST['template'] == '3':
+        contents_upper = [
+            {'type': 'html', 'content': """<h1>Fengende overskrift</h1>"""},
+            {'type': 'image', 'content': """<img src=\"""" + settings.STATIC_URL + """img/templates/placeholder.jpg" alt="Placeholder">"""},
+            {'type': 'html', 'content': """<p>BILDETEKST: Nunc diam velit, adipiscing ut tristique vitae, sagittis vel odio. Maecenas convallis.<br><em>Foto: Ola Nordmann/DNT</em></p>"""},
+        ]
+        contents_lower_left = [
+            {'type': 'html', 'content': """<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue. Nam tincidunt congue enim, ut porta lorem lacinia consectetur. Donec ut libero sed arcu vehicula ultricies a non tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut gravida lorem. Ut turpis felis, pulvinar a semper sed, adipiscing id dolor. Pellentesque auctor nisi id magna consequat sagittis. Curabitur dapibus enim sit amet elit pharetra tincidunt feugiat nisl imperdiet. Ut convallis libero in urna ultrices accumsan. Donec sed odio eros. Donec viverra mi quis quam pulvinar at malesuada arcu rhoncus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In rutrum accumsan ultricies. Mauris vitae nisi at sem facilisis semper ac in est.</p>"""},
+            {'type': 'html', 'content': """<h2>Mindre overskrift</h2><p>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In euismod ultrices facilisis. Vestibulum porta sapien adipiscing augue congue id pretium lectus molestie. Proin quis dictum nisl. Morbi id quam sapien, sed vestibulum sem. Duis elementum rutrum mauris sed convallis. Proin vestibulum magna mi. Aenean tristique hendrerit magna, ac facilisis nulla hendrerit ut. Sed non tortor sodales quam auctor elementum. Donec hendrerit nunc eget elit pharetra pulvinar. Suspendisse id tempus tortor. Aenean luctus, elit commodo laoreet commodo, justo nisi consequat massa, sed vulputate quam urna quis eros. Donec vel.</p>"""},
+        ]
+        contents_lower_right = [
+            {'type': 'html', 'content': """<p>Suspendisse lectus leo, consectetur in tempor sit amet, placerat quis neque. Etiam luctus porttitor lorem, sed suscipit est rutrum non. Curabitur lobortis nisl a enim congue semper. Aenean commodo ultrices imperdiet. Vestibulum ut justo vel sapien venenatis tincidunt. Phasellus eget dolor sit amet ipsum dapibus condimentum vitae quis lectus.</p>"""},
+            {'type': 'image', 'content': """<img src=\"""" + settings.STATIC_URL + """img/templates/placeholder.jpg" alt="Placeholder">"""},
+            {'type': 'html', 'content': """<p>BILDETEKST: Donec ut libero sed arcu vehicula.<br><em>Foto: Kari Nordmann/DNT</em></p>"""},
+        ]
+        row = Row(version=version, order=0)
+        row.save()
+        column = Column(row=row, span=12, offset=0, order=0)
+        column.save()
+        for i in range(len(contents_upper)):
+            content = Content(column=column, content=contents_upper[i]['content'], type=contents_upper[i]['type'], order=i)
+            content.save()
+        row = Row(version=version, order=1)
+        row.save()
+        column = Column(row=row, span=9, offset=0, order=0)
+        column.save()
+        for i in range(len(contents_lower_left)):
+            content = Content(column=column, content=contents_lower_left[i]['content'], type=contents_lower_left[i]['type'], order=i)
+            content.save()
+        column = Column(row=row, span=3, offset=0, order=0)
+        column.save()
+        for i in range(len(contents_lower_right)):
+            content = Content(column=column, content=contents_lower_right[i]['content'], type=contents_lower_right[i]['type'], order=i)
+            content.save()
     return HttpResponseRedirect(reverse('admin.cms.views.version.edit', args=[version.id]))
 
 @login_required
