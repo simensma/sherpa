ae49643cfc14768ecd1566dd8aebd183a74eb2d7
Ali Kaafarani <ali@kvikshaug.no>
2012-04-20 11:45:41 +0200

    Add three-columns template

--- project/admin/cms/views/page.py
+++ project/admin/cms/views/page.py
@@ -74,6 +74,56 @@ def new(request):
         for i in range(len(contents_lower_right)):
             content = Content(column=column, content=contents_lower_right[i]['content'], type=contents_lower_right[i]['type'], order=i)
             content.save()
+    elif request.POST['template'] == '4':
+        contents_upper = [
+            {'type': 'html', 'content': """<h1>Fengende overskrift</h1>"""},
+            {'type': 'image', 'content': """<img src=\"""" + settings.STATIC_URL + """img/templates/placeholder.jpg" alt="Placeholder">"""},
+            {'type': 'html', 'content': """<p>BILDETEKST: Nunc diam velit, adipiscing ut tristique vitae, sagittis vel odio. Maecenas convallis.<br><em>Foto: Ola Nordmann/DNT</em></p>"""},
+        ]
+        contents_middle_left = [
+            {'type': 'html', 'content': """<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue. Nam tincidunt congue enim, ut porta lorem lacinia consectetur. Donec ut libero sed arcu vehicula ultricies a non tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut gravida lorem. Ut turpis felis, pulvinar a semper sed, adipiscing id dolor. Pellentesque auctor nisi id magna consequat sagittis. Curabitur dapibus enim sit amet elit pharetra tincidunt feugiat nisl imperdiet. Ut convallis libero in urna ultrices accumsan. Donec sed odio eros. Donec viverra mi quis quam pulvinar at malesuada arcu rhoncus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In rutrum accumsan ultricies. Mauris vitae nisi at sem facilisis semper ac in est.</p>"""},
+        ]
+        contents_middle_center = [
+            {'type': 'html', 'content': """<p>Vivamus fermentum semper porta. Nunc diam velit, adipiscing ut tristique vitae, sagittis vel odio. Maecenas convallis ullamcorper ultricies. Curabitur ornare, ligula semper consectetur sagittis, nisi diam iaculis velit, id fringilla sem nunc vel mi. Nam dictum, odio nec pretium volutpat, arcu ante placerat erat, non tristique elit urna et turpis. Quisque mi metus, ornare sit amet fermentum et, tincidunt et orci. Fusce eget orci a orci congue vestibulum. Ut dolor diam, elementum et vestibulum eu, porttitor vel elit. Curabitur venenatis pulvinar tellus gravida ornare. Sed et erat faucibus nunc euismod ultricies ut id justo. Nullam cursus suscipit nisi, et ultrices justo sodales nec. Fusce venenatis facilisis lectus ac semper. Aliquam at massa ipsum. Quisque bibendum purus convallis nulla ultrices ultricies. Nullam aliquam, mi eu aliquam tincidunt, purus velit laoreet tortor, viverra pretium nisi quam vitae mi. Fusce vel volutpat elit. Nam sagittis nisi dui.</p>"""},
+        ]
+        contents_middle_right = [
+            {'type': 'html', 'content': """<p>Suspendisse lectus leo, consectetur in tempor sit amet, placerat quis neque. Etiam luctus porttitor lorem, sed suscipit est rutrum non. Curabitur lobortis nisl a enim congue semper. Aenean commodo ultrices imperdiet. Vestibulum ut justo vel sapien venenatis tincidunt. Phasellus eget dolor sit amet ipsum dapibus condimentum vitae quis lectus. Aliquam ut massa in turpis dapibus convallis. Praesent elit lacus, vestibulum at malesuada et, ornare et est. Ut augue nunc, sodales ut euismod non, adipiscing vitae orci. Mauris ut placerat justo. Mauris in ultricies enim. Quisque nec est eleifend nulla ultrices egestas quis ut quam. Donec sollicitudin lectus a mauris pulvinar id aliquam urna cursus. Cras quis ligula sem, vel elementum mi. Phasellus non ullamcorper urna.</p>"""},
+        ]
+        contents_lower = [
+            {'type': 'image', 'content': """<img src=\"""" + settings.STATIC_URL + """img/templates/placeholder.jpg" alt="Placeholder">"""},
+            {'type': 'html', 'content': """<p>BILDETEKST: Donec ut libero sed arcu vehicula.<br><em>Foto: Kari Nordmann/DNT</em></p>"""},
+        ]
+        row = Row(version=version, order=0)
+        row.save()
+        column = Column(row=row, span=12, offset=0, order=0)
+        column.save()
+        for i in range(len(contents_upper)):
+            content = Content(column=column, content=contents_upper[i]['content'], type=contents_upper[i]['type'], order=i)
+            content.save()
+        row = Row(version=version, order=1)
+        row.save()
+        column = Column(row=row, span=4, offset=0, order=0)
+        column.save()
+        for i in range(len(contents_middle_left)):
+            content = Content(column=column, content=contents_middle_left[i]['content'], type=contents_middle_left[i]['type'], order=i)
+            content.save()
+        column = Column(row=row, span=4, offset=0, order=1)
+        column.save()
+        for i in range(len(contents_middle_center)):
+            content = Content(column=column, content=contents_middle_center[i]['content'], type=contents_middle_center[i]['type'], order=i)
+            content.save()
+        column = Column(row=row, span=4, offset=0, order=2)
+        column.save()
+        for i in range(len(contents_middle_right)):
+            content = Content(column=column, content=contents_middle_right[i]['content'], type=contents_middle_right[i]['type'], order=i)
+            content.save()
+        row = Row(version=version, order=2)
+        row.save()
+        column = Column(row=row, span=12, offset=0, order=0)
+        column.save()
+        for i in range(len(contents_lower)):
+            content = Content(column=column, content=contents_lower[i]['content'], type=contents_lower[i]['type'], order=i)
+            content.save()
     return HttpResponseRedirect(reverse('admin.cms.views.version.edit', args=[version.id]))
 
 @login_required
