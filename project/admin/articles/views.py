9c762e98ddce381e93d2f8e16b37a2e55a89f460
Ali Kaafarani <ali@kvikshaug.no>
2012-04-20 09:44:29 +0200

    Swap placeholder image to a large jpg

--- project/admin/articles/views.py
+++ project/admin/articles/views.py
@@ -30,12 +30,12 @@ def new(request):
     # Save a new article based on this pre-defined template.
     contents = [
         {'type': 'html', 'content': """<h1>Fengende overskrift</h1>"""},
-        {'type': 'image', 'content': """<img src=\"""" + settings.STATIC_URL + """img/templates/placeholder.png" alt="placeholder">"""},
+        {'type': 'image', 'content': """<img src=\"""" + settings.STATIC_URL + """img/templates/placeholder.jpg" alt="placeholder">"""},
         {'type': 'html', 'content': """<p>BILDETEKST: Nunc diam velit, adipiscing ut tristique vitae, sagittis vel odio. Maecenas convallis.<br><em>Foto: Ola Nordmann/DNT</em></p>"""},
         {'type': 'html', 'content': """<div class="lede"><p>Suspendisse lectus leo, consectetur in tempor sit amet, placerat quis neque. Etiam luctus porttitor lorem, sed suscipit est rutrum non. Curabitur lobortis nisl a enim congue semper. Aenean commodo ultrices imperdiet.</p></div>"""},
         {'type': 'widget', 'content': json.dumps({'widget': 'editor', 'article': article.id})},
         {'type': 'html', 'content': """<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue. Nam tincidunt congue enim, ut porta lorem lacinia consectetur. Donec ut libero sed arcu vehicula ultricies a non tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut gravida lorem. Ut turpis felis, pulvinar a semper sed, adipiscing id dolor.</p><h2>Vivamus fermentum semper porta.</h2><p>Pellentesque auctor nisi id magna consequat sagittis. Curabitur dapibus enim sit amet elit pharetra tincidunt feugiat nisl imperdiet. Ut convallis libero in urna ultrices accumsan. Donec sed odio eros. Donec viverra mi quis quam pulvinar at malesuada arcu rhoncus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In rutrum accumsan ultricies. Mauris vitae nisi at sem facilisis semper ac in est.</p>"""},
-        {'type': 'image', 'content': """<img src=\"""" + settings.STATIC_URL + """img/templates/placeholder.png" alt="placeholder">"""},
+        {'type': 'image', 'content': """<img src=\"""" + settings.STATIC_URL + """img/templates/placeholder.jpg" alt="placeholder">"""},
         {'type': 'html', 'content': """<p>BILDETEKST: Donec ut libero sed arcu vehicula.<br><em>Foto: Kari Nordmann/DNT</em></p>"""},
     ]
 
