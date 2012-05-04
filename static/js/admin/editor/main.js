51b71a47fcf658d86e667f2fa7b4725acd024603
Ali Kaafarani <ali@kvikshaug.no>
2012-05-04 13:43:49 +0200

    Move saving logic to seperate javascript file

--- static/js/admin/editor/main.js
+++ static/js/admin/editor/main.js
@@ -494,119 +494,6 @@ $(document).ready(function() {
     });
 
     /**
-     * Saving the document
-     */
-
-    var lastSaveCount = 0;
-    var updateSaveCountID;
-    function updateSaveCount() {
-        lastSaveCount += 1;
-        if(lastSaveCount < 30) {
-            $("#toolbar span.save-text").html("<i class=\"icon-ok\"></i> Artikkelen er nylig lagret.");
-        } else if(lastSaveCount < 60) {
-            $("#toolbar span.save-text").html("<i class=\"icon-warning-sign\"></i> Sist lagret for " + lastSaveCount + " sekunder siden.");
-        } else {
-            $("#toolbar span.save-text").html("<i class=\"icon-warning-sign\"></i> Sist lagret for " + Math.floor(lastSaveCount / 60) + " minutt" + (lastSaveCount >= 120 ? 'er' : '') + " siden.");
-        }
-
-        if(lastSaveCount == 60 * 5) {
-            $("div.no-save-warning").show();
-        }
-
-        if($("#toolbar .save input.autosave:checked").length == 1) {
-            var val = $("#toolbar .save input.autosave-frequency").val();
-            if(val.match(/^\d+$/) && lastSaveCount > (val * 60)) {
-                $("#toolbar .save button.save").click();
-                return;
-            }
-        }
-        updateSaveCountID = setTimeout(updateSaveCount, 1000);
-    }
-    updateSaveCount();
-
-    // Warn when autosave-number is invalid
-    $("#toolbar .save input.autosave-frequency").keyup(function() {
-        if($(this).val().match(/^\d+$/)) {
-            $(this).parents(".control-group").removeClass('error');
-        } else {
-            $(this).siblings("input.autosave").removeAttr('checked');
-            $(this).parents(".control-group").addClass('error');
-        }
-    });
-
-    $("#toolbar .save button.save").click(function() {
-        clearInterval(updateSaveCountID);
-        $(this).hide();
-        $("div.no-save-warning").hide();
-        $("#toolbar span.save-text").text("Lagrer, vennligst vent...");
-        enableOverlay();
-        disableEditing();
-        var rows = [];
-        $("article div.row").each(function() {
-            var row = {
-                id: $(this).attr('data-id'),
-                order: $(this).prevAll().length
-            };
-            rows = rows.concat([row]);
-        });
-        var columns = [];
-        $("article div.column").each(function() {
-            var column = {
-                id: $(this).attr('data-id'),
-                order: $(this).prevAll().length
-            };
-            columns = columns.concat([column]);
-        });
-        var contents = [];
-        $("article div.html").each(function() {
-            var content = {
-                id: $(this).attr('data-id'),
-                order: $(this).prevAll().length,
-                content: $(this).html()
-            };
-            contents = contents.concat([content]);
-        });
-        $("article div.image").each(function() {
-            var anchor;
-            if($(this).find('a').length == 0) {
-                anchor = null;
-            } else {
-                anchor = $(this).find('a').attr('href');
-            }
-            var image = {
-                src: $(this).find('img').attr('src'),
-                alt: $(this).find('img').attr('alt'),
-                anchor: anchor
-            };
-            var content = {
-                id: $(this).attr('data-id'),
-                order: $(this).prevAll().length,
-                content: JSON.stringify(image)
-            };
-            contents = contents.concat([content]);
-        });
-
-        $.ajax({
-            url: '/sherpa/cms/editor/' + $("article").attr('data-id') + '/',
-            type: 'POST',
-            data: "rows=" + encodeURIComponent(JSON.stringify(rows)) +
-                  "&columns=" + encodeURIComponent(JSON.stringify(columns)) +
-                  "&contents=" + encodeURIComponent(JSON.stringify(contents))
-        }).done(function(result) {
-            lastSaveCount = 0;
-        }).fail(function(result) {
-            // Todo
-            $(document.body).html(result.responseText);
-        }).always(function(result) {
-            updateSaveCount();
-            disableOverlay();
-            $("#toolbar button.save").show();
-            enableEditing();
-        });
-    });
-
-
-    /**
      * Small, logical code snippets
      */
 
@@ -855,4 +742,8 @@ $(document).ready(function() {
         }
     }
 
+    window.enableOverlay = enableOverlay;
+    window.disableOverlay = disableOverlay;
+    window.enableEditing = enableEditing;
+    window.disableEditing = disableEditing;
 });
