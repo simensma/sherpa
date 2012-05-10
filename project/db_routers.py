24254937dfcdd86fdc31e412670741e6e40c6acd
Ali Kaafarani <ali@kvikshaug.no>
2012-05-10 17:40:57 +0200

    Add db routes for group app models

--- project/db_routers.py
+++ project/db_routers.py
@@ -1,8 +1,12 @@
 class Router(object):
     def db_for_read(self, model, **hints):
+        if model._meta.app_label == 'group':
+            return 'sherpa-2'
         return None
 
     def db_for_write(self, model, **hints):
+        if model._meta.app_label == 'group':
+            raise Exception("Don't write to this model!")
         return None
 
     def allow_syncdb(self, db, model):
