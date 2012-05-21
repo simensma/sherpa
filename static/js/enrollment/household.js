311000a5ab9b8a04b30f5dd7c971eb71217a83a7
Ali Kaafarani <ali@kvikshaug.no>
2012-05-21 13:28:38 +0200

    Remove obsolete code

--- static/js/enrollment/household.js
+++ static/js/enrollment/household.js
@@ -1,12 +1,3 @@
 $(document).ready(function() {
 
-    $("form#household div.household").hide();
-    $("form#household input[name='household']").click(function() {
-        if($(this).prop('checked')) {
-            $("form#household div.household").show();
-        } else {
-            $("form#household div.household").hide();
-        }
-    });
-
 });
