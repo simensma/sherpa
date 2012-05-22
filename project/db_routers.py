df10448731b67d417a68549cd16e4614ae8785b9
Ali Kaafarani <ali@kvikshaug.no>
2012-05-22 14:24:01 +0200

    Use object name to route Group table

--- project/db_routers.py
+++ project/db_routers.py
@@ -1,13 +1,13 @@
 class Router(object):
     def db_for_read(self, model, **hints):
-        if model._meta.app_label == 'group':
+        if model._meta.object_name == 'Group':
             return 'sherpa-2'
         elif model._meta.object_name == 'FocusUser' or model._meta.object_name == 'FocusActType':
             return 'focus'
         return None
 
     def db_for_write(self, model, **hints):
-        if model._meta.app_label == 'group':
+        if model._meta.object_name == 'Group':
             raise Exception("Don't write to this model!")
         elif model._meta.object_name == 'FocusUser' or model._meta.object_name == 'FocusActType':
             return 'focus'
