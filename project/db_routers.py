7ca019749b0dcaa6b32686b9dd23a1c16d10c191
Ali Kaafarani <ali@kvikshaug.no>
2012-05-22 14:27:49 +0200

    Add Actor and ActorAddress model from Focus

--- project/db_routers.py
+++ project/db_routers.py
@@ -2,12 +2,12 @@ class Router(object):
     def db_for_read(self, model, **hints):
         if model._meta.object_name == 'Group':
             return 'sherpa-2'
-        elif model._meta.object_name == 'FocusUser' or model._meta.object_name == 'FocusActType':
+        elif model._meta.object_name == 'FocusUser' or model._meta.object_name == 'FocusActType' or model._meta.object_name == 'Actor' or model._meta.object_name == 'ActorAddress':
             return 'focus'
         return None
 
     def db_for_write(self, model, **hints):
-        if model._meta.object_name == 'Group':
+        if model._meta.object_name == 'Group' or model._meta.object_name == 'Actor' or model._meta.object_name == 'ActorAddress':
             raise Exception("Don't write to this model!")
         elif model._meta.object_name == 'FocusUser' or model._meta.object_name == 'FocusActType':
             return 'focus'
@@ -16,7 +16,7 @@ class Router(object):
     def allow_syncdb(self, db, model):
         if model._meta.object_name == 'Group':
             return False
-        if model._meta.object_name == 'FocusUser' or model._meta.object_name == 'FocusActType':
+        if model._meta.object_name == 'FocusUser' or model._meta.object_name == 'FocusActType' or model._meta.object_name == 'Actor' or model._meta.object_name == 'ActorAddress':
             return False
         if db in ['sherpa-2', 'sherpa-2.5', 'focus']:
             return False
