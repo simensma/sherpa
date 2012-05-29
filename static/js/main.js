e7f1ebd951368b0e945230dfcb2c53bd3c04fecc
Ali Kaafarani <ali@kvikshaug.no>
2012-05-29 22:05:08 +0200

    Add language select-dropdown

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
 
+    $("nav#menus div.language-bar select").change(function() {
+        var val = $(this).children("option:selected").val();
+        if(val != '') {
+            window.location = val;
+        }
+    });
+
 });
