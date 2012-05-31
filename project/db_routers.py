6adefcf170aecceeb5e10bccf674e1423f1f382e
Ali Kaafarani <ali@kvikshaug.no>
2012-05-31 20:08:37 +0200

    Use lists for simpler comparison

--- project/db_routers.py
+++ project/db_routers.py
@@ -2,21 +2,19 @@ class Router(object):
     def db_for_read(self, model, **hints):
         if model._meta.object_name == 'Group':
             return 'sherpa-2'
-        elif model._meta.object_name == 'FocusUser' or model._meta.object_name == 'FocusActType' or model._meta.object_name == 'Actor' or model._meta.object_name == 'ActorAddress' or model._meta.object_name == 'FocusZipcode':
+        elif model._meta.object_name in ['FocusUser', 'FocusActType', 'Actor', 'ActorAddress', 'FocusZipcode']:
             return 'focus'
         return None
 
     def db_for_write(self, model, **hints):
-        if model._meta.object_name == 'Group' or model._meta.object_name == 'Actor' or model._meta.object_name == 'ActorAddress' or model._meta.object_name == 'FocusZipcode':
+        if model._meta.object_name in ['Group', 'Actor', 'ActorAddress', 'FocusZipcode']:
             raise Exception("Don't write to this model!")
-        elif model._meta.object_name == 'FocusUser' or model._meta.object_name == 'FocusActType':
+        elif model._meta.object_name in ['FocusUser', 'FocusActType']:
             return 'focus'
         return None
 
     def allow_syncdb(self, db, model):
-        if model._meta.object_name == 'Group':
-            return False
-        if model._meta.object_name == 'FocusUser' or model._meta.object_name == 'FocusActType' or model._meta.object_name == 'Actor' or model._meta.object_name == 'ActorAddress' or model._meta.object_name == 'FocusZipcode':
+        if model._meta.object_name in ['Group', 'FocusUser', 'FocusActType', 'Actor', 'ActorAddress', 'FocusZipcode']:
             return False
         if db in ['sherpa-2', 'sherpa-2.5', 'focus']:
             return False
