14fb9f754758b5831bf41a55b5bbd1d14f8053ab
Ali Kaafarani <ali@kvikshaug.no>
2012-06-01 16:14:08 +0200

    Add member-benefits listing

--- static/js/membership.js
+++ static/js/membership.js
@@ -8,9 +8,22 @@ $(document).ready(function() {
         $(this).attr('src', '/static/img/icons/glyph/original/glyphicons_195_circle_info.png');
     });
 
-    $("div#faq dl dd").hide();
-    $("div#faq dl dt").click(function() {
-        $(this).next().toggle();
+    $("div#benefits dl dd, div#faq dl dd").hide();
+    $("div#benefits dl dt, div#faq dl dt").click(function() {
+        $(this).next().toggle(400);
     });
+
+    var duration = 600;
+    $("div#benefits").hide();
+    $("div.benefits-toggle a.hider").hide();
+    $("div.benefits-toggle-buttons div.hide-button").hide();
+    $("div.benefits-toggle").click(function() {
+        $("div#benefits").toggle(duration);
+        $("div.benefits-toggle a.shower").toggle();
+        $("div.benefits-toggle a.hider").toggle();
+        $("div.benefits-toggle-buttons div.hide-button").toggle();
+        $("div.benefits-toggle-buttons div.show-button").toggle();
+    });
+
 
 });
