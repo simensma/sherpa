430e3e90c0ba86452aa3b5d7b44c9bc25b51f624
Ali Kaafarani <ali@kvikshaug.no>
2012-06-05 01:09:30 +0200

    Handle DoesNotExist when searching for zipcode/group

--- project/membership/views.py
+++ project/membership/views.py
@@ -7,8 +7,14 @@ from django.template.defaultfilters import slugify
 from group.models import Group
 from user.models import FocusZipcode, FocusPrice
 
+# Slug used for error-handling redirection
+invalid_zipcode = 'ugyldig-postnummer'
+unregistered_zipcode = 'uregistrert-postnummer'
+
 def index(request):
-    return render(request, 'membership/index.html')
+    context = {'invalid_zipcode': request.GET.get(invalid_zipcode, ''),
+        'unregistered_zipcode': request.GET.get(unregistered_zipcode, ''),}
+    return render(request, 'membership/index.html', context)
 
 def benefits(request, group):
     if group != None:
@@ -22,10 +28,15 @@ def benefits(request, group):
     return render(request, 'membership/benefits.html', context)
 
 def zipcode_search(request):
-    zipcode = FocusZipcode.objects.get(postcode=request.POST['zipcode'])
-    # Note: Redirecting requires performing the group lookup twice
-    group = Group.objects.get(focus_id=zipcode.main_group_id)
-    return HttpResponseRedirect("%s-%s/" % (reverse('membership.views.benefits', args=[group.id])[:-1], slugify(group.name)))
+    try:
+        zipcode = FocusZipcode.objects.get(postcode=request.POST['zipcode'])
+        # Note: Redirecting requires performing the group lookup twice
+        group = Group.objects.get(focus_id=zipcode.main_group_id)
+        return HttpResponseRedirect("%s-%s/" % (reverse('membership.views.benefits', args=[group.id])[:-1], slugify(group.name)))
+    except FocusZipcode.DoesNotExist:
+        return HttpResponseRedirect("%s?%s=%s" % (reverse('membership.views.index'), invalid_zipcode, request.POST['zipcode']))
+    except Group.DoesNotExist:
+        return HttpResponseRedirect("%s?%s=%s" % (reverse('membership.views.index'), unregistered_zipcode, request.POST['zipcode']))
 
 def service(request):
     return render(request, 'membership/service.html')
