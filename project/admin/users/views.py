00371f4f640af5c8390aab5fdabb161fa8f2e924
Ali Kaafarani <ali@kvikshaug.no>
2012-06-06 17:45:52 +0200

    Order user list by name

--- project/admin/users/views.py
+++ project/admin/users/views.py
@@ -4,6 +4,6 @@ from django.contrib.auth.models import User
 
 @login_required
 def index(request):
-    users = User.objects.all()
+    users = User.objects.all().order_by('first_name')
     context = {'users': users}
     return render(request, 'admin/users/index.html', context)
