bdfd478f82cf093548c002e03faf52fa6ecb88ca
Ali Kaafarani <ali@kvikshaug.no>
2012-06-07 14:39:57 +0200

    Don't process menu logid for ajax requests

--- project/context_processors.py
+++ project/context_processors.py
@@ -5,11 +5,12 @@ from page.models import Menu
 import re
 
 def menus(request):
-    menus = Menu.objects.all().order_by('order')
-    for menu in menus:
-        url = re.sub('https?:\/\/', '', menu.url) # Strip protocol
-        url = re.sub('[^\/]$', '/', url) # Add final slash if missing
-        if request.get_host() + request.path == url:
-            menu.active = True
-            break
-    return {'menus': menus}
+    if not request.is_ajax():
+        menus = Menu.objects.all().order_by('order')
+        for menu in menus:
+            url = re.sub('https?:\/\/', '', menu.url) # Strip protocol
+            url = re.sub('[^\/]$', '/', url) # Add final slash if missing
+            if request.get_host() + request.path == url:
+                menu.active = True
+                break
+        return {'menus': menus}
