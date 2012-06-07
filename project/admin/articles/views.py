bca3a595462627216b7073187c4041bb5a837b55
Ali Kaafarani <ali@kvikshaug.no>
2012-06-07 14:18:33 +0200

    Redesign article templates

--- project/admin/articles/views.py
+++ project/admin/articles/views.py
@@ -92,18 +92,13 @@ def edit_version(request, version):
     return render(request, 'admin/articles/edit_version.html', context)
 
 def create_template(template, version, title):
-    if template == '1':
-        # Empty
-        return
-    elif template == '2':
+    if template == '0':
         contents = [
             {'type': 'title', 'content': """<h1>%s</h1>""" % title},
-            {'type': 'image', 'content': json.dumps({'src': settings.STATIC_URL + "img/placeholder.jpg", 'alt': "placeholder"})},
-            {'type': 'html', 'content': """<p>BILDETEKST: Nunc diam velit, adipiscing ut tristique vitae, sagittis vel odio. Maecenas convallis.<br><em>Foto: Ola Nordmann/DNT</em></p>"""},
-            {'type': 'lede', 'content': """<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue. Nam tincidunt congue enim, ut porta lorem lacinia consectetur. Donec ut libero sed arcu vehicula ultricies a non tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut gravida lorem. Ut turpis felis, pulvinar a semper sed, adipiscing id dolor. Pellentesque auctor nisi id magna consequat sagittis. Curabitur dapibus enim sit amet elit pharetra tincidunt feugiat nisl imperdiet. Ut convallis libero in urna ultrices accumsan. Donec sed odio eros. Donec viverra mi quis quam pulvinar at malesuada arcu rhoncus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In rutrum accumsan ultricies. Mauris vitae nisi at sem facilisis semper ac in est.</p>"""},
-            {'type': 'html', 'content': """<h2>Mindre overskrift</h2><p>Vivamus fermentum semper porta. Nunc diam velit, adipiscing ut tristique vitae, sagittis vel odio. Maecenas convallis ullamcorper ultricies. Curabitur ornare, ligula semper consectetur sagittis, nisi diam iaculis velit, id fringilla sem nunc vel mi. Nam dictum, odio nec pretium volutpat, arcu ante placerat erat, non tristique elit urna et turpis. Quisque mi metus, ornare sit amet fermentum et, tincidunt et orci. Fusce eget orci a orci congue vestibulum. Ut dolor diam, elementum et vestibulum eu, porttitor vel elit. Curabitur venenatis pulvinar tellus gravida ornare. Sed et erat faucibus nunc euismod ultricies ut id justo. Nullam cursus suscipit nisi, et ultrices justo sodales nec. Fusce venenatis facilisis lectus ac semper. Aliquam at massa ipsum. Quisque bibendum purus convallis nulla ultrices ultricies. Nullam aliquam, mi eu aliquam tincidunt, purus velit laoreet tortor, viverra pretium nisi quam vitae mi. Fusce vel volutpat elit. Nam sagittis nisi dui.</p>"""},
-            {'type': 'image', 'content': json.dumps({'src': settings.STATIC_URL + "img/placeholder.jpg", 'alt': "placeholder"})},
-            {'type': 'html', 'content': """<p>BILDETEKST: Donec ut libero sed arcu vehicula.<br><em>Foto: Kari Nordmann/DNT</em></p>"""},
+            {'type': 'image', 'content': json.dumps({'src': settings.STATIC_URL + "img/placeholder.jpg", "description": "", "photographer": "", "anchor": None})},
+            {'type': 'lede', 'content': """<p class="lede"><br></p>"""},
+            {'type': 'html', 'content': """<p><br></p>"""},
+            {'type': 'image', 'content': json.dumps({'src': settings.STATIC_URL + "img/placeholder.jpg", "description": "", "photographer": "", "anchor": None})},
         ]
         row = Row(version=version, order=0)
         row.save()
@@ -112,20 +107,18 @@ def create_template(template, version, title):
         for i in range(len(contents)):
             content = Content(column=column, content=contents[i]['content'], type=contents[i]['type'], order=i)
             content.save()
-    elif template == '3':
+    elif template == '1':
         contents_upper = [
             {'type': 'title', 'content': """<h1>%s</h1>""" % title},
-            {'type': 'image', 'content': json.dumps({'src': settings.STATIC_URL + "img/placeholder.jpg", 'alt': "placeholder"})},
-            {'type': 'html', 'content': """<p>BILDETEKST: Nunc diam velit, adipiscing ut tristique vitae, sagittis vel odio. Maecenas convallis.<br><em>Foto: Ola Nordmann/DNT</em></p>"""},
+            {'type': 'image', 'content': json.dumps({'src': settings.STATIC_URL + "img/placeholder.jpg", "description": "", "photographer": "", "anchor": None})},
+            {'type': 'lede', 'content': """<p class="lede"><br></p>"""},
         ]
         contents_lower_left = [
-            {'type': 'lede', 'content': """<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue. Nam tincidunt congue enim, ut porta lorem lacinia consectetur. Donec ut libero sed arcu vehicula ultricies a non tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut gravida lorem. Ut turpis felis, pulvinar a semper sed, adipiscing id dolor. Pellentesque auctor nisi id magna consequat sagittis. Curabitur dapibus enim sit amet elit pharetra tincidunt feugiat nisl imperdiet. Ut convallis libero in urna ultrices accumsan. Donec sed odio eros. Donec viverra mi quis quam pulvinar at malesuada arcu rhoncus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In rutrum accumsan ultricies. Mauris vitae nisi at sem facilisis semper ac in est.</p>"""},
-            {'type': 'html', 'content': """<h2>Mindre overskrift</h2><p>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In euismod ultrices facilisis. Vestibulum porta sapien adipiscing augue congue id pretium lectus molestie. Proin quis dictum nisl. Morbi id quam sapien, sed vestibulum sem. Duis elementum rutrum mauris sed convallis. Proin vestibulum magna mi. Aenean tristique hendrerit magna, ac facilisis nulla hendrerit ut. Sed non tortor sodales quam auctor elementum. Donec hendrerit nunc eget elit pharetra pulvinar. Suspendisse id tempus tortor. Aenean luctus, elit commodo laoreet commodo, justo nisi consequat massa, sed vulputate quam urna quis eros. Donec vel.</p>"""},
+            {'type': 'html', 'content': """<p><br></p>"""},
         ]
         contents_lower_right = [
-            {'type': 'html', 'content': """<p>Suspendisse lectus leo, consectetur in tempor sit amet, placerat quis neque. Etiam luctus porttitor lorem, sed suscipit est rutrum non. Curabitur lobortis nisl a enim congue semper. Aenean commodo ultrices imperdiet. Vestibulum ut justo vel sapien venenatis tincidunt. Phasellus eget dolor sit amet ipsum dapibus condimentum vitae quis lectus.</p>"""},
-            {'type': 'image', 'content': json.dumps({'src': settings.STATIC_URL + "img/placeholder.jpg", 'alt': "placeholder"})},
-            {'type': 'html', 'content': """<p>BILDETEKST: Donec ut libero sed arcu vehicula.<br><em>Foto: Kari Nordmann/DNT</em></p>"""},
+            {'type': 'html', 'content': """<p><br></p>"""},
+            {'type': 'image', 'content': json.dumps({'src': settings.STATIC_URL + "img/placeholder.jpg", "description": "", "photographer": "", "anchor": None})},
         ]
         row = Row(version=version, order=0)
         row.save()
@@ -145,54 +138,4 @@ def create_template(template, version, title):
         column.save()
         for i in range(len(contents_lower_right)):
             content = Content(column=column, content=contents_lower_right[i]['content'], type=contents_lower_right[i]['type'], order=i)
-            content.save()
-    elif template == '4':
-        contents_upper = [
-            {'type': 'title', 'content': """<h1>%s</h1>""" % title},
-            {'type': 'image', 'content': json.dumps({'src': settings.STATIC_URL + "img/placeholder.jpg", 'alt': "placeholder"})},
-            {'type': 'html', 'content': """<p>BILDETEKST: Nunc diam velit, adipiscing ut tristique vitae, sagittis vel odio. Maecenas convallis.<br><em>Foto: Ola Nordmann/DNT</em></p>"""},
-        ]
-        contents_middle_left = [
-            {'type': 'lede', 'content': """<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue. Nam tincidunt congue enim, ut porta lorem lacinia consectetur. Donec ut libero sed arcu vehicula ultricies a non tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>"""},
-        ]
-        contents_middle_center = [
-            {'type': 'html', 'content': """<p>Vivamus fermentum semper porta. Nunc diam velit, adipiscing ut tristique vitae, sagittis vel odio. Maecenas convallis ullamcorper ultricies. Curabitur ornare, ligula semper consectetur sagittis, nisi diam iaculis velit, id fringilla sem nunc vel mi. Nam dictum, odio nec pretium volutpat, arcu ante placerat erat, non tristique elit urna et turpis. Quisque mi metus, ornare sit amet fermentum et, tincidunt et orci. Fusce eget orci a orci congue vestibulum.</p>"""},
-        ]
-        contents_middle_right = [
-            {'type': 'html', 'content': """<p>Vivamus fermentum semper porta. Nunc diam velit, adipiscing ut tristique vitae, sagittis vel odio. Maecenas convallis ullamcorper ultricies. Curabitur ornare, ligula semper consectetur sagittis, nisi diam iaculis velit, id fringilla sem nunc vel mi. Nam dictum, odio nec pretium volutpat, arcu ante placerat erat, non tristique elit urna et turpis. Quisque mi metus, ornare sit amet fermentum et, tincidunt et orci. Fusce eget orci a orci congue vestibulum.</p>"""},
-        ]
-        contents_lower = [
-            {'type': 'image', 'content': json.dumps({'src': settings.STATIC_URL + "img/placeholder.jpg", 'alt': "placeholder"})},
-            {'type': 'html', 'content': """<p>BILDETEKST: Donec ut libero sed arcu vehicula.<br><em>Foto: Kari Nordmann/DNT</em></p>"""},
-        ]
-        row = Row(version=version, order=0)
-        row.save()
-        column = Column(row=row, span=12, offset=0, order=0)
-        column.save()
-        for i in range(len(contents_upper)):
-            content = Content(column=column, content=contents_upper[i]['content'], type=contents_upper[i]['type'], order=i)
-            content.save()
-        row = Row(version=version, order=1)
-        row.save()
-        column = Column(row=row, span=4, offset=0, order=0)
-        column.save()
-        for i in range(len(contents_middle_left)):
-            content = Content(column=column, content=contents_middle_left[i]['content'], type=contents_middle_left[i]['type'], order=i)
-            content.save()
-        column = Column(row=row, span=4, offset=0, order=1)
-        column.save()
-        for i in range(len(contents_middle_center)):
-            content = Content(column=column, content=contents_middle_center[i]['content'], type=contents_middle_center[i]['type'], order=i)
-            content.save()
-        column = Column(row=row, span=4, offset=0, order=2)
-        column.save()
-        for i in range(len(contents_middle_right)):
-            content = Content(column=column, content=contents_middle_right[i]['content'], type=contents_middle_right[i]['type'], order=i)
-            content.save()
-        row = Row(version=version, order=2)
-        row.save()
-        column = Column(row=row, span=12, offset=0, order=0)
-        column.save()
-        for i in range(len(contents_lower)):
-            content = Content(column=column, content=contents_lower[i]['content'], type=contents_lower[i]['type'], order=i)
             content.save()
