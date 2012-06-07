3a7adaa1a2c50791887f73b444d2b2a4a78ca342
Ali Kaafarani <ali@kvikshaug.no>
2012-06-07 13:58:28 +0200

    Add custom gravatar template tag

---
+++ project/articles/templatetags/gravatar.py
@@ -0,0 +1,13 @@
+from django import template
+
+import md5
+
+register = template.Library()
+
+GRAVATAR_URL = "http://www.gravatar.com/avatar"
+
+def gravatar(email, size=60, default='mm'):
+    hash = md5.new(email.strip().lower()).hexdigest()
+    return "%s/%s?s=%s&d=%s" % (GRAVATAR_URL, hash, size, default)
+
+register.simple_tag(gravatar)
