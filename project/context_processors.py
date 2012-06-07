0adf5c8e93c9c4bb2e42e8cbd47dcad73b1ebe85
Ali Kaafarani <ali@kvikshaug.no>
2012-06-07 14:50:55 +0200

    Fix final-slash regex bug

--- project/context_processors.py
+++ project/context_processors.py
@@ -9,7 +9,9 @@ def menus(request):
         menus = Menu.objects.all().order_by('order')
         for menu in menus:
             url = re.sub('https?:\/\/', '', menu.url) # Strip protocol
-            url = re.sub('[^\/]$', '/', url) # Add final slash if missing
+            # Add final slash if missing
+            if url[-1] != '/':
+                url = "%s/" % url
             if request.get_host() + request.path == url:
                 menu.active = True
                 break
