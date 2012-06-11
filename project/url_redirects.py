c2ac94e1a3cccd8fc2e64886a1e9db9bed0bf695
Ali Kaafarani <ali@kvikshaug.no>
2012-06-11 12:59:38 +0200

    Use consistent quote signs

--- project/url_redirects.py
+++ project/url_redirects.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('',
-    url(r'^images/(?P<slug>.*)', 'page.views.redirect', kwargs={'url': "http://www.turistforeningen.no/images/"}),
-    url(r'^album/(?P<slug>.*)', 'page.views.redirect', kwargs={'url': "http://www.turistforeningen.no/album/"}),
+    url(r'^images/(?P<slug>.*)', 'page.views.redirect', kwargs={'url': 'http://www.turistforeningen.no/images/'}),
+    url(r'^album/(?P<slug>.*)', 'page.views.redirect', kwargs={'url': 'http://www.turistforeningen.no/album/'}),
 )
