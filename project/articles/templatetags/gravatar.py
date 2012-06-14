3961367363da080eb5cb7409348f5f5a9d9ff9a0
Ali Kaafarani <ali@kvikshaug.no>
2012-06-14 23:12:31 +0200

    Redesign byline

--- project/articles/templatetags/gravatar.py
+++ project/articles/templatetags/gravatar.py
@@ -6,7 +6,7 @@ register = template.Library()
 
 GRAVATAR_URL = "http://www.gravatar.com/avatar"
 
-def gravatar(email, size=60, default='mm'):
+def gravatar(email, size=40, default='mm'):
     hash = md5.new(email.strip().lower()).hexdigest()
     return "%s/%s?s=%s&d=%s" % (GRAVATAR_URL, hash, size, default)
 
