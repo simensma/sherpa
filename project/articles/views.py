f04304f6575b802306ad2b309612c5be5eceeb1d
Ali Kaafarani <ali@kvikshaug.no>
2012-06-15 10:40:37 +0200

    Add caching for article display

--- project/articles/views.py
+++ project/articles/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.cache import cache
 
 from articles.models import Article
 from page.models import AdPlacement, Variant, Version, Row, Column, Content
@@ -18,22 +19,26 @@ def index(request):
     return render(request, "page/articles-list.html", context)
 
 def show(request, article, text):
-    # Assume no segmentation for now
-    article = Article.objects.get(id=article)
-    variant = Variant.objects.get(article=article, segment=None)
-    version = Version.objects.get(variant=variant, active=True)
-    version.load_preview()
-    rows = Row.objects.filter(version=version).order_by('order')
-    for row in rows:
-        columns = Column.objects.filter(row=row).order_by('order')
-        for column in columns:
-            contents = Content.objects.filter(column=column).order_by('order')
-            for content in contents:
-                if content.type == 'widget':
-                    content.content = parse_widget(json.loads(content.content))
-                elif content.type == 'image':
-                    content.content = json.loads(content.content)
-            column.contents = contents
-        row.columns = columns
-    context = {'rows': rows, 'version': version, 'ad': AdPlacement.get_active_ad('articles')}
+    context = cache.get('articles.%s' % article)
+    if context == None:
+        # Assume no segmentation for now
+        article = Article.objects.get(id=article)
+        variant = Variant.objects.get(article=article, segment=None)
+        version = Version.objects.get(variant=variant, active=True)
+        version.load_preview()
+        rows = Row.objects.filter(version=version).order_by('order')
+        for row in rows:
+            columns = Column.objects.filter(row=row).order_by('order')
+            for column in columns:
+                contents = Content.objects.filter(column=column).order_by('order')
+                for content in contents:
+                    if content.type == 'widget':
+                        content.content = parse_widget(json.loads(content.content))
+                    elif content.type == 'image':
+                        content.content = json.loads(content.content)
+                column.contents = contents
+            row.columns = columns
+        context = {'rows': rows, 'version': version}
+        cache.set('articles.%s' % article.id, context, 60 * 60 * 6)
+    context['ad'] = AdPlacement.get_active_ad('articles')
     return render(request, "page/article.html", context)
