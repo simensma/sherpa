5ad4777645b616eabbbe6c5688c21bc7fe614e1d
Ali Kaafarani <ali@kvikshaug.no>
2012-06-25 17:07:23 +0200

    Remove uncompleted comment

--- project/user/models.py
+++ project/user/models.py
@@ -5,7 +5,6 @@ from django.conf import settings
 class Profile(models.Model):
     user = models.OneToOneField(User)
     phone = models.CharField(max_length=20)
-    # The length h
     password_restore_key = models.CharField(max_length=settings.RESTORE_PASSWORD_KEY_LENGTH, null=True, unique=True)
     password_restore_date = models.DateTimeField(null=True)
     # At some point, this model will be extended to contain member data, syncing with Focus.
