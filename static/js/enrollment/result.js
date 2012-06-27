25ca4ee813f09abf483e15ac2fb0ee7b86331998
Ali Kaafarani <ali@kvikshaug.no>
2012-06-27 11:13:28 +0200

    Enhance SMS reciept error message text

--- static/js/enrollment/result.js
+++ static/js/enrollment/result.js
@@ -11,20 +11,21 @@ $(document).ready(function() {
             data: 'index=' + encodeURIComponent(index)
         }).done(function(result) {
             result = JSON.parse(result);
+            var memberserviceBackup = 'Dersom du/dere har planlagt å dra på tur i nærmeste fremtid, og ikke rekker å vente på at medlemskortet ankommer, kan dere kontakte medlemsservice for å få tilsendt kvittering på SMS.';
             if(result.error == 'none') {
                 button.after('<p class="receipt-success">Kvittering har blitt sendt på SMS til ' + number + '.</p>');
                 button.remove();
             } else if(result.error == 'foreign_number') {
-                button.after('<p class="receipt-error">En teknisk feil har oppstått ved utsendelse av SMS. Vennligst kontakt medlemsservice for å motta SMS-kvittering.</p>');
+                button.after('<p class="receipt-error">En teknisk feil har oppstått ved utsendelse av SMS. ' + memberserviceBackup + '</p>');
                 button.remove();
             } else if(result.error == 'not_registered') {
-                button.after('<p class="receipt-error">En teknisk feil har oppstått ved utsendelse av SMS. Vennligst kontakt medlemsservice for å motta SMS-kvittering.</p>');
+                button.after('<p class="receipt-error">En teknisk feil har oppstått ved utsendelse av SMS. ' + memberserviceBackup + '</p>');
                 button.remove();
             } else if(result.error == 'already_sent') {
                 button.after('<p class="receipt-error">SMS-Kvittering har allerede blitt sendt til dette nummeret. Ta kontakt med medlemsservice dersom du mener dette er feil.</p>');
                 button.remove();
             } else if(result.error == 'service_fail') {
-                button.after('<p class="receipt-error">En teknisk feil har oppstått ved kontakt med vår SMS-leverandør. Feilmelding oppgitt er:</p><pre>' + result.message + '</pre><p class="receipt-error">Vennligst kontakt medlemsservice for å motta SMS-kvittering.</p>');
+                button.after('<p class="receipt-error">En teknisk feil har oppstått ved kontakt med vår SMS-leverandør. Feilmelding oppgitt er:</p><pre>' + result.message + '</pre><p class="receipt-error">' + memberserviceBackup + '</p>');
                 button.remove();
             }
         }).fail(function(result) {
