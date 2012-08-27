622f8746a2234ca71e5b167330d25eba9ca57907
Ali Kaafarani <ali@kvikshaug.no>
2012-08-27 14:29:38 +0200

    Add UI for handling tags in article editor

--- static/js/admin/editor/article.js
+++ static/js/admin/editor/article.js
@@ -23,6 +23,49 @@ $(document).ready(function() {
         });
     });
 
+    /* Tags */
+
+    // Create the tagger object, make it globally accessible (save.js will use this)
+    window.tagger = new Tagger($("div.editor-header div.tags input[name='tags']"), function(tag) {
+        // New tag added
+        var tag = $('<div class="tag">' + tag + ' <a href="javascript:undefined"><img src="/static/img/so/close-default.png"></a></div>');
+        $("div.tag-box").append(tag);
+    }, function(tag) {
+        // Existing tag
+        $("div.tag-box div.tag").each(function() {
+            if($(this).text().trim().toLowerCase() == tag.toLowerCase()) {
+                var item = $(this);
+                var c = item.css('color');
+                var bg = item.css('background-color');
+                item.css('color', 'white');
+                item.css('background-color', 'red');
+                setTimeout(function() {
+                    item.css('color', c);
+                    item.css('background-color', bg);
+                }, 1000);
+            }
+        });
+    });
+
+    // Collect existing tags based on the DOM and layout
+    var tags = [];
+    $("div.editor-header div.tags div.tag").each(function() {
+        tags.push($(this).text().trim());
+    });
+    tagger.tags = tags;
+
+    // Add events to the tag remover button
+    $(document).on('mouseover', 'div.editor-header div.tags div.tag-box div.tag a', function() {
+        $(this).children("img").attr('src', '/static/img/so/close-hover.png');
+    });
+    $(document).on('mouseout', 'div.editor-header div.tags div.tag-box div.tag a', function() {
+        $(this).children("img").attr('src', '/static/img/so/close-default.png');
+    });
+    $(document).on('click', 'div.editor-header div.tags div.tag-box div.tag a', function() {
+        tagger.removeTag($(this).parent().text().trim());
+        $(this).parent().remove();
+    });
+
     /* Change thumbnail-image */
     if($("div.editor-header input[name='thumbnail'][value='default'][checked]").length > 0 ||
         $("div.editor-header input[name='thumbnail'][value='none'][checked]").length > 0) {
