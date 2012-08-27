77bce936041dfb3d0d4d585fb2a45706165829c8
Ali Kaafarani <ali@kvikshaug.no>
2012-08-27 20:09:07 +0200

    Use more specific selectors for page header meta

--- static/js/admin/editor/page.js
+++ static/js/admin/editor/page.js
@@ -2,31 +2,31 @@ $(document).ready(function() {
 
     /* Publish/unpublish */
 
-    if($("div.status[data-published]").length == 0) {
-        $("div.status button.unpublish").hide();
+    if($("div.editor-header.page div.status[data-published]").length == 0) {
+        $("div.editor-header.page div.status button.unpublish").hide();
     } else {
-        $("div.status button.publish").hide();
+        $("div.editor-header.page div.status button.publish").hide();
     }
 
-    $("div.status button.publish").click(function() {
+    $("div.editor-header.page div.status button.publish").click(function() {
         if(!confirm("Er du sikker på at du vil publisere denne siden?")) {
             return;
         }
         setPublished(true, function() {
-            $("div.status h1.publish span.false").removeClass('false').addClass('true').text('publisert');
-            $("div.status button.publish").hide();
-            $("div.status button.unpublish").show();
+            $("div.editor-header.page div.status h1.publish span.false").removeClass('false').addClass('true').text('publisert');
+            $("div.editor-header.page div.status button.publish").hide();
+            $("div.editor-header.page div.status button.unpublish").show();
         });
     });
 
-    $("div.status button.unpublish").click(function() {
+    $("div.editor-header.page div.status button.unpublish").click(function() {
         if(!confirm("Er du HELT sikker på at du vil trekke tilbake denne siden? Den vil forsvinne fra nyhetsutlistingen, og ikke dukke opp som søkeresultat når en søker.")) {
             return;
         }
         setPublished(false, function() {
-            $("div.status h1.publish span.true").removeClass('true').addClass('false').text('ikke publisert');
-            $("div.status button.publish").show();
-            $("div.status button.unpublish").hide();
+            $("div.editor-header.page div.status h1.publish span.true").removeClass('true').addClass('false').text('ikke publisert');
+            $("div.editor-header.page div.status button.publish").show();
+            $("div.editor-header.page div.status button.unpublish").hide();
         });
     });
 
@@ -35,30 +35,31 @@ $(document).ready(function() {
     }
 
     /* Delete page */
-    $("a.delete-page").click(function(e) {
+    $("div.editor-header.page a.delete-page").click(function(e) {
         if(!confirm("Er du HELT sikker på at du vil slette denne siden, for alltid?\n\nHvis du bare vil ta den bort fra forsiden og søkeresultater, men beholde innholdet, bør du heller bare avpublisere den.\n\nDette kan du ikke angre!")) {
             e.preventDefault();
         }
     });
 
     /* Rename title */
-    $("input[name='title']").focusout(function() {
+    $("div.editor-header.page input[name='title']").focusout(function() {
         var title = $(this).val();
-        $("span.title-status").hide();
+        $("div.editor-header.page span.title-status").hide();
         $(this).after('<img class="title-loader" src="/static/img/ajax-loader-small.gif" alt="Laster...">');
+        debugger;
         $.ajax({
             url: '/sherpa/cms/side/tittel/' + $("div.editor-header.page").attr('data-id') + '/',
             data: 'title=' + encodeURIComponent(title)
         }).always(function() {
-            $("img.title-loader").remove();
-            $("span.title-status").show();
+            $("div.editor-header.page img.title-loader").remove();
+            $("div.editor-header.page span.title-status").show();
         });
     });
 
     /* Change parent */
-    $("select[name='parent']").change(function() {
+    $("div.editor-header.page select[name='parent']").change(function() {
         var parent = $(this).find("option:selected").val();
-        $("span.parent-status").hide();
+        $("div.editor-header.page span.parent-status").hide();
         $(this).after('<img class="parent-loader" src="/static/img/ajax-loader-small.gif" alt="Laster...">');
         $.ajax({
             url: '/sherpa/cms/side/foreldre/' + $("div.editor-header.page").attr('data-id') + '/',
@@ -67,14 +68,14 @@ $(document).ready(function() {
             result = JSON.parse(result);
             if(result.error == 'parent_in_parent') {
                 alert('Du kan ikke velge den siden, fordi den allerede er en underside av denne siden.');
-                $("select[name='parent']").val($("select[name='parent'] option.default").val());
+                $("div.editor-header.page select[name='parent']").val($("div.editor-header.page select[name='parent'] option.default").val());
             } else {
-                $("select[name='parent'] option.default").removeClass('default');
-                $("select[name='parent'] option:selected").addClass('default');
+                $("div.editor-header.page select[name='parent'] option.default").removeClass('default');
+                $("div.editor-header.page select[name='parent'] option:selected").addClass('default');
             }
         }).always(function() {
-            $("img.parent-loader").remove();
-            $("span.parent-status").show();
+            $("div.editor-header.page img.parent-loader").remove();
+            $("div.editor-header.page span.parent-status").show();
         });
     });
 
