c2e31259ac5e9bc41dd3d98a89ce1e04d8dcefff
Ali Kaafarani <ali@kvikshaug.no>
2012-09-07 10:17:35 +0200

    Remove basic analytics for temporary empty page

--- project/admin/analytics/urls.py
+++ project/admin/analytics/urls.py
@@ -1,7 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('admin.analytics.views',
-    # Analytics
-    url(r'^$', 'visitors_list'),
-    url(r'^sidevisninger/(?P<visitor>\d+)/$', 'requests_list'),
+    url(r'^$', 'index'),
 )
