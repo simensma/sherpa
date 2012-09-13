19b09ba1ea98fb399c8e918f1539cc01a50f0751
Ali Kaafarani <ali@kvikshaug.no>
2012-09-13 15:07:15 +0200

    Add preliminary gift-form

--- static/js/enrollment/gift.js
+++ static/js/enrollment/gift.js
@@ -3,6 +3,8 @@ $(document).ready(function() {
     $("div.gift-choice").click(function() {
         $("div.gift-choice").removeClass('active');
         $(this).addClass('active');
+        $("p.type-display span").hide();
+        $("p.type-display span." + $(this).attr('data-name')).show();
         $("form#gift").show();
     });
 });
