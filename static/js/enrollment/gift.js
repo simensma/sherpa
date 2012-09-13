608c693ef689efab1809a9a6df1ef1f696db4a8b
Ali Kaafarani <ali@kvikshaug.no>
2012-09-13 17:43:45 +0200

    Load location for zipcode automatically

--- static/js/enrollment/gift.js
+++ static/js/enrollment/gift.js
@@ -7,4 +7,31 @@ $(document).ready(function() {
         $("p.type-display span." + $(this).attr('data-name')).show();
         $("form#gift").show();
     });
+
+    $("form#gift input.zipcode").keyup(function() {
+        var self = $(this);
+        if(self.val().match(/^\d{4}$/)) {
+            self.siblings("img.ajaxloader").show();
+            $.ajaxQueue({
+                url: '/postnummer/' + encodeURIComponent(self.val()) + '/',
+                type: 'POST'
+            }).done(function(result) {
+                result = JSON.parse(result);
+                if(result.location != undefined) {
+                    self.siblings("input.city").val(result.location);
+                    self.parents("div.control-group.zipcode").removeClass('error').addClass('success');
+                } else if(result.error == "does_not_exist") {
+                    self.siblings("input.city").val("Ukjent postnummer");
+                    self.parents("div.control-group.zipcode").removeClass('success').addClass('error');
+                }
+            }).fail(function(result) {
+                self.siblings("input.city").val("Teknisk feil");
+                self.parents("div.control-group.zipcode").removeClass('success').addClass('error');
+            }).always(function(result) {
+                self.siblings("img.ajaxloader").hide();
+            });
+        } else {
+            self.siblings("input.city").val("");
+        }
+    });
 });
