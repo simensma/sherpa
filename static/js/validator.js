b25fae782e72678733c297611e06a8bbdeed6e6d
Ali Kaafarani <ali@kvikshaug.no>
2012-09-14 16:33:51 +0200

    Don't use start/end terminators for address

--- static/js/validator.js
+++ static/js/validator.js
@@ -7,7 +7,7 @@ var Validator = function() {
         },
         'address': function(input, req, opts) {
             if(!req && input == '') { return true; }
-            return input.match(/^[^\s]+$/) != null;
+            return input.match(/[^\s]/) != null;
         },
         'zipcode': function(input, req, opts) {
             if(!req && input == '') { return true; }
