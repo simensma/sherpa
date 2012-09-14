1ff1fc679abac599c543a421db793580f70eb6d9
Ali Kaafarani <ali@kvikshaug.no>
2012-09-14 16:25:57 +0200

    Use select to choose gift type instead of headers

--- static/js/enrollment/gift.js
+++ static/js/enrollment/gift.js
@@ -1,12 +1,5 @@
 $(document).ready(function() {
-    $("form#gift").hide();
-    $("div.gift-choice").click(function() {
-        $("div.gift-choice").removeClass('active');
-        $(this).addClass('active');
-        $("p.type-display span").hide();
-        $("p.type-display span." + $(this).attr('data-name')).show();
-        $("form#gift").show();
-    });
+    $("select[name='receiver_type']").chosen({disable_search: true});
 
     $("form#gift input.zipcode").keyup(function() {
         var self = $(this);
