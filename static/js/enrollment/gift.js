a10e0eb9068362f2870f7f5bd153dd7567c035d9
Ali Kaafarani <ali@kvikshaug.no>
2012-09-17 11:23:55 +0200

    Allow multiple receivers

--- static/js/enrollment/gift.js
+++ static/js/enrollment/gift.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
-    $("select[name='receiver_type']").chosen({disable_search: true});
+    $("select.skel[name='receiver_type']").clone().show().appendTo($("form#gift div.receiver_type")).chosen({disable_search: true});
 
-    $("form#gift input.zipcode").keyup(function() {
+    $(document).on('keyup', 'form#gift input.zipcode', function() {
         var self = $(this);
         if(self.val().match(/^\d{4}$/)) {
             self.siblings("img.ajaxloader").show();
@@ -25,32 +25,12 @@ $(document).ready(function() {
             });
         } else {
             self.siblings("input.city").val("");
+            self.parents("div.control-group").removeClass('error warning success');
         }
     });
 
-    $("form#gift input[name='receiver_dob']").datepicker({
-        changeMonth: true,
-        changeYear: true,
-        firstDay: 1,
-        yearRange: "1900:c",
-        dateFormat: 'dd.mm.yy',
-        dayNames: ['Søndag', 'Mandag', 'Tirsdag', 'Onsdag', 'Torsdag', 'Fredag', 'Lørdag'],
-        dayNamesShort: ['Søn', 'Man', 'Tir', 'Ons', 'Tor', 'Fre', 'Lør'],
-        dayNamesMin: ['Sø', 'Ma', 'Ti', 'On', 'To', 'Fr', 'Lø'],
-        monthNames: ['Januar', 'Februar', 'Mars', 'April', 'Mai', 'Juni', 'Juli', 'August',
-          'September', 'Oktober', 'November', 'Desember'],
-        monthNamesShort: ['Jan', 'Feb', 'Mar', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt',
-          'Nov', 'Des'],
-        defaultDate: '-15y',
-        showOn: 'both',
-        buttonImage: '/static/img/calendar.png',
-        buttonImageOnly: true,
-        buttonText: 'Velg dato...',
-        onClose: validateDatepicker
-    });
-
     // Clear input validation-status upon focus
-    $("form#gift input").focus(function() {
+    $(document).on('focus', "form#gift input", function() {
         $(this).parents("div.control-group").removeClass('error warning success');
     });
 
@@ -79,15 +59,64 @@ $(document).ready(function() {
     validator.addValidation('phone', $("form#gift input[name='giver_phone']"), markInput, false);
     validator.addValidation('email', $("form#gift input[name='giver_email']"), markInput, false);
 
-    validator.addValidation('full_name', $("form#gift input[name='receiver_name']"), markInput, true);
-    validator.addValidation('address', $("form#gift input[name='receiver_address']"), markInput, true);
-    validator.addValidation('zipcode', $("form#gift input[name='receiver_zipcode']"), markInputZipcode, true);
-    validator.addValidation('phone', $("form#gift input[name='receiver_phone']"), markInput, false);
-    validator.addValidation('email', $("form#gift input[name='receiver_email']"), markInput, false);
+    addReceiverValidations($("form#gift div.receiver-box"));
+
+    function addReceiverValidations(box) {
+        validator.addValidation('full_name', box.find("input[name='receiver_name']"), markInput, true);
+        validator.addValidation('address', box.find("input[name='receiver_address']"), markInput, true);
+        validator.addValidation('zipcode', box.find("input[name='receiver_zipcode']"), markInputZipcode, true);
+        validator.addValidation('phone', box.find("input[name='receiver_phone']"), markInput, false);
+        validator.addValidation('email', box.find("input[name='receiver_email']"), markInput, false);
+
+        box.find("input[name='receiver_dob']").datepicker({
+            changeMonth: true,
+            changeYear: true,
+            firstDay: 1,
+            yearRange: "1900:c",
+            dateFormat: 'dd.mm.yy',
+            dayNames: ['Søndag', 'Mandag', 'Tirsdag', 'Onsdag', 'Torsdag', 'Fredag', 'Lørdag'],
+            dayNamesShort: ['Søn', 'Man', 'Tir', 'Ons', 'Tor', 'Fre', 'Lør'],
+            dayNamesMin: ['Sø', 'Ma', 'Ti', 'On', 'To', 'Fr', 'Lø'],
+            monthNames: ['Januar', 'Februar', 'Mars', 'April', 'Mai', 'Juni', 'Juli', 'August',
+              'September', 'Oktober', 'November', 'Desember'],
+            monthNamesShort: ['Jan', 'Feb', 'Mar', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt',
+              'Nov', 'Des'],
+            defaultDate: '-15y',
+            showOn: 'both',
+            buttonImage: '/static/img/calendar.png',
+            buttonImageOnly: true,
+            buttonText: 'Velg dato...',
+            onClose: validateDatepicker
+        });
+    }
 
     function validateDatepicker() {
         // Datepicker calls this on close
         markInput($(this), validator.validate('date', $("form#gift input[name='receiver_dob']").val(), true));
     }
+
+    // Add more receivers
+    $("form#gift button.new-receiver").click(function(e) {
+        e.preventDefault();
+        var list = $("div.receiver-container div.receiver-box");
+        var clone = list.first().clone();
+        var type = clone.find("div.receiver_type").empty();
+        var select = $("select.skel[name='receiver_type']").clone().removeClass('skel').show().appendTo(type);
+        var button = $('<button class="btn btn-danger remove-receiver"><i class="icon-remove"></i> Ta bort denne mottakeren</button>');
+        button.click(function(e) {
+            e.preventDefault();
+            $(this).parents('div.receiver-box').slideUp(function() { $(this).remove(); });
+        });
+        clone.append(button);
+        clone.find("div.control-group").removeClass('error warning success').focus(function() {
+            $(this).parents("div.control-group").removeClass('error warning success');
+        });
+        clone.find("input").val("");
+        addReceiverValidations(clone);
+        clone.addClass('hide');
+        list.last().after(clone);
+        select.chosen({disable_search: true});
+        clone.slideDown();
+    });
 
 });
