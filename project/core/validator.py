84f380ff81793c0c0ecd59c179e23e4c7da10193
Ali Kaafarani <ali@kvikshaug.no>
2012-09-18 10:42:42 +0200

    Add more serverside validation methods

--- project/core/validator.py
+++ project/core/validator.py
@@ -5,6 +5,16 @@ def name(name, req=True):
         return True
     return len(re.findall('.+\s.+', name)) > 0
 
+def address(address, req=True):
+    if not req and address == '':
+        return True
+    return len(re.findall('[^\s]', address)) > 0
+
+def zipcode(zipcode, req=True):
+    if not req and zipcode == '':
+        return True
+    return len(re.findall('\d{4}', zipcode)) > 0
+
 def phone(phone, req=True):
     if not req and phone == '':
         return True
@@ -16,3 +26,8 @@ def email(email, req=True):
         return True
     # Email matches anything@anything.anything, without whitespace
     return len(re.findall('^[^\s]+@[^\s]+\.[^\s]+$', email)) > 0
+
+def memberno(memberno, req=True):
+    if not req and memberno == '':
+        return True
+    return len(re.findall('^\d+$', memberno)) > 0
