062e72520422c71ff450c3f92716999dbf6f782b
Ali Kaafarani <ali@kvikshaug.no>
2012-10-04 14:12:20 +0200

    Display associations with visit address

--- apps/association/urls.py
+++ apps/association/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('association.views',
     url(r'^$', 'index'),
+    url(r'^utsalgssteder/$', 'visit'),
     url(r'^filtrer/$', 'filter'),
 )
