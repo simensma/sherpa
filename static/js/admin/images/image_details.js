9ed90f05bebd60631e206c6759e976f754cec8ab
Ali Kaafarani <ali@kvikshaug.no>
2012-10-26 14:52:52 +0200

    Enable moving image from image details view

--- static/js/admin/images/image_details.js
+++ static/js/admin/images/image_details.js
@@ -18,4 +18,18 @@ $(document).ready(function() {
             e.preventDefault();
         }
     });
+
+    $("button.move-image").click(function() {
+        var album_id = $(this).attr('data-album-id');
+        AlbumPicker.open({
+            album_id: album_id,
+            allow_root: false,
+            allow_deselect: true,
+            picked: function(album) {
+                var form = $("form.move-image");
+                form.find("input[name='destination_album']").val(album.id);
+                form.submit();
+            }
+        });
+    });
 });
