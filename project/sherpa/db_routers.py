a282c53054ab6241c5f17dd5898b674162279476
Ali Kaafarani <ali@kvikshaug.no>
2012-11-01 10:42:51 +0100

    Use app labels for db_routers
    This avoids confusions for models of the same name

--- project/sherpa/db_routers.py
+++ project/sherpa/db_routers.py
@@ -1,20 +1,23 @@
 class Router(object):
     def db_for_read(self, model, **hints):
-        if model._meta.object_name in ['Association', 'Cabin']:
+        if model._meta.app_label == 'sherpa2':
             return 'sherpa-2'
-        elif model._meta.object_name in ['Enrollment', 'Actor', 'ActorAddress', 'FocusZipcode', 'Price']:
+        elif model._meta.app_label == 'focus':
             return 'focus'
         return None
 
     def db_for_write(self, model, **hints):
-        if model._meta.object_name in ['Association', 'Cabin', 'Actor', 'ActorAddress', 'FocusZipcode', 'Price']:
+        if model._meta.app_label == 'sherpa2':
             raise Exception("Don't write to this model!")
-        elif model._meta.object_name in ['Enrollment']:
-            return 'focus'
+        elif model._meta.app_label == 'focus':
+            if model._meta.object_name in ['Enrollment']:
+                return 'focus'
+            else:
+                raise Exception("Don't write to this model!")
         return None
 
     def allow_syncdb(self, db, model):
-        if model._meta.object_name in ['Association', 'Cabin', 'Enrollment', 'Actor', 'ActorAddress', 'FocusZipcode', 'Price']:
+        if model._meta.app_label in ['sherpa2', 'focus']:
             return False
         if db in ['sherpa-2', 'sherpa-2.5', 'focus']:
             return False
