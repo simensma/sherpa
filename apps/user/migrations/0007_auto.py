b161f23f488bc9de5881759a1ffd59da42f0e3c0
Ali Kaafarani <ali@kvikshaug.no>
2012-11-05 16:03:30 +0100

    Reorder migrations after rebase

---
+++ apps/user/migrations/0007_auto.py
@@ -0,0 +1,118 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    depends_on = (
+        ('association', '0002_import_associations'),
+    )
+
+    def forwards(self, orm):
+        
+        # Adding M2M table for field associations on 'Profile'
+        db.create_table('user_profile_associations', (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('profile', models.ForeignKey(orm['user.profile'], null=False)),
+            ('association', models.ForeignKey(orm['association.association'], null=False))
+        ))
+        db.create_unique('user_profile_associations', ['profile_id', 'association_id'])
+
+
+    def backwards(self, orm):
+        
+        # Removing M2M table for field associations on 'Profile'
+        db.delete_table('user_profile_associations')
+
+
+    models = {
+        'association.association': {
+            'Meta': {'object_name': 'Association'},
+            'counties': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'associations'", 'symmetrical': 'False', 'to': "orm['user.County']"}),
+            'email': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255'}),
+            'facebook_url': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '2048'}),
+            'focus_id': ('django.db.models.fields.IntegerField', [], {'default': 'None', 'null': 'True'}),
+            'gmap_url': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '2048'}),
+            'group_type': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'organization_no': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255'}),
+            'parent': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'children'", 'null': 'True', 'to': "orm['association.Association']"}),
+            'phone': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255'}),
+            'post_address': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255'}),
+            'type': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'visit_address': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255'}),
+            'zipcode': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['user.Zipcode']", 'null': 'True'})
+        },
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2012, 11, 2, 13, 22, 36, 547263)'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2012, 11, 2, 13, 22, 36, 547112)'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'user.county': {
+            'Meta': {'object_name': 'County'},
+            'code': ('django.db.models.fields.CharField', [], {'max_length': '2'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'sherpa_id': ('django.db.models.fields.IntegerField', [], {'null': 'True'})
+        },
+        'user.focuscountry': {
+            'Meta': {'object_name': 'FocusCountry'},
+            'code': ('django.db.models.fields.CharField', [], {'max_length': '2'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'scandinavian': ('django.db.models.fields.BooleanField', [], {'default': 'False'})
+        },
+        'user.profile': {
+            'Meta': {'object_name': 'Profile'},
+            'associations': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'users'", 'symmetrical': 'False', 'to': "orm['association.Association']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'password_restore_date': ('django.db.models.fields.DateTimeField', [], {'null': 'True'}),
+            'password_restore_key': ('django.db.models.fields.CharField', [], {'max_length': '40', 'unique': 'True', 'null': 'True'}),
+            'phone': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
+            'user': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['auth.User']", 'unique': 'True'})
+        },
+        'user.zipcode': {
+            'Meta': {'object_name': 'Zipcode'},
+            'city': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'city_code': ('django.db.models.fields.CharField', [], {'max_length': '4'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'location': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'zipcode': ('django.db.models.fields.CharField', [], {'max_length': '4'})
+        }
+    }
+
+    complete_apps = ['user']
--- apps/user/migrations/0010_merge_models.py
+++ apps/user/migrations/0010_merge_models.py
@@ -7,26 +7,17 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        
-        # Adding M2M table for field associations on 'Profile'
-        db.create_table('user_profile_associations', (
-            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
-            ('profile', models.ForeignKey(orm['user.profile'], null=False)),
-            ('association', models.ForeignKey(orm['association.association'], null=False))
-        ))
-        db.create_unique('user_profile_associations', ['profile_id', 'association_id'])
+        pass
 
 
     def backwards(self, orm):
-        
-        # Removing M2M table for field associations on 'Profile'
-        db.delete_table('user_profile_associations')
+        pass
 
 
     models = {
         'association.association': {
             'Meta': {'object_name': 'Association'},
-            'counties': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'associations'", 'symmetrical': 'False', 'to': "orm['user.County']"}),
+            'counties': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'associations'", 'symmetrical': 'False', 'to': "orm['core.County']"}),
             'email': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255'}),
             'facebook_url': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '2048'}),
             'focus_id': ('django.db.models.fields.IntegerField', [], {'default': 'None', 'null': 'True'}),
@@ -40,7 +31,7 @@ class Migration(SchemaMigration):
             'post_address': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255'}),
             'type': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
             'visit_address': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255'}),
-            'zipcode': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['user.Zipcode']", 'null': 'True'})
+            'zipcode': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['core.Zipcode']", 'null': 'True'})
         },
         'auth.group': {
             'Meta': {'object_name': 'Group'},
@@ -57,7 +48,7 @@ class Migration(SchemaMigration):
         },
         'auth.user': {
             'Meta': {'object_name': 'User'},
-            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2012, 11, 2, 13, 22, 36, 547263)'}),
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2012, 11, 5, 16, 3, 26, 825290)'}),
             'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
             'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
             'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
@@ -65,7 +56,7 @@ class Migration(SchemaMigration):
             'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
             'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2012, 11, 2, 13, 22, 36, 547112)'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2012, 11, 5, 16, 3, 26, 825117)'}),
             'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
             'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
             'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
@@ -78,19 +69,20 @@ class Migration(SchemaMigration):
             'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
         },
-        'user.county': {
+        'core.county': {
             'Meta': {'object_name': 'County'},
             'code': ('django.db.models.fields.CharField', [], {'max_length': '2'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
             'sherpa_id': ('django.db.models.fields.IntegerField', [], {'null': 'True'})
         },
-        'user.focuscountry': {
-            'Meta': {'object_name': 'FocusCountry'},
-            'code': ('django.db.models.fields.CharField', [], {'max_length': '2'}),
+        'core.zipcode': {
+            'Meta': {'object_name': 'Zipcode'},
+            'area': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'city': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'city_code': ('django.db.models.fields.CharField', [], {'max_length': '4'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
-            'scandinavian': ('django.db.models.fields.BooleanField', [], {'default': 'False'})
+            'zipcode': ('django.db.models.fields.CharField', [], {'max_length': '4'})
         },
         'user.profile': {
             'Meta': {'object_name': 'Profile'},
@@ -100,14 +92,6 @@ class Migration(SchemaMigration):
             'password_restore_key': ('django.db.models.fields.CharField', [], {'max_length': '40', 'unique': 'True', 'null': 'True'}),
             'phone': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
             'user': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['auth.User']", 'unique': 'True'})
-        },
-        'user.zipcode': {
-            'Meta': {'object_name': 'Zipcode'},
-            'city': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'city_code': ('django.db.models.fields.CharField', [], {'max_length': '4'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'location': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'zipcode': ('django.db.models.fields.CharField', [], {'max_length': '4'})
         }
     }
 
