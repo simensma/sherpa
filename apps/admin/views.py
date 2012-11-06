78ac754ba3cd5669cc4336620d9cbc98baa0dbaf
Ali Kaafarani <ali@kvikshaug.no>
2012-11-06 13:09:27 +0100

    Add comment

--- apps/admin/views.py
+++ apps/admin/views.py
@@ -8,6 +8,7 @@ def index(request):
     return render(request, 'main/admin/dashboard.html')
 
 def set_active_association(request, association):
+    # Note: this object will be copied in session for a while and will NOT get updated even if the original object is.
     request.session['active_association'] = Association.objects.get(id=association)
     if request.META.get('REFERER') != None:
         return HttpResponseRedirect(request.META.get('REFERER'))
