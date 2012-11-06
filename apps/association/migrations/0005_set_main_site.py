4f52d76a499d95f6dc638a258e7da46e68f066a5
Ali Kaafarani <ali@kvikshaug.no>
2012-11-06 13:07:41 +0100

    Relate main association to its site

---
+++ apps/association/migrations/0005_set_main_site.py
@@ -0,0 +1,85 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import DataMigration
+from django.db import models
+
+class Migration(DataMigration):
+
+    def forwards(self, orm):
+        site = orm['core.Site'].objects.get(domain='www.turistforeningen.no')
+        association = orm['association.Association'].objects.get(name='Den Norske Turistforening')
+        association.site = site
+        association.save()
+
+    def backwards(self, orm):
+        pass
+
+    models = {
+        'association.association': {
+            'Meta': {'object_name': 'Association'},
+            'counties': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'associations'", 'symmetrical': 'False', 'to': "orm['core.County']"}),
+            'email': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255'}),
+            'facebook_url': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '2048'}),
+            'focus_id': ('django.db.models.fields.IntegerField', [], {'default': 'None', 'null': 'True'}),
+            'gmap_url': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '2048'}),
+            'group_type': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'organization_no': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255'}),
+            'parent': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'children'", 'null': 'True', 'to': "orm['association.Association']"}),
+            'phone': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255'}),
+            'post_address': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255'}),
+            'site': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['core.Site']", 'unique': 'True', 'null': 'True'}),
+            'type': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'visit_address': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255'}),
+            'zipcode': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['core.Zipcode']", 'null': 'True'})
+        },
+        'core.county': {
+            'Meta': {'object_name': 'County'},
+            'code': ('django.db.models.fields.CharField', [], {'max_length': '2'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'sherpa_id': ('django.db.models.fields.IntegerField', [], {'null': 'True'})
+        },
+        'core.focuscountry': {
+            'Meta': {'object_name': 'FocusCountry'},
+            'code': ('django.db.models.fields.CharField', [], {'max_length': '2'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'scandinavian': ('django.db.models.fields.BooleanField', [], {'default': 'False'})
+        },
+        'core.search': {
+            'Meta': {'object_name': 'Search'},
+            'date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'query': ('django.db.models.fields.CharField', [], {'max_length': '1024'})
+        },
+        'core.site': {
+            'Meta': {'object_name': 'Site'},
+            'domain': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'prefix': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'template': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['core.SiteTemplate']"})
+        },
+        'core.sitetemplate': {
+            'Meta': {'object_name': 'SiteTemplate'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'core.tag': {
+            'Meta': {'object_name': 'Tag'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '200'})
+        },
+        'core.zipcode': {
+            'Meta': {'object_name': 'Zipcode'},
+            'area': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'city': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'city_code': ('django.db.models.fields.CharField', [], {'max_length': '4'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'zipcode': ('django.db.models.fields.CharField', [], {'max_length': '4'})
+        }
+    }
+
+    complete_apps = ['core', 'association']
