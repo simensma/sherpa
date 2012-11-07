972a498f2b5f80580d57362e4eebdab142a01dd8
Ali Kaafarani <ali@kvikshaug.no>
2012-11-07 09:48:06 +0100

    Don't allow changing to not-owned association

--- apps/admin/views.py
+++ apps/admin/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.core.exceptions import PermissionDenied
 
 from association.models import Association
 
@@ -9,7 +10,11 @@ def index(request):
 
 def set_active_association(request, association):
     # Note: this object will be copied in session for a while and will NOT get updated even if the original object is.
-    request.session['active_association'] = Association.objects.get(id=association)
+    association = Association.objects.get(id=association)
+    if not association in request.user.get_profile().associations.all():
+        raise PermissionDenied
+
+    request.session['active_association'] = association
     if request.META.get('HTTP_REFERER') != None:
         return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
     else:
