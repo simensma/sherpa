ea6a06387559121126bfdc11e64c9eeff634e6e7
Ali Kaafarani <ali@kvikshaug.no>
2012-11-09 10:27:20 +0100

    Remove obsolete js statements

--- static/js/membership.js
+++ static/js/membership.js
@@ -2,12 +2,6 @@
 
 $(document).ready(function() {
 
-    $("table.prices img[data-popover]").hover(function() {
-        $(this).attr('src', '/static/img/icons/glyph/red/glyphicons_195_circle_info.png');
-    }, function() {
-        $(this).attr('src', '/static/img/icons/glyph/original/glyphicons_195_circle_info.png');
-    });
-
     $("div#benefits dl dd, div#faq dl dd").hide();
     $("div#benefits dl dt, div#faq dl dt").click(function() {
         $(this).next().toggle(400);
