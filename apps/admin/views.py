6fe576dc62116b4e98d862cce0cfe9c8d01b48d3
Ali Kaafarani <ali@kvikshaug.no>
2012-11-14 10:19:50 +0100

    Remove unnecessary import

--- apps/admin/views.py
+++ apps/admin/views.py
@@ -3,7 +3,5 @@ from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.core.exceptions import PermissionDenied
 
-from association.models import Association
-
 def index(request):
     return render(request, 'common/admin/dashboard.html')
