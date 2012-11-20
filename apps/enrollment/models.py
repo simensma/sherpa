92161c135bdc5f6f792558e0a476c4d98c96c009
Ali Kaafarani <ali@kvikshaug.no>
2012-11-20 16:55:51 +0100

    Explicitly use unicode strings for messages

--- apps/enrollment/models.py
+++ apps/enrollment/models.py
@@ -43,33 +43,33 @@ class Giver():
 
         if not validator.name(self.name):
             if add_messages:
-                messages.error(request, "Ditt eget navn mangler.")
+                messages.error(request, u"Ditt eget navn mangler.")
             valid = False
 
         if not validator.address(self.address):
             if add_messages:
-                messages.error(request, "Din egen adresse mangler. Vi sender faktura og medlemskort til denne, derfor må vi ha den.")
+                messages.error(request, u"Din egen adresse mangler. Vi sender faktura og medlemskort til denne, derfor må vi ha den.")
             valid = False
 
         if not validator.zipcode(self.zipcode) or self.area == '':
             # Empty area defines invalid zipcode, as stated in __init__
             if add_messages:
-                messages.error(request, "Postnummeret ditt er ikke gyldig. Vi sender faktura og medlemskort til din adresse, derfor må vi ha den.")
+                messages.error(request, u"Postnummeret ditt er ikke gyldig. Vi sender faktura og medlemskort til din adresse, derfor må vi ha den.")
             valid = False
 
         if not validator.memberno(self.memberno, req=False):
             if add_messages:
-                messages.error(request, "Medlemsnummeret ditt kan kun bestå av tall. Du trenger ikke være medlem for å bestille gavemedlemskap, da kan du la medlemsnummerfeltet stå tomt.")
+                messages.error(request, u"Medlemsnummeret ditt kan kun bestå av tall. Du trenger ikke være medlem for å bestille gavemedlemskap, da kan du la medlemsnummerfeltet stå tomt.")
             valid = False
 
         if not validator.phone(self.phone, req=False):
             if add_messages:
-                messages.error(request, "Telefonnummeret ditt må være minst 8 siffer. Du trenger ikke oppgi telefonnummeret ditt, men vi anbefaler at du gir oss minst én måte å kontakte deg.")
+                messages.error(request, u"Telefonnummeret ditt må være minst 8 siffer. Du trenger ikke oppgi telefonnummeret ditt, men vi anbefaler at du gir oss minst én måte å kontakte deg.")
             valid = False
 
         if not validator.email(self.email, req=False):
             if add_messages:
-                messages.error(request, "E-postadressen din er ikke en gyldig adresse. Du trenger ikke oppgi e-postadressen din, men vi anbefaler at du gir oss minst én måte å kontakte deg.")
+                messages.error(request, u"E-postadressen din er ikke en gyldig adresse. Du trenger ikke oppgi e-postadressen din, men vi anbefaler at du gir oss minst én måte å kontakte deg.")
             valid = False
 
         return valid
@@ -98,7 +98,7 @@ class Receiver():
 
         if self.type_index < 0 or self.type_index >= len(membership_types):
             if add_messages:
-                messages.error(request, "Du har på en eller annen måte klart å angi en ugyldig medlemskapstype. Vennligst bruk select-boksen til å velge medlemskapstype.")
+                messages.error(request, u"Du har på en eller annen måte klart å angi en ugyldig medlemskapstype. Vennligst bruk select-boksen til å velge medlemskapstype.")
             valid = False
 
         if not validator.name(self.name):
@@ -106,32 +106,32 @@ class Receiver():
                 if len(self.name) > 0:
                     messages.error(request, u"Du må angi fullt navn til %s." % self.name)
                 else:
-                    messages.error(request, "En av mottakerne mangler navn.")
+                    messages.error(request, u"En av mottakerne mangler navn.")
             valid = False
 
         if not isinstance(self.dob, datetime):
             if add_messages:
-                messages.error(request, "Fødselsdatoen til %s er ugyldig." % self.name)
+                messages.error(request, u"Fødselsdatoen til %s er ugyldig." % self.name)
             valid = False
 
         if not validator.address(self.address):
             if add_messages:
-                messages.error(request, "%s mangler adresse." % self.name)
+                messages.error(request, u"%s mangler adresse." % self.name)
             valid = False
 
         if not validator.zipcode(self.zipcode) or self.area == '':
             if add_messages:
-                messages.error(request, "Postnummeret til %s er ikke gyldig." % self.name)
+                messages.error(request, u"Postnummeret til %s er ikke gyldig." % self.name)
             valid = False
 
         if not validator.phone(self.phone, req=False):
             if add_messages:
-                messages.error(request, "Telefonnummeret til %s må bestå av minst 8 siffer." % self.name)
+                messages.error(request, u"Telefonnummeret til %s må bestå av minst 8 siffer." % self.name)
             valid = False
 
         if not validator.email(self.email, req=False):
             if add_messages:
-                messages.error(request, "E-postadressen til %s er ikke gyldig." % self.name)
+                messages.error(request, u"E-postadressen til %s er ikke gyldig." % self.name)
             valid = False
 
         return valid
