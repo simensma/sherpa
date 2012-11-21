0ac062ecd67384d942ced8393e5e2750a92e5e0f
Ali Kaafarani <ali@kvikshaug.no>
2012-11-21 16:34:30 +0100

    Add second instagram bulk

--- apps/instagram/views.py
+++ apps/instagram/views.py
@@ -9,10 +9,10 @@ initial_url = 'https://api.instagram.com/v1/tags/turistforeningen/media/recent?c
 def index(request):
     r = requests.get(initial_url)
     data = json.loads(r.content)
-    images = [i['images'] for i in data['data']]
-    bulk = images[0:6]
+    bulk = data['data'][0:6]
+    bulk2 = data['data'][6:18]
     context = {
-        'images': images,
         'bulk': bulk,
+        'bulk2': bulk2,
     }
     return render(request, 'instagram/index.html', context)
