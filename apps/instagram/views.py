14353ef0f809a17cfb2b8e9f65d89f0972f40e36
Ali Kaafarani <ali@kvikshaug.no>
2012-11-21 15:10:24 +0100

    Add initial instagram rendering

--- apps/instagram/views.py
+++ apps/instagram/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
+from django.conf import settings
+
+import requests, json
+
+initial_url = 'https://api.instagram.com/v1/tags/turistforeningen/media/recent?client_id=%s' % settings.INSTAGRAM_CLIENT_ID
 
 def index(request):
-    return render(request, 'instagram/index.html')
+    r = requests.get(initial_url)
+    data = json.loads(r.content)
+    images = [i['images'] for i in data['data']]
+    bulk = images[0:6]
+    context = {
+        'images': images,
+        'bulk': bulk,
+    }
+    return render(request, 'instagram/index.html', context)
