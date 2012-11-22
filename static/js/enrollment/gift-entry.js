f430de7436edcc6b127979e7af46124605448043
Ali Kaafarani <ali@kvikshaug.no>
2012-11-22 15:56:08 +0100

    Use class, not id

--- static/js/enrollment/gift-entry.js
+++ static/js/enrollment/gift-entry.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
     $("form.gift-entry button[type='submit']").click(function() {
-        $("form#gift-entry input[name='type']").val($(this).attr('data-type'));
+        $("form.gift-entry input[name='type']").val($(this).attr('data-type'));
     });
 });
