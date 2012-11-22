b01c5639fbdef98c24a83926ded488d8ac09da7d
Ali Kaafarani <ali@kvikshaug.no>
2012-11-22 09:18:56 +0100

    Cache instagram requests

--- apps/instagram/views.py
+++ apps/instagram/views.py
@@ -2,14 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from django.conf import settings
 from django.template import RequestContext, loader
+from django.core.cache import cache
 
 import requests, json
 
 initial_url = 'https://api.instagram.com/v1/tags/turistforeningen/media/recent?client_id=%s' % settings.INSTAGRAM_CLIENT_ID
 
 def index(request):
-    r = requests.get(initial_url)
-    data = json.loads(r.content)
+    data = cache.get('instagram.url.%s' % initial_url)
+    if data == None:
+        r = requests.get(initial_url)
+        data = json.loads(r.content)
+        cache.set('instagram.url.%s' % initial_url, data, 60 * 60)
+
     bulk = data['data']
     request.session['next_instagram_url'] = data['pagination']['next_url']
     context = {
@@ -18,8 +23,12 @@ def index(request):
     return render(request, 'instagram/index.html', context)
 
 def more(request):
-    r = requests.get(request.session['next_instagram_url'])
-    data = json.loads(r.content)
+    data = cache.get('instagram.url.%s' % request.session['next_instagram_url'])
+    if data == None:
+        r = requests.get(request.session['next_instagram_url'])
+        data = json.loads(r.content)
+        cache.set('instagram.url.%s' % request.session['next_instagram_url'], data, 60 * 60)
+
     bulk = data['data']
     request.session['next_instagram_url'] = data['pagination']['next_url']
     t = loader.get_template('instagram/bulk.html')
