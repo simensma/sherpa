86e4abf6ba66e4b39bad8de50f328c968c111cd2
Ali Kaafarani <ali@kvikshaug.no>
2012-11-22 14:46:46 +0100

    Add more margin to gift-entry boxes

--- static/js/enrollment/gift.js
+++ static/js/enrollment/gift.js
@@ -2,7 +2,7 @@ window.datePickerCount = 1;
 
 $(document).ready(function() {
 
-    $("form#gift-entry button[type='submit']").click(function() {
+    $("form.gift-entry button[type='submit']").click(function() {
         $("form#gift-entry input[name='type']").val($(this).attr('data-type'));
     });
 
