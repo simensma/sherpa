b8c61fad618320eb0fa9956cc2092fe6d3380294
Ali Kaafarani <ali@kvikshaug.no>
2012-11-26 16:05:21 +0100

    Enable checking missing zipcodes from Focus

--- apps/core/management/commands/zipcode.py
+++ apps/core/management/commands/zipcode.py
@@ -1,6 +1,7 @@
 # encoding: utf-8
 from django.core.management.base import BaseCommand
 from core.models import Zipcode, ZipcodeState
+from focus.models import FocusZipcode
 from datetime import datetime
 import re
 
@@ -29,11 +30,103 @@ class ZipcodeRemoval():
         self.date = date
 
 class Command(BaseCommand):
-    args = '<check|report|sync>'
+    args = '<check|report|sync|focus>'
     help = 'Rapporterer og utfører endringer i postens postnummerregister'
 
     def handle(self, *args, **options):
-        self.stdout.write("Sammenlikner postnummerregistre, vennligst vent...\n\n")
+
+
+        if len(args) == 0:
+            args = ['check']
+
+        if args[0] == 'check':
+            if not self.parse_registers():
+                return
+            self.stdout.write("Endringer siden %s:\n" % self.last_update)
+            self.stdout.write(" - %s nye postnumre\n" % len(self.to_be_created))
+            self.stdout.write(" - %s endrede postnumre\n" % len(self.to_be_changed))
+            self.stdout.write(" - %s slettede postnumre\n" % len(self.to_be_removed))
+            self.stdout.write("\n")
+            self.stdout.write("Kjør med 'report' for fullstendig rapport (kan bli lang)\n")
+            self.stdout.write("Kjør med 'sync' for å oppdatere databasen automatisk\n")
+
+        elif args[0] == 'report':
+            if not self.parse_registers():
+                return
+            self.stdout.write("Postnummerendringer siden %s:\n\n" % self.last_update)
+
+            self.stdout.write("Nye:\n")
+            for p in self.to_be_created:
+                self.stdout.write("%s %s %s %s\n" % (p.zipcode, p.area, p.city_code, p.city))
+            self.stdout.write("\n")
+
+            self.stdout.write("Endret:\n")
+            for p in self.to_be_changed:
+                self.stdout.write("%s %s -> %s %s\n" % (p.old_zipcode, p.old_area, p.new_zipcode, p.new_area))
+            self.stdout.write("\n")
+
+            self.stdout.write("Slettet:\n")
+            for p in self.to_be_removed:
+                self.stdout.write("%s %s\n" % (p.zipcode, p.area))
+
+
+        elif args[0] == 'sync':
+            if not self.parse_registers():
+                return
+            self.stdout.write(" - %s nye postnumre vil legges inn\n" % len(self.to_be_created))
+            self.stdout.write(" - %s postnumre vil endres\n" % len(self.to_be_changed))
+            self.stdout.write(" - %s postnumre skal slettes, men SLETTES IKKE AUTOMATISK\n" % len(self.to_be_removed))
+            self.stdout.write("\n")
+
+            if raw_input("Fortsett? (y/n) ") == 'y':
+
+                self.stdout.write("(1/2) Legger inn nye postnumre...\n")
+                for nz in self.to_be_created:
+                    z = Zipcode(
+                        zipcode=nz.zipcode,
+                        area=nz.area,
+                        city_code=nz.city_code,
+                        city=nz.city)
+                    z.save()
+
+                self.stdout.write("(2/2) Oppdaterer eksisterende postnumre...\n")
+                for cz in self.to_be_changed:
+                    z = Zipcode.objects.get(zipcode=cz.old_zipcode, area=cz.old_area)
+                    z.zipcode = cz.new_zipcode
+                    z.area = cz.new_area
+                    z.save()
+
+                state = ZipcodeState.objects.all()[0]
+                state.last_update = datetime.now()
+                state.save()
+
+                self.stdout.write("\n")
+                self.stdout.write("Postnummerdatabasen har blitt synkronisert!\n")
+
+                if len(self.to_be_removed) > 0:
+                    self.stdout.write("Du bør slette følgende numre manuelt:\n")
+                    self.stdout.write("\n")
+
+                    for rz in self.to_be_removed:
+                        self.stdout.write("  %s %s\n" % (rz.zipcode, rz.area))
+
+                    self.stdout.write("\n")
+                    self.stdout.write("Hvis du har relasjoner til andre tabeller bør du sjekke om det finnes noen knytning til hver av disse.\n")
+                    self.stdout.write("Hvis noen slike finnes er jeg ikke helt sikker på hva fremgangsmåten er - det må du finne ut av!\n")
+                    self.stdout.write("\n")
+                    self.stdout.write("Merk: Disse postnumrene vil ikke listes ved neste sync, fordi last_update er satt til \"nå\"\n")
+
+        elif args[0] == 'focus':
+            self.stdout.write("Følgende er i vår database men ikke i Focus:\n")
+            missing = Zipcode.objects.exclude(zipcode__in=[z['zipcode'] for z in FocusZipcode.objects.all().values('zipcode')]).order_by('zipcode')
+            for z in missing:
+                self.stdout.write("%s %s %s %s\n" % (z.zipcode, z.area, z.city_code, z.city))
+            self.stdout.write("\n")
+
+            self.stdout.write("Totalt %s manglende postnumre.\n" % len(missing))
+
+    def parse_registers(self):
+        self.stdout.write("Parser postnummerregistre, vennligst vent...\n\n")
 
         filename_new = 'postnummerlogg_nye_ansi.txt'
         filename_changes = 'postnummerlogg_endringer_ansi.txt'
@@ -51,7 +144,7 @@ class Command(BaseCommand):
                         self.stdout.write("Error: Klarte ikke å parse følgende linje i '%s':\n" % filename_new)
                         self.stdout.write("  -> '%s'\n" % line.strip())
                         self.stdout.write("Sjekk om det er nytt format i postnummerregisterfilene. Du må kanskje inn og oppdatere zipcode-kommandoen :)\n")
-                        return
+                        return False
 
             with open(filename_changes, 'rt') as f:
                 self.changed_zipcodes = list()
@@ -75,90 +168,19 @@ class Command(BaseCommand):
                         self.stdout.write("Error: Klarte ikke å parse følgende linje i '%s':\n" % filename_changes)
                         self.stdout.write("  -> '%s'\n" % line.strip())
                         self.stdout.write("Sjekk om det er nytt format i postnummerregisterfilene. Du må kanskje inn og oppdatere zipcode-kommandoen :)\n")
-                        return
+                        return False
+
+            self.last_update = ZipcodeState.objects.all()[0].last_update
+            self.to_be_created = [nz for nz in self.new_zipcodes if nz.date > self.last_update]
+            self.to_be_changed = [nz for nz in self.changed_zipcodes if nz.date > self.last_update]
+            self.to_be_removed = [nz for nz in self.removed_zipcodes if nz.date > self.last_update]
+
+            return True
 
         except IOError:
              self.stdout.write("Forventet å finne følgende filer i denne mappa:\n")
              self.stdout.write("  '%s' (Oversikt over alle nye postnummer med TAB-separerte felter, ANSI).\n" % filename_new)
              self.stdout.write("  '%s' (Oversikt over alle endringer med TAB-separerte felter, ANSI).\n" % filename_changes)
              self.stdout.write("Last dem ned herfra: http://www.bring.no/144754/postnummertabeller\n")
-             return
+             return False
 
-        last_update = ZipcodeState.objects.all()[0].last_update
-        to_be_created = [nz for nz in self.new_zipcodes if nz.date > last_update]
-        to_be_changed = [nz for nz in self.changed_zipcodes if nz.date > last_update]
-        to_be_removed = [nz for nz in self.removed_zipcodes if nz.date > last_update]
-
-        if len(args) == 0:
-            args = ['check']
-
-        if args[0] == 'check':
-            self.stdout.write("Endringer siden %s:\n" % last_update)
-            self.stdout.write(" - %s nye postnumre\n" % len(to_be_created))
-            self.stdout.write(" - %s endrede postnumre\n" % len(to_be_changed))
-            self.stdout.write(" - %s slettede postnumre\n" % len(to_be_removed))
-            self.stdout.write("\n")
-            self.stdout.write("Kjør med 'report' for fullstendig rapport (kan bli lang)\n")
-            self.stdout.write("Kjør med 'sync' for å oppdatere databasen automatisk\n")
-
-        elif args[0] == 'report':
-            self.stdout.write("Postnummerendringer siden %s:\n\n" % last_update)
-
-            self.stdout.write("Nye:\n")
-            for p in to_be_created:
-                self.stdout.write("%s %s %s %s\n" % (p.zipcode, p.area, p.city_code, p.city))
-            self.stdout.write("\n")
-
-            self.stdout.write("Endret:\n")
-            for p in to_be_changed:
-                self.stdout.write("%s %s -> %s %s\n" % (p.old_zipcode, p.old_area, p.new_zipcode, p.new_area))
-            self.stdout.write("\n")
-
-            self.stdout.write("Slettet:\n")
-            for p in to_be_removed:
-                self.stdout.write("%s %s\n" % (p.zipcode, p.area))
-
-
-        elif args[0] == 'sync':
-            self.stdout.write(" - %s nye postnumre vil legges inn\n" % len(to_be_created))
-            self.stdout.write(" - %s postnumre vil endres\n" % len(to_be_changed))
-            self.stdout.write(" - %s postnumre skal slettes, men SLETTES IKKE AUTOMATISK\n" % len(to_be_removed))
-            self.stdout.write("\n")
-
-            if raw_input("Fortsett? (y/n) ") == 'y':
-
-                self.stdout.write("(1/2) Legger inn nye postnumre...\n")
-                for nz in to_be_created:
-                    z = Zipcode(
-                        zipcode=nz.zipcode,
-                        area=nz.area,
-                        city_code=nz.city_code,
-                        city=nz.city)
-                    z.save()
-
-                self.stdout.write("(2/2) Oppdaterer eksisterende postnumre...\n")
-                for cz in to_be_changed:
-                    z = Zipcode.objects.get(zipcode=cz.old_zipcode, area=cz.old_area)
-                    z.zipcode = cz.new_zipcode
-                    z.area = cz.new_area
-                    z.save()
-
-                state = ZipcodeState.objects.all()[0]
-                state.last_update = datetime.now()
-                state.save()
-
-                self.stdout.write("\n")
-                self.stdout.write("Postnummerdatabasen har blitt synkronisert!\n")
-
-                if len(to_be_removed) > 0:
-                    self.stdout.write("Du bør slette følgende numre manuelt:\n")
-                    self.stdout.write("\n")
-
-                    for rz in to_be_removed:
-                        self.stdout.write("  %s %s\n" % (rz.zipcode, rz.area))
-
-                    self.stdout.write("\n")
-                    self.stdout.write("Hvis du har relasjoner til andre tabeller bør du sjekke om det finnes noen knytning til hver av disse.\n")
-                    self.stdout.write("Hvis noen slike finnes er jeg ikke helt sikker på hva fremgangsmåten er - det må du finne ut av!\n")
-                    self.stdout.write("\n")
-                    self.stdout.write("Merk: Disse postnumrene vil ikke listes ved neste sync, fordi last_update er satt til \"nå\"\n")
