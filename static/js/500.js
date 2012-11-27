040564cd56dc52381158f42722aae5728921f7bf
Ali Kaafarani <ali@kvikshaug.no>
2012-11-27 16:13:30 +0100

    Add photographer name to 500 page

--- static/js/500.js
+++ static/js/500.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
     $("div.plax img.background").plaxify({"xRange": 15, "yRange": 15})
-    $("div.plax p").plaxify({"xRange": 20, "yRange": 20, "invert": true})
+    $("div.plax p.description").plaxify({"xRange": 20, "yRange": 20, "invert": true})
     $("div.plax h1").plaxify({"xRange": 20, "yRange": 25})
+    $("div.plax p.photographer").plaxify({"xRange": 15, "yRange": 15})
     $.plax.enable();
 
     $("a.refresh").click(function() {
