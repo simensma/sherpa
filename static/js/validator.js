1c61d4140d706b5550aea755dc161215ded6beed
Ali Kaafarani <ali@kvikshaug.no>
2012-11-29 15:13:46 +0100

    Merge ZipcodeValidator with general Validator

--- static/js/validator.js
+++ static/js/validator.js
@@ -68,4 +68,64 @@
         }
     };
 
+
+    /**
+     * Zipcode-validation with AJAX-lookup
+     */
+    Validator.triggerZipcode = function(zipcode) { zipcode.keyup(); }
+    Validator.stopZipcodeValidation = function(zipcode) { zipcode.off('.zipcodevalidator'); }
+    Validator.validateZipcode = function(control_group, zipcode, area, loader, options) {
+
+        triggers.push(function() { zipcode.keyup(); });
+
+        zipcode.on('focusin.zipcodevalidator', function() {
+            control_group.removeClass('error success');
+        });
+
+        zipcode.on('focusout.zipcodevalidator', function() {
+            control_group.removeClass('error success');
+            if(control_group.data('valid')) {
+                control_group.addClass('success');
+            } else {
+                control_group.addClass('error');
+            }
+        });
+
+        zipcode.on('keyup.zipcodevalidator', function() {
+            control_group.removeClass('success error');
+            function end() {
+                if(!zipcode.is(":focus")) {
+                    // Trigger validation-check
+                    zipcode.focusout();
+                }
+            }
+            control_group.data('valid', false);
+            if(!zipcode.val().match(/^\d{4}$/)) {
+                end();
+                return zipcode;
+            }
+            loader.show();
+            $.ajax({
+                url: '/postnummer/' + encodeURIComponent(zipcode.val()) + '/',
+                type: 'POST'
+            }).done(function(result) {
+                result = JSON.parse(result);
+                if(result.area != undefined) {
+                    area.val(result.area);
+                    control_group.addClass('success');
+                    control_group.data('valid', true);
+                } else if(result.error == "does_not_exist") {
+                    area.val("Ukjent postnummer");
+                    control_group.addClass('error');
+                }
+            }).fail(function(result) {
+                area.val("Teknisk feil");
+                control_group.addClass('error');
+            }).always(function(result) {
+                loader.hide();
+                end();
+            });
+        });
+    }
+
 }(window.Validator = window.Validator || {}, jQuery ));
