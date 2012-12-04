16d4e963dc4569849cc600e4280c06dac95086ca
Ali Kaafarani <ali@kvikshaug.no>
2012-12-04 20:10:24 +0100

    Add client-side validations

--- static/js/user/update_account.js
+++ static/js/user/update_account.js
@@ -1,9 +1,64 @@
 $(document).ready(function() {
 
+    Validator.validate({
+        method: 'full_name',
+        control_group: $("div.control-group.name"),
+        input: $("input[name='name']"),
+        req: true
+    });
+
+    Validator.validate({
+        method: 'email',
+        control_group: $("div.control-group.email"),
+        input: $("input[name='email']"),
+        req: true
+    });
+
+    Validator.validate({
+        method: 'phone',
+        control_group: $("div.control-group.phone_home"),
+        input: $("input[name='phone_home']"),
+        req: false
+    });
+
+    Validator.validate({
+        method: 'phone',
+        control_group: $("div.control-group.phone_mobile"),
+        input: $("input[name='phone_mobile']"),
+        req: false
+    });
+
     $("input[name='dob']").pickadate({
         format: 'dd.mm.yyyy',
         yearSelector: true,
-        monthSelector: true
+        monthSelector: true,
+        onClose: function() {
+            Validator.performValidation({
+                method: 'date',
+                control_group: $("div.control-group.dob"),
+                input: $("input[name='dob']"),
+                req: true
+            });
+        }
     });
+
+    Validator.validate({
+        method: 'address',
+        control_group: $("div.control-group.address"),
+        input: $("input[name='address']"),
+        req: true
+    });
+
+    Validator.validateZipcode(
+        $("div.control-group.zipcode"),
+        $("input[name='zipcode']"),
+        $("input[name='area']"),
+        $("img.ajaxloader.zipcode")
+    );
+
+    // Trigger all validations
+    Validator.trigger();
+    $("input[name='dob']").data('pickadate').close();
+    Validator.triggerZipcode($("input[name='zipcode']"));
 
 });
