1b39f1a304f7736b64f9c536eb7a2be68cb19a58
Ali Kaafarani <ali@kvikshaug.no>
2012-12-05 13:33:57 +0100

    Add pickadate defaults

---
+++ static/lib/pickadate/pickadate-defaults.js
@@ -0,0 +1,7 @@
+$.extend( $.fn.pickadate.defaults, {
+    format: 'dd.mm.yyyy',
+    firstDay: 1,
+    monthsFull: [ 'Januar', 'Februar', 'Mars', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Desember' ],
+    monthsShort: [ 'Jan', 'Feb', 'Mar', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Des' ],
+    weekdaysShort: [ 'Søn', 'Man', 'Tir', 'Ons', 'Tor', 'Fre', 'Lør' ]
+});
