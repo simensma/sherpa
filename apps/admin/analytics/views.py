bf08c96af3aa6a80ec0777770ee18a77fbee0927
Ali Kaafarani <ali@kvikshaug.no>
2012-12-16 12:12:30 +0100

    Add admin-ui view for listing 404-hits

--- apps/admin/analytics/views.py
+++ apps/admin/analytics/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.core.cache import cache
 
-from analytics.models import Search
+from analytics.models import Search, NotFound
 
 def index(request):
     return render(request, 'common/admin/analytics/index.html')
@@ -28,3 +28,24 @@ def searches(request):
 
     context = {'most_searched': most_searched, 'latest_searches': latest_searches}
     return render(request, 'common/admin/analytics/searches.html', context)
+
+def not_found(request):
+    most_requested = cache.get('analytics.not_found.most_requested')
+    if most_requested is None:
+        hits = NotFound.on(request.session['active_association'].site).all()
+        hashes = {}
+
+        for hit in hits:
+            hashes[hit.path.lower()] = hashes.get(hit.path.lower(), 0) + 1
+
+        most_requested = []
+        for path, count in hashes.items():
+            most_requested.append({'path': path, 'count': count})
+
+        most_requested = sorted(most_requested, key=lambda search: -search['count'])
+        cache.set('analytics.not_found.most_requested', most_requested, 60 * 60 * 24)
+
+    latest_requests = NotFound.on(request.session['active_association'].site).all().order_by('-date')[:50]
+
+    context = {'most_requested': most_requested, 'latest_requests': latest_requests}
+    return render(request, 'common/admin/analytics/not_found.html', context)
