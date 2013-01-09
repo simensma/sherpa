bc649a6beba168e920211ac0b77471e0f8715f7f
Ali Kaafarani <ali@kvikshaug.no>
2013-01-09 15:48:35 +0100

    Store internal reference to widget-editor window

--- static/js/admin/editor/widgets/carousel.js
+++ static/js/admin/editor/widgets/carousel.js
@@ -5,6 +5,7 @@
     var currentIndex = 0;
     var imageList = [];
     var newWidgetParentWidth = 0;
+    var widget_editor; // Will be set in the document.ready block further down
 
     /* Private methods */
 
@@ -35,37 +36,37 @@
                 imageList[currentIndex].ratio = imageList[currentIndex-1].ratio
             }
         }
-        $("div.widget-edit[data-widget='carousel'] input[name='ratio']").each(function(){
+        widget_editor.find("input[name='ratio']").each(function(){
             if($(this).val() == imageList[currentIndex].ratio){
                 $(this).attr("checked", "checked");
             }
         });
 
-        $("div.widget-edit[data-widget='carousel'] label[name='sequence']").text("Bilde " + (currentIndex+1) + "/" + imageList.length + " ");
-        $("div.widget-edit[data-widget='carousel'] input[name='url']").val(removeImageSizeFromUrl(imageList[currentIndex].url));
-        $("div.widget-edit[data-widget='carousel'] input[name='description']").val(imageList[currentIndex].description);
-        $("div.widget-edit[data-widget='carousel'] input[name='photographer']").val(imageList[currentIndex].photographer);
+        widget_editor.find("label[name='sequence']").text("Bilde " + (currentIndex+1) + "/" + imageList.length + " ");
+        widget_editor.find("input[name='url']").val(removeImageSizeFromUrl(imageList[currentIndex].url));
+        widget_editor.find("input[name='description']").val(imageList[currentIndex].description);
+        widget_editor.find("input[name='photographer']").val(imageList[currentIndex].photographer);
 
         if(imageList[currentIndex].url.trim().length < 1){
-            var def = $("div.widget-edit[data-widget='carousel'] img[name='preview']").attr("default");
-            $("div.widget-edit[data-widget='carousel'] img[name='preview']").attr('src', def);
-            $("div.widget-edit[data-widget='carousel'] div#preview").hide();
+            var def = widget_editor.find("img[name='preview']").attr("default");
+            widget_editor.find("img[name='preview']").attr('src', def);
+            widget_editor.find("div#preview").hide();
         }else{
-            $("div.widget-edit[data-widget='carousel'] img[name='preview']").attr('src', addImageSizeToUrl(imageList[currentIndex].url, IMAGE_PPREVIEW_WIDTH));
-            $("div.widget-edit[data-widget='carousel'] div#preview").show();
+            widget_editor.find("img[name='preview']").attr('src', addImageSizeToUrl(imageList[currentIndex].url, IMAGE_PPREVIEW_WIDTH));
+            widget_editor.find("div#preview").show();
         }
 
         if(currentIndex == imageList.length -1){
-            $("div.widget-edit button.next").text(" Nytt bilde");
-            $("div.widget-edit button.next").prepend("<i class='icon-plus'></i>");
+            widget_editor.find("button.next").text(" Nytt bilde");
+            widget_editor.find("button.next").prepend("<i class='icon-plus'></i>");
         }else{
-            $("div.widget-edit button.next").text("Neste bilde ")
-            $("div.widget-edit button.next").append("<i class='icon-chevron-right'></i>");
+            widget_editor.find("button.next").text("Neste bilde ")
+            widget_editor.find("button.next").append("<i class='icon-chevron-right'></i>");
         }
 
         //hax, the onload function is for when you are changing images and the selector needs a new height
-        $("div.widget-edit[data-widget='carousel'] img[name='preview']").imagesLoaded(function(){
-            openImageCropper($("div.widget-edit[data-widget='carousel'] img[name='preview']"), $("div.widget-edit[data-widget='carousel']"), imageList[currentIndex].selection);
+        widget_editor.find("img[name='preview']").imagesLoaded(function(){
+            openImageCropper(widget_editor.find("img[name='preview']"), widget_editor, imageList[currentIndex].selection);
             setCarouselRatio(imageList[currentIndex].selection == undefined);
             if(imageList[currentIndex].url.trim().length < 1){
                 currentCropperInstance.cancelSelection();
@@ -154,7 +155,8 @@
 
     $(document).ready(function() {
 
-        $("div.widget-edit[data-widget='carousel'] div#ratio-radio").append(getRatioRadioButtons());
+        widget_editor = $("div.widget-edit[data-widget='carousel']");
+        widget_editor.find("div#ratio-radio").append(getRatioRadioButtons());
 
         //carousel, stop spinning
         $('.carousel').each(function(){
@@ -164,14 +166,14 @@
         });
 
         // carousel navigation
-        $("div.widget-edit button.previous").click(function() {
+        widget_editor.find("button.previous").click(function() {
             ImageCarouselWidget.saveCropping();
             if(currentIndex > 0){
                 currentIndex--;
             }
             displayCurrentImage();
         });
-        $("div.widget-edit button.next").click(function() {
+        widget_editor.find("button.next").click(function() {
             ImageCarouselWidget.saveCropping();
             if(currentIndex == imageList.length -1){
                 //another image is added if you are at the last image and the current image isn't a blank
@@ -190,7 +192,7 @@
         });
 
         //remove clicked
-        $("div.widget-edit[data-widget='carousel'] button[name='remove']").click(function(){
+        widget_editor.find("button[name='remove']").click(function(){
             imageList.splice(currentIndex, 1);
             if(currentIndex > 0){
                 currentIndex--;
@@ -208,31 +210,31 @@
         });
 
         //choose clicked
-        $("div.widget-edit[data-widget='carousel'] button[name='choose']").click(function(){
+        widget_editor.find("button[name='choose']").click(function(){
             chooseImagefromArchive(chooseFromSpurce);
         });
 
         //upload clicked
-        $("div.widget-edit[data-widget='carousel'] button[name='upload']").click(function(){
+        widget_editor.find("button[name='upload']").click(function(){
             openImageUpload(chooseFromSpurce);
         });
 
         //updating data in "model" on key up
-        $("div.widget-edit[data-widget='carousel'] input[name='url']").keyup(function(){
+        widget_editor.find("input[name='url']").keyup(function(){
             imageList[currentIndex].url = $(this).val().trim();
             imageList[currentIndex].selection = undefined;
-            $("div.widget-edit[data-widget='carousel'] img[name='preview']").attr('src', addImageSizeToUrl(imageList[currentIndex].url, IMAGE_PPREVIEW_WIDTH));
+            widget_editor.find("img[name='preview']").attr('src', addImageSizeToUrl(imageList[currentIndex].url, IMAGE_PPREVIEW_WIDTH));
         });
-        $("div.widget-edit[data-widget='carousel'] input[name='description']").keyup(function(){
+        widget_editor.find("input[name='description']").keyup(function(){
             imageList[currentIndex].description = $(this).val().trim();
         });
-        $("div.widget-edit[data-widget='carousel'] input[name='photographer']").keyup(function(){
+        widget_editor.find("input[name='photographer']").keyup(function(){
             imageList[currentIndex].photographer = $(this).val().trim();
         });
 
         //ratio
-        $("div.widget-edit[data-widget='carousel'] input[name='ratio']").change(function(){
-            imageList[currentIndex].ratio = $("div.widget-edit[data-widget='carousel'] input[name='ratio']:checked").val();
+        widget_editor.find("input[name='ratio']").change(function(){
+            imageList[currentIndex].ratio = widget_editor.find("input[name='ratio']:checked").val();
             setCarouselRatio(true);
         });
 
