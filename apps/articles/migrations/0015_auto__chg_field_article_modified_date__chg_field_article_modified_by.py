44dc40392424a83b95d823a8d2908ff3fb487c63
Ali Kaafarani <ali@kvikshaug.no>
2013-01-10 13:35:28 +0100

    Allow null values in modified_date and modified_by

---
+++ apps/articles/migrations/0015_auto__chg_field_article_modified_date__chg_field_article_modified_by.py
@@ -0,0 +1,152 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Changing field 'Article.modified_date'
+        db.alter_column('articles_article', 'modified_date', self.gf('django.db.models.fields.DateTimeField')(null=True))
+
+        # Changing field 'Article.modified_by'
+        db.alter_column('articles_article', 'modified_by_id', self.gf('django.db.models.fields.related.ForeignKey')(null=True, to=orm['user.Profile']))
+
+
+    def backwards(self, orm):
+        
+        # User chose to not deal with backwards NULL issues for 'Article.modified_date'
+        raise RuntimeError("Cannot reverse this migration. 'Article.modified_date' and its values cannot be restored.")
+
+        # User chose to not deal with backwards NULL issues for 'Article.modified_by'
+        raise RuntimeError("Cannot reverse this migration. 'Article.modified_by' and its values cannot be restored.")
+
+
+    models = {
+        'articles.article': {
+            'Meta': {'object_name': 'Article'},
+            'created_by': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'articles_created'", 'to': "orm['user.Profile']"}),
+            'created_date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'hide_thumbnail': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified_by': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'articles_modified'", 'null': 'True', 'to': "orm['user.Profile']"}),
+            'modified_date': ('django.db.models.fields.DateTimeField', [], {'null': 'True'}),
+            'pub_date': ('django.db.models.fields.DateTimeField', [], {'null': 'True'}),
+            'published': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'site': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['core.Site']"}),
+            'thumbnail': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True'})
+        },
+        'articles.oldarticle': {
+            'Meta': {'object_name': 'OldArticle'},
+            'author_email': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'author_name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'content': ('django.db.models.fields.TextField', [], {}),
+            'date': ('django.db.models.fields.DateTimeField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'lede': ('django.db.models.fields.TextField', [], {}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        'association.association': {
+            'Meta': {'object_name': 'Association'},
+            'counties': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'associations'", 'symmetrical': 'False', 'to': "orm['core.County']"}),
+            'email': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255'}),
+            'facebook_url': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '2048'}),
+            'focus_id': ('django.db.models.fields.IntegerField', [], {'default': 'None', 'null': 'True'}),
+            'gmap_url': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '2048'}),
+            'group_type': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'organization_no': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255'}),
+            'parent': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'children'", 'null': 'True', 'to': "orm['association.Association']"}),
+            'phone': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255'}),
+            'post_address': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255'}),
+            'site': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['core.Site']", 'unique': 'True', 'null': 'True'}),
+            'type': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'visit_address': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255'}),
+            'zipcode': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['core.Zipcode']", 'null': 'True'})
+        },
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 1, 10, 13, 34, 8, 808665)'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 1, 10, 13, 34, 8, 808427)'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'core.county': {
+            'Meta': {'object_name': 'County'},
+            'code': ('django.db.models.fields.CharField', [], {'max_length': '2'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'sherpa_id': ('django.db.models.fields.IntegerField', [], {'null': 'True'})
+        },
+        'core.site': {
+            'Meta': {'object_name': 'Site'},
+            'domain': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'prefix': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'template': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['core.SiteTemplate']"})
+        },
+        'core.sitetemplate': {
+            'Meta': {'object_name': 'SiteTemplate'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'core.zipcode': {
+            'Meta': {'object_name': 'Zipcode'},
+            'area': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'city': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'city_code': ('django.db.models.fields.CharField', [], {'max_length': '4'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'zipcode': ('django.db.models.fields.CharField', [], {'max_length': '4'})
+        },
+        'user.associationrole': {
+            'Meta': {'object_name': 'AssociationRole'},
+            'association': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['association.Association']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'profile': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['user.Profile']"}),
+            'role': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        'user.profile': {
+            'Meta': {'object_name': 'Profile'},
+            'associations': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'users'", 'symmetrical': 'False', 'through': "orm['user.AssociationRole']", 'to': "orm['association.Association']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'memberid': ('django.db.models.fields.IntegerField', [], {'unique': 'True', 'null': 'True'}),
+            'password_restore_date': ('django.db.models.fields.DateTimeField', [], {'null': 'True'}),
+            'password_restore_key': ('django.db.models.fields.CharField', [], {'max_length': '40', 'unique': 'True', 'null': 'True'}),
+            'sherpa_email': ('django.db.models.fields.EmailField', [], {'max_length': '75'}),
+            'user': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['auth.User']", 'unique': 'True'})
+        }
+    }
+
+    complete_apps = ['articles']
--- apps/articles/models.py
+++ apps/articles/models.py
@@ -11,8 +11,8 @@ class Article(models.Model):
     pub_date = models.DateTimeField(null=True)
     created_date = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey('user.Profile', related_name='articles_created')
-    modified_date = models.DateTimeField()
-    modified_by = models.ForeignKey('user.Profile', related_name='articles_modified')
+    modified_date = models.DateTimeField(null=True)
+    modified_by = models.ForeignKey('user.Profile', related_name='articles_modified', null=True)
 
     site = models.ForeignKey('core.Site')
     @staticmethod
