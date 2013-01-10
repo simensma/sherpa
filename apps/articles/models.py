001de4ffbd77e9291d519d63847f0c0a4fce191e
Ali Kaafarani <ali@kvikshaug.no>
2013-01-10 15:50:54 +0100

    Reorder model fields

--- apps/articles/models.py
+++ apps/articles/models.py
@@ -9,10 +9,10 @@ class Article(models.Model):
     hide_thumbnail = models.BooleanField()
     published = models.BooleanField()
     pub_date = models.DateTimeField(null=True)
-    created_date = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey('user.Profile', related_name='articles_created')
-    modified_date = models.DateTimeField(null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
     modified_by = models.ForeignKey('user.Profile', related_name='articles_modified', null=True)
+    modified_date = models.DateTimeField(null=True)
 
     site = models.ForeignKey('core.Site')
     @staticmethod
