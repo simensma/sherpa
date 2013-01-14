c41da756fbca966a7808cfab250a868f3b54561e
Ali Kaafarani <ali@kvikshaug.no>
2013-01-14 13:24:20 +0100

    Add initial aktivitet-model

--- apps/aktiviteter/models.py
+++ apps/aktiviteter/models.py
@@ -1,1 +1,6 @@
 from django.db import models
+
+class Aktivitet(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    start_date = models.DateTimeField()
