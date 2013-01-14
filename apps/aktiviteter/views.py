abe30286d54e5c7d1225dea0a23fa24d9ef38b86
Ali Kaafarani <ali@kvikshaug.no>
2013-01-14 15:43:57 +0100

    List and show aktiviteter on aktivitet-page

--- apps/aktiviteter/views.py
+++ apps/aktiviteter/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 
+from aktiviteter.models import Aktivitet
+
 def index(request):
-    context = {}
+    aktiviteter = Aktivitet.objects.all().order_by('-start_date')
+    context = {'aktiviteter': aktiviteter}
     return render(request, 'common/aktiviteter/index.html', context)
+
+def show(request, aktivitet):
+    aktivitet = Aktivitet.objects.get(id=aktivitet)
+    context = {'aktivitet': aktivitet}
+    return render(request, 'common/aktiviteter/show.html', context)
