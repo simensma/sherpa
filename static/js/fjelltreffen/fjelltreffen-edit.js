eb5dde1cfc1819c920bb4a4e01503b704ff879e8
Ali Kaafarani <ali@kvikshaug.no>
2013-01-23 14:54:24 +0100

    Use correct selectors for validations

--- static/js/fjelltreffen/fjelltreffen-edit.js
+++ static/js/fjelltreffen/fjelltreffen-edit.js
@@ -2,22 +2,22 @@ $(document).ready(function() {
 
     Validator.validate({
         method: 'email',
-        control_group: $("div.control-group.annonse-email-control"),
-        input: $("input.annonse-email"),
+        control_group: $("div.control-group.email"),
+        input: $("input[name='email']"),
         req: true
     });
 
     Validator.validate({
         method: 'anything',
-        control_group: $("div.control-group.annonse-title-control"),
-        input: $("input.annonse-title"),
+        control_group: $("div.control-group.title"),
+        input: $("input[name='title']"),
         req: true
     });
 
     Validator.validate({
         method: 'anything',
-        control_group: $("div.control-group.annonse-text-control"),
-        input: $("textarea.annonse-text"),
+        control_group: $("div.control-group.text"),
+        input: $("textarea[name='text']"),
         req: true
     });
 
