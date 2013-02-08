979884828aef06c88bb0d84dda22e148b879dd26
Ali Kaafarani <ali@kvikshaug.no>
2013-02-08 09:28:33 +0100

    Add text-to-speech to captcha

--- project/sherpa/settings.py
+++ project/sherpa/settings.py
@@ -38,6 +38,7 @@ FJELLTREFFEN_ANNONSE_RETENTION_DAYS = 90 # How long fjelltreffen-annonser are sh
 FJELLTREFFEN_AGE_LIMITS = [18, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80] # Age queries are rounded to these values
 FJELLTREFFEN_AGE_LIMIT = min(FJELLTREFFEN_AGE_LIMITS)
 FJELLTREFFEN_REPORT_EMAIL = 'DNT medlemsservice <medlem@turistforeningen.no>'
+CAPTCHA_FLITE_PATH = '/usr/bin/flite'
 
 
 
