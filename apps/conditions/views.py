3931d3fa7384a770039a6f1139151542d1f19750
Ali Kaafarani <ali@kvikshaug.no>
2013-02-14 14:09:06 +0100

    Add advertisement to conditions page

--- apps/conditions/views.py
+++ apps/conditions/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.core.cache import cache
 
 from sherpa2.models import Condition
+from page.models import AdPlacement
 
 
 def index(request):
@@ -15,5 +16,6 @@ def index(request):
     available_locations = sorted(available_locations, key=lambda l: l.name)
     context = {
         'conditions': conditions,
-        'available_locations': available_locations}
+        'available_locations': available_locations,
+        'advertisement': AdPlacement.get_active_ad()}
     return render(request, 'main/conditions/index.html', context)
