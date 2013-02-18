46657c83c5c14995480597e52c804fc685790e64
Ali Kaafarani <ali@kvikshaug.no>
2013-02-18 16:08:10 +0100

    Remove unused imports

--- apps/admin/views.py
+++ apps/admin/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
-from django.core.urlresolvers import reverse
-from django.core.exceptions import PermissionDenied
 
 def index(request):
     return render(request, 'common/admin/dashboard.html')
