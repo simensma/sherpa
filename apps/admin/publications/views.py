f149d4858cf760524afb78c81c11845b5aae14a9
Ali Kaafarani <ali@kvikshaug.no>
2013-02-21 14:47:12 +0100

    Add initial editing functionality

--- apps/admin/publications/views.py
+++ apps/admin/publications/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.contrib import messages
 
+from association.models import Association
 from admin.models import Publication
 
 def index(request):
@@ -18,5 +20,16 @@ def create(request):
 
 def edit(request, publication):
     publication = Publication.objects.get(id=publication)
-    context = {'publication': publication}
-    return render(request, 'common/admin/publications/edit.html', context)
+    if request.method == 'GET':
+        context = {'publication': publication}
+        return render(request, 'common/admin/publications/edit.html', context)
+    elif request.method == 'POST':
+        publication.title = request.POST['title']
+        association = Association.objects.get(id=request.POST['association'])
+        if association in request.user.get_profile().all_associations():
+            publication.association = association
+        if request.POST['license'] in [l[0] for l in Publication.LICENSE_CHOICES]:
+            publication.license = request.POST['license']
+        publication.save()
+        messages.info(request, 'publication_info_saved')
+        return HttpResponseRedirect(reverse('admin.publications.views.edit', args=[publication.id]))
