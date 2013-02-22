28d75965285c25b52d098132e3121eb9b0f5aa9c
Ali Kaafarani <ali@kvikshaug.no>
2013-02-22 13:46:47 +0100

    Rename the image-upload-dialog class accordingly

--- static/js/admin/images/util/image-upload-dialog.js
+++ static/js/admin/images/util/image-upload-dialog.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
 
-    var tagger = new TypicalTagger($("div.image-fast-upload form.image-uploader input[name='tags']"), $("div.image-fast-upload div.tag-box"));
+    var tagger = new TypicalTagger($("div.image-upload-dialog form.image-uploader input[name='tags']"), $("div.image-upload-dialog div.tag-box"));
 
-    $("div.image-fast-upload form").submit(function(e) {
-        $("div.image-fast-upload div.uploading").show();
+    $("div.image-upload-dialog form").submit(function(e) {
+        $("div.image-upload-dialog div.uploading").show();
         var tags = JSON.stringify(tagger.tags);
-        $("div.image-fast-upload form.image-uploader input[name='tags-serialized']").val(tags);
-        $("div.image-fast-upload input[type='submit']").attr('disabled', 'disabled');
-        $("div.image-fast-upload input[type='reset']").attr('disabled', 'disabled');
+        $("div.image-upload-dialog form.image-uploader input[name='tags-serialized']").val(tags);
+        $("div.image-upload-dialog input[type='submit']").attr('disabled', 'disabled');
+        $("div.image-upload-dialog input[type='reset']").attr('disabled', 'disabled');
     });
 
-    $("div.image-fast-upload form input[name='photographer']").typeahead({
+    $("div.image-upload-dialog form input[name='photographer']").typeahead({
         minLength: 3,
         source: function(query, process) {
             $.ajaxQueue({
@@ -22,9 +22,9 @@ $(document).ready(function() {
         }
     });
 
-    $("div.image-fast-upload button.cancel-upload").click(function(e) {
+    $("div.image-upload-dialog button.cancel-upload").click(function(e) {
         uploadCancelled = false;
-        $("div.image-fast-upload").modal('hide');
+        $("div.image-upload-dialog").modal('hide');
     });
 });
 
@@ -34,21 +34,21 @@ var uploadCancelled = false;
 function uploadComplete(status, url){
     if(!uploadCancelled){
         if(status === "no_files"){
-            $("div.image-fast-upload input[type='submit']").removeAttr('disabled');
-            $("div.image-fast-upload input[type='reset']").removeAttr('disabled');
-            $("div.image-fast-upload div.upload-no-files").show();
-            $("div.image-fast-upload div.uploading").hide();
+            $("div.image-upload-dialog input[type='submit']").removeAttr('disabled');
+            $("div.image-upload-dialog input[type='reset']").removeAttr('disabled');
+            $("div.image-upload-dialog div.upload-no-files").show();
+            $("div.image-upload-dialog div.uploading").hide();
         } else if(status === "success"){
-            var description = $("div.image-fast-upload input[name='credits']").val();
-            var photographer = $("div.image-fast-upload input[name='photographer']").val();
-            $("div.image-fast-upload div.uploading").hide();
-            $("div.image-fast-upload").modal('hide');
+            var description = $("div.image-upload-dialog input[name='credits']").val();
+            var photographer = $("div.image-upload-dialog input[name='photographer']").val();
+            $("div.image-upload-dialog div.uploading").hide();
+            $("div.image-upload-dialog").modal('hide');
             uploadCompleteCallback(url, description, photographer);
         } else {//parse error or unexpected reply
-            $("div.image-fast-upload input[type='submit']").removeAttr('disabled');
-            $("div.image-fast-upload input[type='reset']").removeAttr('disabled');
-            $("div.image-fast-upload div.upload-failed").show();
-            $("div.image-fast-upload div.uploading").hide();
+            $("div.image-upload-dialog input[type='submit']").removeAttr('disabled');
+            $("div.image-upload-dialog input[type='reset']").removeAttr('disabled');
+            $("div.image-upload-dialog div.upload-failed").show();
+            $("div.image-upload-dialog div.uploading").hide();
         }
     }
 }
@@ -57,18 +57,18 @@ function openImageUpload(callback){
     uploadCancelled = false;
     uploadCompleteCallback = callback;
 
-    $("div.image-fast-upload").modal();
-    $("div.image-fast-upload input[type='submit']").removeAttr('disabled');
-    $("div.image-fast-upload input[type='reset']").removeAttr('disabled');
+    $("div.image-upload-dialog").modal();
+    $("div.image-upload-dialog input[type='submit']").removeAttr('disabled');
+    $("div.image-upload-dialog input[type='reset']").removeAttr('disabled');
     resetImageUpload();
 }
 
 function resetImageUpload(){
-    $("div.image-fast-upload input[type='reset']").click();
-    $("div.image-fast-upload input[name='tags-serialized']").val("");
-    $("div.image-fast-upload div.tag-box").empty();
+    $("div.image-upload-dialog input[type='reset']").click();
+    $("div.image-upload-dialog input[name='tags-serialized']").val("");
+    $("div.image-upload-dialog div.tag-box").empty();
 
-    $("div.image-fast-upload div.uploading").hide();
-    $("div.image-fast-upload div.upload-failed").hide();
-    $("div.image-fast-upload div.upload-no-files").hide();
+    $("div.image-upload-dialog div.uploading").hide();
+    $("div.image-upload-dialog div.upload-failed").hide();
+    $("div.image-upload-dialog div.upload-no-files").hide();
 }
