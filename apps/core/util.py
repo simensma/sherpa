d2e4571933e9759f0d6665023a589f3d7e5a64ac
Ali Kaafarani <ali@kvikshaug.no>
2013-02-26 10:44:27 +0100

    Reverse the logic order

--- apps/core/util.py
+++ apps/core/util.py
@@ -5,19 +5,21 @@ from django.conf import settings
 import re
 
 def use_image_thumb(url, preferred_size):
-    if url.find("%s/%s" % (settings.AWS_BUCKET, settings.AWS_IMAGEGALLERY_PREFIX)) != -1:
-        pre, post = url.rsplit('.', 1)
+    if url.find("%s/%s" % (settings.AWS_BUCKET, settings.AWS_IMAGEGALLERY_PREFIX)) == -1:
+        # Not a file from our image archive, don't modify it
+        return url
 
-        # If it's already a thumb, use the original
-        for thumb in settings.THUMB_SIZES:
-            if pre.endswith('-%s' % thumb):
-                pre = re.sub('-%s' % thumb, '', pre)
+    pre, post = url.rsplit('.', 1)
 
-        larger_than_preferred = [t for t in settings.THUMB_SIZES if t >= preferred_size]
-        if len(larger_than_preferred) == 0:
-            # No thumbs are larger than the requested size, just use the original image
-            return "%s.%s" % (pre, post)
-        else:
-            appropriate_size = min(larger_than_preferred)
-        return "%s-%s.%s" % (pre, appropriate_size, post)
-    return url
+    # If it's already a thumb, use the original
+    for thumb in settings.THUMB_SIZES:
+        if pre.endswith('-%s' % thumb):
+            pre = re.sub('-%s' % thumb, '', pre)
+
+    larger_than_preferred = [t for t in settings.THUMB_SIZES if t >= preferred_size]
+    if len(larger_than_preferred) == 0:
+        # No thumbs are larger than the requested size, just use the original image
+        return "%s.%s" % (pre, post)
+    else:
+        appropriate_size = min(larger_than_preferred)
+    return "%s-%s.%s" % (pre, appropriate_size, post)
