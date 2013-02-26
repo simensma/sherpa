e715ec34c2c5d4c1f835f54bea099eacd5f094bd
Ali Kaafarani <ali@kvikshaug.no>
2013-02-26 10:08:05 +0100

    Fix js syntax

--- static/js/admin/editor/image-utils.js
+++ static/js/admin/editor/image-utils.js
@@ -15,11 +15,11 @@ var DEFAULT_CROP_RATIO = PREDEFINED_CROP_RATIOS.Fri;
 
 var DEFAULT_IMAGE = "http://www.turistforeningen.no/static/img/placeholder.png";
 
-function getRatioRadioButtons(){
+function getRatioRadioButtons() {
     var ratioradio = "<table><tr>";
     var r = PREDEFINED_CROP_RATIOS;
     for(var key in r) {
-        if(r.hasOwnProperty(key)){
+        if(r.hasOwnProperty(key)) {
             ratioradio += "<td><input type='radio' name='ratio' value='" + r[key] + "'> " + key + " </td>";
         }
     }
@@ -27,9 +27,9 @@ function getRatioRadioButtons(){
     return ratioradio;
 }
 
-function addImageSizeToUrl(url, size){
+function addImageSizeToUrl(url, size) {
     //if url not from turistforeningen, do nothing
-    if(url.indexOf(LOCAL_IMAGE_PREFIX) < 0){
+    if(url.indexOf(LOCAL_IMAGE_PREFIX) < 0) {
         return url;
     }
 
@@ -37,38 +37,38 @@ function addImageSizeToUrl(url, size){
     url = url.replace(/-\d+/g, "");
 
     //the requested size is the original size
-    if(size == undefined){
+    if(size === undefined) {
         return url;
     }
 
     var parts = url.split(".");
     var newurl = parts[0];
-    for(var i = 1; i< parts.length-1; i++){
+    for(var i = 1; i< parts.length-1; i++) {
         newurl += "." + parts[i];
     }
     newurl += "-" + size + "." + parts[parts.length-1];
     return newurl;
 }
 
-function removeImageSizeFromUrl(url){
-    if(url.indexOf(LOCAL_IMAGE_PREFIX) < 0){
+function removeImageSizeFromUrl(url) {
+    if(url.indexOf(LOCAL_IMAGE_PREFIX) < 0) {
         return url;
     }
-    return url.replace(/-\d+/g, '')
+    return url.replace(/-\d+/g, '');
 }
 
-function bestSizeForImage(displayWidth){
-    for(var i = THUMB_SIZES.length-1; i >= 0; i--){
-        if(THUMB_SIZES[i] >= displayWidth){
+function bestSizeForImage(displayWidth) {
+    for(var i = THUMB_SIZES.length-1; i >= 0; i--) {
+        if(THUMB_SIZES[i] >= displayWidth) {
             return THUMB_SIZES[i];
         }
     }
     return undefined;
 }
 
-function ratioIsValid(width, height){
+function ratioIsValid(width, height) {
     var r = width/height;
-    if(!isNaN(r) && width > 0 && height > 0){
+    if(!isNaN(r) && width > 0 && height > 0) {
         return true;
     }
     return false;
