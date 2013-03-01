0f9ca4f14e86db21af66ba1408990b1f26e7c2af
Ali Kaafarani <ali@kvikshaug.no>
2013-03-01 12:07:11 +0100

    Hide access option if picked national association

--- static/js/admin/publications/edit_publication.js
+++ static/js/admin/publications/edit_publication.js
@@ -3,15 +3,22 @@ $(document).ready(function() {
     var input_logo = form.find("input[name='logo']");
     var logo_image = form.find("img.logo");
     var association_select = form.find("select[name='association']");
-    var access_association_name = form.find("div.control-group.access span.association-name");
+    var access = form.find("div.control-group.access");
+    var access_association_name = access.find("span.association-name");
 
     input_logo.change(function() {
         logo_image.attr('src', $(this).val());
     });
 
     association_select.change(function() {
-        var name = window.association_main_mappings[$(this).find("option:selected").val()];
-        access_association_name.text(name);
+        var selected = $(this).find("option:selected");
+        if(selected.attr("data-type") == "sentral") {
+            access.hide();
+        } else {
+            access.show();
+            var name = window.association_main_mappings[selected.val()];
+            access_association_name.text(name);
+        }
     });
 
     form.find("div.control-group.logo a.pick-from-image-archive").click(function() {
