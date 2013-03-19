87881eacf51f537622e4236e8143934df428b0fb
Ali Kaafarani <ali@kvikshaug.no>
2013-03-19 15:06:59 +0100

    Handle receipt-SMS ajax-error

--- static/js/enrollment/result.js
+++ static/js/enrollment/result.js
@@ -6,12 +6,12 @@ $(document).ready(function() {
         var button = $(this);
         var index = $(this).attr('data-index');
         var number = $(this).attr('data-number');
+        var memberserviceBackup = 'Dersom du/dere har planlagt å dra på tur i nærmeste fremtid, og ikke rekker å vente på at medlemskortet ankommer, kan dere kontakte medlemsservice for å få tilsendt kvittering på SMS.';
         $.ajax({
             url: '/innmelding/sms/',
             data: { index: index }
         }).done(function(result) {
             result = JSON.parse(result);
-            var memberserviceBackup = 'Dersom du/dere har planlagt å dra på tur i nærmeste fremtid, og ikke rekker å vente på at medlemskortet ankommer, kan dere kontakte medlemsservice for å få tilsendt kvittering på SMS.';
             if(result.error == 'none') {
                 button.after('<p class="receipt-success">Kvittering har blitt sendt på SMS til ' + number + '.</p>');
                 button.remove();
@@ -32,7 +32,8 @@ $(document).ready(function() {
                 button.remove();
             }
         }).fail(function(result) {
-            // Todo
+            button.after('<p class="receipt-error">En teknisk feil har oppstått ved utsendelse av SMS. ' + memberserviceBackup + '</p>');
+            button.remove();
         });
     });
 
