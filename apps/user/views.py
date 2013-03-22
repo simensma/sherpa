880190bbb14954431f8c2cc17949df690f24b0ee
Ali Kaafarani <ali@kvikshaug.no>
2013-03-22 15:03:29 +0100

    Remove unused import

--- apps/user/views.py
+++ apps/user/views.py
@@ -5,7 +5,6 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib import messages
-from django.core.cache import cache
 from django.db.models import Q
 
 from datetime import datetime
