c217792963bf0b76aaf3ba9172dcbe13769f3ee8
Ali Kaafarani <ali@kvikshaug.no>
2013-03-22 12:55:59 +0100

    Set field name for receive_email on Actor

--- apps/focus/models.py
+++ apps/focus/models.py
@@ -83,6 +83,7 @@ class Actor(models.Model):
     end_code = models.CharField(max_length=5, db_column=u'EndCd')
 
     # Other relevant stuff
+    receive_email = models.BooleanField(db_column=u'OptBit1')
     reserved_against_partneroffers = models.BooleanField(db_column=u'OptBit3')
 
     orgno = models.CharField(max_length=50, db_column=u'OrgNo')
@@ -115,7 +116,6 @@ class Actor(models.Model):
     optchar7 = models.CharField(max_length=10, db_column=u'OptChar7')
     optchar8 = models.CharField(max_length=10, db_column=u'OptChar8')
     optchar9 = models.CharField(max_length=10, db_column=u'OptChar9')
-    optbit1 = models.BooleanField(db_column=u'OptBit1')
     optbit2 = models.BooleanField(db_column=u'OptBit2')
     optbit4 = models.BooleanField(db_column=u'OptBit4')
     optbit5 = models.BooleanField(db_column=u'OptBit5')
