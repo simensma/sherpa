571e6e95853c307e1695929e407814941f71a92a
Ali Kaafarani <ali@kvikshaug.no>
2013-04-02 14:30:30 +0200

    Use norwegian URL slugs

--- apps/aktiviteter/urls.py
+++ apps/aktiviteter/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('aktiviteter.views',
     url(r'^$', 'index'),
-    url(r'^show/(?P<aktivitet>\d+)/$', 'show'),
+    url(r'^vis/(?P<aktivitet>\d+)/$', 'show'),
 )
