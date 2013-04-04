d303dcb720da26731295031f7e5ebbd382aef04e
Ali Kaafarani <ali@kvikshaug.no>
2013-04-04 19:02:14 +0200

    Remove the last references to the old user page

--- project/sherpa/urls_redirects.py
+++ project/sherpa/urls_redirects.py
@@ -15,10 +15,6 @@ urlpatterns = patterns('',
     # Keep old admin-ui for now (difference is /admin/ vs /sherpa/)
     url(r'^admin/(?P<slug>.*)', 'page.views.redirect', kwargs={'url': 'http://%s/admin/' % settings.OLD_SITE}),
 
-    # Keep old user page
-    # Note: This URL uses hardcoded links in enrollment result pages and email receipts.
-    url(r'^minside/(?P<slug>.*)', 'page.views.redirect', kwargs={'url': 'http://%s/minside/' % settings.OLD_SITE}),
-
     # Old static content
     url(r'^img/(?P<slug>.*)', 'page.views.redirect', kwargs={'url': 'http://%s/img/' % settings.OLD_SITE}),
     url(r'^images/(?P<slug>.*)', 'page.views.redirect', kwargs={'url': 'http://%s/images/' % settings.OLD_SITE}),
