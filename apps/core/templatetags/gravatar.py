2ee5c139c51521956bd0b02b028b1fdb1d6b4633
Ali Kaafarani <ali@kvikshaug.no>
2013-04-05 10:01:24 +0200

    Use register as decorator consistently

--- apps/core/templatetags/gravatar.py
+++ apps/core/templatetags/gravatar.py
@@ -6,8 +6,7 @@ register = template.Library()
 
 GRAVATAR_URL = "http://www.gravatar.com/avatar"
 
+@register.simple_tag
 def gravatar(email, size=40, default='mm'):
     hash = md5.new(email.strip().lower()).hexdigest()
     return "%s/%s?s=%s&d=%s" % (GRAVATAR_URL, hash, size, default)
-
-register.simple_tag(gravatar)
