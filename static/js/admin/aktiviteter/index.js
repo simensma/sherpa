064fef572576960d983a78f6de252b9d2482283d
Ali Kaafarani <ali@kvikshaug.no>
2013-04-09 17:39:56 +0200

    Add initial category UI

---
+++ static/js/admin/aktiviteter/index.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    var listing = $("div.aktivitet-listing");
+    var new_categories = listing.find("div.new-categories");
+    var form = new_categories.find("form.create-new-category");
+    var form_category = form.find("input[name='category']");
+
+    listing.find("a.create-new").click(function() {
+        $(this).parent().hide();
+        new_categories.slideDown();
+    });
+
+    form.find("input[type='submit']").click(function() {
+        form_category.val($(this).attr('data-tag-name'));
+    });
+});
