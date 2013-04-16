66f9486ec3e9018e65fb651c1ad7353a0c97a5d4
Ali Kaafarani <ali@kvikshaug.no>
2013-04-16 11:11:51 +0200

    Don't use a 2-step slider for creating new

--- static/js/admin/aktiviteter/index.js
+++ static/js/admin/aktiviteter/index.js
@@ -4,11 +4,6 @@ $(document).ready(function() {
     var form = new_categories.find("form.create-new-category");
     var form_category = form.find("input[name='category']");
 
-    listing.find("a.create-new").click(function() {
-        $(this).parent().hide();
-        new_categories.slideDown();
-    });
-
     form.find("input[type='submit']").click(function() {
         form_category.val($(this).attr('data-tag-name'));
     });
