0b413a0983638743c6b8bc61b8594480bde9bccf
Ali Kaafarani <ali@kvikshaug.no>
2013-05-02 12:49:25 +0200

    Enable positioning marker with popup details

--- static/js/admin/aktiviteter/edit/position.js
+++ static/js/admin/aktiviteter/edit/position.js
@@ -1,7 +1,38 @@
 $(document).ready(function() {
-    // var map = L.map('map').setView([51.505, -0.09], 8);
+
+    var wrapper = $("div.edit-aktivitet-position");
+    var map_element = wrapper.find("div.leaflet-map");
+    var popup_content = wrapper.find("div.popup-content").html();
+
     var map = L.map('map').setView([65, 12], 5);
     L.tileLayer('http://opencache.statkart.no/gatekeeper/gk/gk.open_gmaps?layers=topo2&zoom={z}&x={x}&y={y}', {
         attribution: 'Kartverket'
     }).addTo(map);
+    var marker;
+
+    map.addControl(new L.Control.Draw({
+        position: 'topleft',
+        polyline: false,
+        polygon: false,
+        rectangle: false,
+        circle: false,
+        marker: {
+            title: "Velg startposisjon"
+        }
+    }));
+
+    map.on('draw:marker-created', function(e) {
+        if(typeof marker !== "undefined") {
+            marker.setLatLng(e.marker.getLatLng());
+            marker.update();
+        } else {
+            e.marker.bindPopup(popup_content).addTo(map);
+            marker = e.marker;
+        }
+        marker.openPopup();
+    });
+
+    map_element.find("a.leaflet-control-draw-marker").tooltip({
+        placement: 'right'
+    });
 });
