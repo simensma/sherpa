86e3741cc23ad8b94d745a682f6f752df00efa7e
Ali Kaafarani <ali@kvikshaug.no>
2013-05-02 09:28:52 +0200

    Tweak initial map settings

--- static/js/admin/aktiviteter/edit/position.js
+++ static/js/admin/aktiviteter/edit/position.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     // var map = L.map('map').setView([51.505, -0.09], 8);
-    var map = L.map('map').setView([59.230596, 7.537651], 13);
+    var map = L.map('map').setView([65, 12], 5);
     L.tileLayer('http://opencache.statkart.no/gatekeeper/gk/gk.open_gmaps?layers=topo2&zoom={z}&x={x}&y={y}', {
         attribution: 'Kartverket'
     }).addTo(map);
