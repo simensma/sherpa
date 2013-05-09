5acd36bcbdd6d555f76ecaf8bc6bee891ed5cde7
Ali Kaafarani <ali@kvikshaug.no>
2013-05-09 10:21:12 +0200

    Perform lookups from just the form element

--- static/js/user/update_account.js
+++ static/js/user/update_account.js
@@ -1,20 +1,22 @@
 $(document).ready(function() {
 
+    var form = $("form.account-info");
+
     Validator.validate({
         method: 'full_name',
-        control_group: $("div.control-group.name"),
-        input: $("input[name='name']"),
+        control_group: form.find("div.control-group.name"),
+        input: form.find("input[name='name']"),
         req: true
     });
 
     Validator.validate({
         method: 'email',
-        control_group: $("div.control-group.email"),
-        input: $("input[name='email']"),
+        control_group: form.find("div.control-group.email"),
+        input: form.find("input[name='email']"),
         req: true
     });
 
-    var control_group = $("div.control-group.sherpa-email");
+    var control_group = form.find("div.control-group.sherpa-email");
     if(control_group.length > 0) {
         Validator.validate({
             method: 'email',
@@ -26,41 +28,41 @@ $(document).ready(function() {
 
     Validator.validate({
         method: 'phone',
-        control_group: $("div.control-group.phone_home"),
-        input: $("input[name='phone_home']"),
+        control_group: form.find("div.control-group.phone_home"),
+        input: form.find("input[name='phone_home']"),
         req: false
     });
 
     Validator.validate({
         method: 'phone',
-        control_group: $("div.control-group.phone_mobile"),
-        input: $("input[name='phone_mobile']"),
+        control_group: form.find("div.control-group.phone_mobile"),
+        input: form.find("input[name='phone_mobile']"),
         req: false
     });
 
     Validator.validate({
         method: 'address',
-        control_group: $("div.control-group.address"),
-        input: $("input[name='address']"),
+        control_group: form.find("div.control-group.address"),
+        input: form.find("input[name='address']"),
         req: true
     });
 
     Validator.validateZipcode(
-        $("div.control-group.zipcode"),
-        $("input[name='zipcode']"),
-        $("input[name='area']"),
-        $("img.ajaxloader.zipcode")
+        form.find("div.control-group.zipcode"),
+        form.find("input[name='zipcode']"),
+        form.find("input[name='area']"),
+        form.find("img.ajaxloader.zipcode")
     );
 
     // Trigger all validations
     Validator.trigger();
-    Validator.triggerZipcode($("input[name='zipcode']"));
+    Validator.triggerZipcode(form.find("input[name='zipcode']"));
 
-    $("input[name='toggle-sherpa-email']").change(function() {
+    form.find("input[name='toggle-sherpa-email']").change(function() {
         if($(this).is(':checked')) {
-            $("input[name='sherpa-email']").removeAttr('readonly');
+            form.find("input[name='sherpa-email']").removeAttr('readonly');
         } else {
-            $("input[name='sherpa-email']").val('').attr('readonly', true).focusout();
+            form.find("input[name='sherpa-email']").val('').attr('readonly', true).focusout();
         }
     });
 
