c742a53b65906561f5706fe5caef0b944b4d0d53
Ali Kaafarani <ali@kvikshaug.no>
2013-06-06 16:45:14 +0200

    Restructure aktivitet-listing markup

--- static/js/aktiviteter/index.js
+++ static/js/aktiviteter/index.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-    var filters = $("div.aktivitet-search-filters");
+    var listing = $("div.aktivitet-listing");
+    var filters = listing.find("div.search-filters");
 
     filters.find("select").chosen({
         'allow_single_deselect': true
