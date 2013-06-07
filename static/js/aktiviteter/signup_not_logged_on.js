934c760c46bdcb8cad0d1ed331cf5dc942458df3
Ali Kaafarani <ali@kvikshaug.no>
2013-06-07 17:29:02 +0200

    Add more choices to signup-page

--- static/js/aktiviteter/signup_not_logged_on.js
+++ static/js/aktiviteter/signup_not_logged_on.js
@@ -4,7 +4,22 @@ $(document).ready(function() {
     var register = signup.find("div.choice.register");
     var simple_signup = signup.find("div.choice.simple-signup");
 
+    login.click(function(e) {
+        $(this).find("p.action a").get(0).click();
+    });
+
+    login.find("p.action a").click(function(e) {
+        e.stopPropagation();
+    });
+
+    register.click(function() {
+        $(this).addClass('active');
+        $(this).find("p.action").hide();
+        $(this).find("div.content").slideDown();
+    });
+
     simple_signup.click(function() {
+        $(this).addClass('active');
         $(this).find("p.action").hide();
         $(this).find("div.content").slideDown();
     });
