32f06ec32c752a63c6d7b247c79a1bf4bf404aff
Ali Kaafarani <ali@kvikshaug.no>
2013-06-07 18:01:27 +0200

    Add client-side validations

--- static/js/aktiviteter/signup_not_logged_on.js
+++ static/js/aktiviteter/signup_not_logged_on.js
@@ -23,4 +23,32 @@ $(document).ready(function() {
         $(this).find("p.action").hide();
         $(this).find("div.content").slideDown();
     });
+
+    Validator.validate({
+        method: 'full_name',
+        control_group: simple_signup.find("div.control-group.name"),
+        input: simple_signup.find("input[name='name']"),
+        req: true
+    });
+
+    Validator.validate({
+        method: 'email',
+        control_group: simple_signup.find("div.control-group.email"),
+        input: simple_signup.find("input[name='email']"),
+        req: false
+    });
+
+    Validator.validate({
+        method: 'phone',
+        control_group: simple_signup.find("div.control-group.phone"),
+        input: simple_signup.find("input[name='phone']"),
+        req: false
+    });
+
+    simple_signup.find("form.simple-signup").submit(function(e) {
+        if($(this).find("input[name='email']").val().trim() === '' && $(this).find("input[name='phone']").val().trim() === '') {
+            alert("Du må oppgi enten et telefonnummer eller en e-postadresse.");
+            e.preventDefault();
+        }
+    });
 });
