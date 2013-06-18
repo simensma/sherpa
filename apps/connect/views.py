af3b1f2803bee7ddef2c5c315b0311c40828d187
Ali Kaafarani <ali@kvikshaug.no>
2013-06-18 15:42:11 +0200

    Add and separate memberdata-retrieval

--- apps/connect/views.py
+++ apps/connect/views.py
@@ -24,19 +24,8 @@ def connect(request):
             'status': 'not logged on'
         })
     else:
-        if request.user.get_profile().is_member():
-            data = json.dumps({
-                'sherpa_id': request.user.get_profile().id,
-                'fornavn': request.user.get_profile().get_first_name(),
-                'etternavn': request.user.get_profile().get_last_name(),
-                'epost': request.user.get_profile().get_email()
-            })
-        else:
-            data = json.dumps({
-                'sherpa_id': request.user.get_profile().id,
-                'memberid': request.user.get_profile().memberid
-                # Todo: Legg inn alle parameterne
-            })
+        memberdata = get_member_data(request.user)
+        return memberdata
 
     encrypted_data = encrypt(client['shared_secret'], data)
 
@@ -63,3 +52,46 @@ def decrypt(key, encoded):
     msg_padded = cipher.decrypt(ciphertext)
     msg = pkcs7.decode(msg_padded, len(key))
     return msg
+
+def get_member_data(user):
+    if not user.get_profile().is_member():
+        return {
+            'sherpa_id': user.get_profile().id,
+            'er_medlem': False,
+            'fornavn': user.get_profile().get_first_name(),
+            'etternavn': user.get_profile().get_last_name(),
+            'epost': user.get_profile().get_email()
+        }
+    else:
+        # The gender definition is in norwegian
+        def api_gender_output(gender):
+            if gender == 'm':
+                return 'M'
+            elif gender == 'f':
+                return 'K'
+
+        address = user.get_profile().get_actor().get_clean_address()
+        return {
+            'sherpa_id': user.get_profile().id,
+            'er_medlem': True,
+            'medlemsnummer': user.get_profile().memberid,
+            'aktivt_medlemskap': user.get_profile().get_actor().has_paid(),
+            'etternavn': user.get_profile().get_last_name(),
+            'fornavn': user.get_profile().get_first_name(),
+            'etternavn': user.get_profile().get_last_name(),
+            'født': user.get_profile().get_actor().birth_date.strftime("%Y-%m-%d"),
+            'kjønn': api_gender_output(user.get_profile().get_actor().get_gender()),
+            'epost': user.get_profile().get_email(),
+            'mobil': user.get_profile().get_actor().phone_mobile,
+            'address': {
+                'adresse1': address.field1,
+                'adresse2': address.field2,
+                'adresse3': address.field3,
+                'postnummer': address.zipcode.zipcode if address.country.code == 'NO' else None,
+                'poststed': address.zipcode.area.title() if address.country.code == 'NO' else None,
+                'land': {
+                    'kode': address.country.code,
+                    'navn': address.country.name
+                }
+            }
+        }
