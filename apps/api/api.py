2f594ac9fd397bd2a62ca53c5c369b51176325f2
Ali Kaafarani <ali@kvikshaug.no>
2013-06-20 13:12:20 +0200

    Create inactive user if necessary

--- apps/api/api.py
+++ apps/api/api.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 import json
 
 from user.models import Profile
+from user.util import create_inactive_user
+from focus.models import Actor
 from exceptions import BadRequest
 from util import get_member_data
 
@@ -14,8 +16,16 @@ def members(request, version, format):
             elif 'sherpa_id' in request.GET:
                 profile = Profile.objects.get(id=request.GET['sherpa_id'])
             elif 'medlemsnummer' in request.GET:
-                # TODO: Create inactive profile if the memberid matches an Actor
-                profile = Profile.objects.get(memberid=request.GET['medlemsnummer'])
+                try:
+                    profile = Profile.objects.get(memberid=request.GET['medlemsnummer'])
+                except Profile.DoesNotExist as e:
+                    try:
+                        # Create an inactive profile if the memberid is valid
+                        actor = Actor.objects.get(memberid=request.GET['medlemsnummer'])
+                        profile = create_inactive_user(actor.memberid)
+                    except Actor.DoesNotExist:
+                        # Nope, just re-raise the original Profile.DoesNotExist
+                        raise e
             else:
                 raise BadRequest("You must supply either an 'sherpa_id' or 'medlemsnummer' parameter for member lookup")
             return HttpResponse(json.dumps(get_member_data(profile)))
