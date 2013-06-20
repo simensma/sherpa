7314890a4d4d563c3631708ac3763d063b9387b0
Ali Kaafarani <ali@kvikshaug.no>
2013-06-20 12:45:44 +0200

    Rename id lookup to sherpa_id

--- apps/api/api.py
+++ apps/api/api.py
@@ -9,15 +9,15 @@ from util import get_member_data
 def members(request, version, format):
     if request.method == 'GET':
         try:
-            if 'id' in request.GET and 'medlemsnummer' in request.GET:
-                profile = Profile.objects.get(id=request.GET['id'], memberid=request.GET['medlemsnummer'])
-            elif 'id' in request.GET:
-                profile = Profile.objects.get(id=request.GET['id'])
+            if 'sherpa_id' in request.GET and 'medlemsnummer' in request.GET:
+                profile = Profile.objects.get(id=request.GET['sherpa_id'], memberid=request.GET['medlemsnummer'])
+            elif 'sherpa_id' in request.GET:
+                profile = Profile.objects.get(id=request.GET['sherpa_id'])
             elif 'medlemsnummer' in request.GET:
                 # TODO: Create inactive profile if the memberid matches an Actor
                 profile = Profile.objects.get(memberid=request.GET['medlemsnummer'])
             else:
-                raise BadRequest("You must supply either an 'id' or 'medlemsnummer' parameter for member lookup")
+                raise BadRequest("You must supply either an 'sherpa_id' or 'medlemsnummer' parameter for member lookup")
             return HttpResponse(json.dumps(get_member_data(profile)))
         except Profile.DoesNotExist:
             return HttpResponse('does not exist, what should i do?')
