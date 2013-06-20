e013eede22e99942f2cf21e1328418bd75a9251c
Ali Kaafarani <ali@kvikshaug.no>
2013-06-20 15:15:09 +0200

    Redirect /api/ hits to documentation site

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import patterns, include, url
 
-urlpatterns = patterns('api.views',
-    url(r'^$', 'index'),
-    url(r'^medlemmer/$', 'index', kwargs={'resource': 'members'}),
+urlpatterns = patterns('',
+    url(r'^$', 'page.views.perform_redirect', kwargs={'url': 'http://docs.turistforeningen.no/'}),
+    url(r'^medlemmer/$', 'api.views.index', kwargs={'resource': 'members'}),
 )
