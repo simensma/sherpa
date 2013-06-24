c525ad5e6ab155c9005bfe8e623e7037aabd83fe
Ali Kaafarani <ali@kvikshaug.no>
2013-06-24 14:57:38 +0200

    Allow longer charfields in image model

---
+++ apps/admin/migrations/0019_auto__chg_field_image_photographer__chg_field_image_credits__chg_field.py
@@ -0,0 +1,201 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+
+        # Changing field 'Image.photographer'
+        db.alter_column(u'admin_image', 'photographer', self.gf('django.db.models.fields.CharField')(max_length=255))
+
+        # Changing field 'Image.credits'
+        db.alter_column(u'admin_image', 'credits', self.gf('django.db.models.fields.CharField')(max_length=255))
+
+        # Changing field 'Image.licence'
+        db.alter_column(u'admin_image', 'licence', self.gf('django.db.models.fields.CharField')(max_length=1023))
+
+    def backwards(self, orm):
+
+        # Changing field 'Image.photographer'
+        db.alter_column(u'admin_image', 'photographer', self.gf('django.db.models.fields.CharField')(max_length=200))
+
+        # Changing field 'Image.credits'
+        db.alter_column(u'admin_image', 'credits', self.gf('django.db.models.fields.CharField')(max_length=200))
+
+        # Changing field 'Image.licence'
+        db.alter_column(u'admin_image', 'licence', self.gf('django.db.models.fields.CharField')(max_length=200))
+
+    models = {
+        u'admin.album': {
+            'Meta': {'object_name': 'Album'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'parent': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['admin.Album']", 'null': 'True'})
+        },
+        u'admin.image': {
+            'Meta': {'object_name': 'Image'},
+            'album': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['admin.Album']", 'null': 'True'}),
+            'credits': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'description': ('django.db.models.fields.TextField', [], {}),
+            'exif': ('django.db.models.fields.TextField', [], {}),
+            'extension': ('django.db.models.fields.CharField', [], {'max_length': '4'}),
+            'hash': ('django.db.models.fields.CharField', [], {'max_length': '40'}),
+            'height': ('django.db.models.fields.IntegerField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'key': ('django.db.models.fields.CharField', [], {'max_length': '8'}),
+            'licence': ('django.db.models.fields.CharField', [], {'max_length': '1023'}),
+            'photographer': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'tags': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'images'", 'symmetrical': 'False', 'to': u"orm['core.Tag']"}),
+            'uploaded': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'uploader': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['user.Profile']"}),
+            'width': ('django.db.models.fields.IntegerField', [], {})
+        },
+        u'admin.imagerecovery': {
+            'Meta': {'object_name': 'ImageRecovery'},
+            'album': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['admin.Album']", 'null': 'True'}),
+            'credits': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'description': ('django.db.models.fields.TextField', [], {}),
+            'exif': ('django.db.models.fields.TextField', [], {}),
+            'extension': ('django.db.models.fields.CharField', [], {'max_length': '4'}),
+            'hash': ('django.db.models.fields.CharField', [], {'max_length': '40'}),
+            'height': ('django.db.models.fields.IntegerField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'key': ('django.db.models.fields.CharField', [], {'max_length': '8'}),
+            'licence': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'photographer': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'tags': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'+'", 'symmetrical': 'False', 'to': u"orm['core.Tag']"}),
+            'uploaded': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'width': ('django.db.models.fields.IntegerField', [], {})
+        },
+        u'admin.publication': {
+            'Meta': {'object_name': 'Publication'},
+            'access': ('django.db.models.fields.CharField', [], {'default': "'all'", 'max_length': '255'}),
+            'association': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['association.Association']"}),
+            'description': ('django.db.models.fields.TextField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'license': ('django.db.models.fields.CharField', [], {'default': "'all_rights_reserved'", 'max_length': '255'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        u'admin.release': {
+            'Meta': {'object_name': 'Release'},
+            'cover_photo': ('django.db.models.fields.CharField', [], {'max_length': '2048'}),
+            'description': ('django.db.models.fields.TextField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'online_view': ('django.db.models.fields.CharField', [], {'max_length': '2048'}),
+            'pdf_file_size': ('django.db.models.fields.IntegerField', [], {'default': 'None', 'null': 'True'}),
+            'pdf_hash': ('django.db.models.fields.CharField', [], {'max_length': '40'}),
+            'pub_date': ('django.db.models.fields.DateTimeField', [], {}),
+            'publication': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'releases'", 'to': u"orm['admin.Publication']"}),
+            'tags': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'releases'", 'symmetrical': 'False', 'to': u"orm['core.Tag']"}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        u'association.association': {
+            'Meta': {'object_name': 'Association'},
+            'counties': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'associations'", 'symmetrical': 'False', 'to': u"orm['core.County']"}),
+            'email': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255'}),
+            'facebook_url': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '2048'}),
+            'focus_id': ('django.db.models.fields.IntegerField', [], {'default': 'None', 'null': 'True'}),
+            'gmap_url': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '2048'}),
+            'group_type': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'organization_no': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255'}),
+            'parent': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'children'", 'null': 'True', 'to': u"orm['association.Association']"}),
+            'phone': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255'}),
+            'post_address': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255'}),
+            'site': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['core.Site']", 'unique': 'True', 'null': 'True'}),
+            'type': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'visit_address': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255'}),
+            'zipcode': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['core.Zipcode']", 'null': 'True'})
+        },
+        u'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'core.county': {
+            'Meta': {'object_name': 'County'},
+            'code': ('django.db.models.fields.CharField', [], {'max_length': '2'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'core.site': {
+            'Meta': {'object_name': 'Site'},
+            'domain': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'prefix': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'template': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['core.SiteTemplate']"})
+        },
+        u'core.sitetemplate': {
+            'Meta': {'object_name': 'SiteTemplate'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'core.tag': {
+            'Meta': {'object_name': 'Tag'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '200'})
+        },
+        u'core.zipcode': {
+            'Meta': {'object_name': 'Zipcode'},
+            'area': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'city': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'city_code': ('django.db.models.fields.CharField', [], {'max_length': '4'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'zipcode': ('django.db.models.fields.CharField', [], {'max_length': '4'})
+        },
+        u'user.associationrole': {
+            'Meta': {'object_name': 'AssociationRole'},
+            'association': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['association.Association']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'profile': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['user.Profile']"}),
+            'role': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        u'user.profile': {
+            'Meta': {'object_name': 'Profile'},
+            'associations': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'users'", 'symmetrical': 'False', 'through': u"orm['user.AssociationRole']", 'to': u"orm['association.Association']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'memberid': ('django.db.models.fields.IntegerField', [], {'unique': 'True', 'null': 'True'}),
+            'password_restore_date': ('django.db.models.fields.DateTimeField', [], {'null': 'True'}),
+            'password_restore_key': ('django.db.models.fields.CharField', [], {'max_length': '40', 'null': 'True'}),
+            'sherpa_email': ('django.db.models.fields.EmailField', [], {'max_length': '75'}),
+            'user': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['auth.User']", 'unique': 'True'})
+        }
+    }
+
+    complete_apps = ['admin']
