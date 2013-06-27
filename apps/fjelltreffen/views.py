fbb8f83dc806f9f85b9f6d17b4d68a500af0a417
Ali Kaafarani <ali@kvikshaug.no>
2013-06-27 14:51:58 +0200

    Remove unused import

--- apps/fjelltreffen/views.py
+++ apps/fjelltreffen/views.py
@@ -4,7 +4,6 @@ from django.template.loader import render_to_string
 from django.template import RequestContext
 from django.contrib import messages
 from django.http import HttpResponse
-from django.core.urlresolvers import reverse
 from django.core.mail import send_mail
 from django.conf import settings
 from django.core.exceptions import PermissionDenied
