21af5ea801b16301f7543cb43b66e21afd754482
Ali Kaafarani <ali@kvikshaug.no>
2013-07-25 16:43:48 +0200

    Refactor infinite-scroller to be more generic

--- static/js/admin/articles.js
+++ static/js/admin/articles.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 
     var table = $("table.article-listing");
+    var container = table.children("tbody");
 
     $("a.new-article").click(function() {
         $("div.new-article").modal();
@@ -15,11 +16,20 @@ $(document).ready(function() {
         $(this).parents("form").submit();
     });
 
+    container.data('bulk', 1);
     InfiniteScroller.enable({
-        container: table.children("tbody"),
+        url: container.attr('data-infinite-scroll-url'),
         triggerType: 'scroll',
-        trigger: table.children("tbody"),
-        loader: $("div.infinite-scroll-loader")
+        trigger: container,
+        container: container,
+        loader: $("div.infinite-scroll-loader"),
+        ajaxData: function() {
+            var bulk = Number(container.data('bulk'));
+            container.data('bulk', bulk + 1);
+            return {
+                bulk: bulk
+            };
+        }
     });
 
 });
--- static/js/infinite-scroller.js
+++ static/js/infinite-scroller.js
@@ -1,10 +1,36 @@
 (function(InfiniteScroller, $, undefined) {
 
-    var container;
+    // Options
+    var url;
+    var ajaxData = function() { return {}; };
     var trigger;
     var triggerType;
+    var container;
     var loader;
-    var handlers;
+    var handlers = { // default ajax handlers
+        done: function(result) {
+            result = $(result.trim());
+            if(result.length === 0) {
+                if(loader !== undefined) {
+                    loader.fadeOut();
+                }
+                complete = true;
+                return;
+            }
+            container.append(result);
+        }, fail: function(result) {
+            alert("Beklager, det oppstod en feil når vi forsøkte å laste flere elementer. Prøv å oppdatere siden, og scrolle ned igjen.");
+        }, always: function(result) {
+            loading = false;
+            if(loader !== undefined) {
+                loader.fadeOut();
+            }
+            if(triggerType === 'button') {
+                trigger.show();
+            }
+        }
+    };
+
     var loading = false;
     var complete = false;
     var windowLoaded = false;
@@ -14,36 +40,17 @@
     });
 
     InfiniteScroller.enable = function(opts) {
-        container = opts.container;
+        url = opts.url;
+        if(opts.ajaxData !== undefined) {
+            ajaxData = opts.ajaxData;
+        }
         trigger = opts.trigger;
         triggerType = opts.triggerType;
+        container = opts.container;
         loader = opts.loader;
         if(opts.handlers !== undefined) {
             handlers = opts.handlers;
-        } else {
-            // Default ajax handlers
-            handlers = {
-                done: function(result) {
-                    result = $(result.trim());
-                    if(result.length === 0) {
-                        loader.fadeOut();
-                        complete = true;
-                        return;
-                    }
-                    container.data('infinite-scroller-bulk', Number(container.data('infinite-scroller-bulk')) + 1);
-                    container.append(result);
-                }, fail: function(result) {
-                    alert("Beklager, det oppstod en feil når vi forsøkte å laste flere elementer. Prøv å oppdatere siden, og scrolle ned igjen.");
-                }, always: function(result) {
-                    loading = false;
-                    loader.fadeOut();
-                    if(triggerType === 'button') {
-                        trigger.show();
-                    }
-                }
-            };
         }
-        container.data('infinite-scroller-bulk', 1);
 
         if(triggerType === 'scroll') {
             // The enable function could be called before or after window load, so make sure the scroll
@@ -53,20 +60,18 @@
             } else {
                 $(window).load(addScrollEvent);
             }
+            function addScrollEvent() {
+                $(window).on('scroll.infinite-scroller', function() {
+                    var scrollLimit = trigger.offset().top + trigger.height();
+                    if(!loading && !complete && $(window).scrollTop() + $(window).height() > scrollLimit) {
+                        loading = true;
+                        load();
+                    }
+                });
+            }
         } else if(triggerType === 'button') {
             trigger.on('click.infinite-scroller', load);
         }
-
-        function addScrollEvent() {
-            $(window).on('scroll.infinite-scroller', function() {
-                var scrollLimit = trigger.offset().top + trigger.height();
-                if(!loading && !complete && $(window).scrollTop() + $(window).height() > scrollLimit) {
-                    loading = true;
-                    load();
-                }
-            });
-        }
-
     };
 
     InfiniteScroller.disable = function(opts) {
@@ -78,13 +83,15 @@
     };
 
     function load() {
-        loader.show();
+        if(loader !== undefined) {
+            loader.show();
+        }
         if(triggerType === 'button') {
             trigger.hide();
         }
         $.ajaxQueue({
-            url: container.attr('data-infinite-scroll-url'),
-            data: { bulk: container.data('infinite-scroller-bulk') }
+            url: url,
+            data: ajaxData()
         }).done(handlers.done).fail(handlers.fail).always(handlers.always);
     }
 
