69bb222c36df2cf173b9fa0f42dafec56cd1dd45
Ali Kaafarani <ali@kvikshaug.no>
2013-09-10 15:30:32 +0200

    Handle iframe-render errors on upload

--- static/js/admin/images/uploader.js
+++ static/js/admin/images/uploader.js
@@ -1,20 +1,31 @@
 var userReady = false;
 var uploadReady = false;
 
+var iframeCallbackSuccess = false;
+var iframeUploadComplete;
+
 $(document).ready(function() {
 
+    var uploader = $("div.uploader");
+    var uploading = uploader.find("div.uploading");
+    var licencing = uploader.find("div.licencing");
+    var image_details = uploader.find("div.image-details");
+    var form_image = uploader.find("form.image-uploader");
+    var form_details = image_details.find("form.upload-image-details");
+    var iframe = uploader.find("iframe");
+
     $("div.imagearchive-action-buttons button.upload").click(function() {
-        $("div.uploader").toggle('slow');
+        uploader.toggle('slow');
     });
 
     TagDisplay.enable({
-        targetInput: $("div.image-details input[name='tags-serialized']"),
-        tagBox: $("div.image-details div.tag-box"),
-        pickerInput: $("div.image-details input[name='tags']")
+        targetInput: image_details.find("input[name='tags-serialized']"),
+        tagBox: image_details.find("div.tag-box"),
+        pickerInput: image_details.find("input[name='tags']")
     });
 
     /* Changing destination album */
-    $("a.albumpicker-trigger").click(function() {
+    uploader.find("a.albumpicker-trigger").click(function() {
         AlbumPicker.open({
             album_id: $(this).attr('data-albumpicker-id'),
             allow_root: false,
@@ -23,22 +34,32 @@ $(document).ready(function() {
                 if(album.name === '') {
                     album.name = "(Legges ikke i album)";
                 }
-                var form = $("form.upload-image-details");
-                form.find("input[name='album']").val(album.id);
-                form.find("p.chosen-album span.display-name").text(album.name);
-                form.find("p.chosen-album a").attr('data-albumpicker-id', album.id);
+                form_details.find("input[name='album']").val(album.id);
+                form_details.find("p.chosen-album span.display-name").text(album.name);
+                form_details.find("p.chosen-album a").attr('data-albumpicker-id', album.id);
             }
         });
     });
 
-    $("form.image-uploader input[type='submit']").click(function() {
-        $("form.image-uploader").hide();
-        $("div.licencing").hide();
-        $("div.uploading").show();
-        $("div.image-details").show();
+    form_image.find("input[type='submit']").click(function() {
+        form_image.hide();
+        licencing.hide();
+        uploading.show();
+        image_details.show();
     });
 
-    var photographer = $("div.image-details form input[name='photographer']");
+    iframe.load(function() {
+        setTimeout(function() {
+            if(!iframeCallbackSuccess) {
+                // Well, the iframe loaded but callback-success wasn't set. Maybe a timeout or something happened,
+                // we didn't render the expected result at least. Show an error.
+                uploading.hide();
+                uploader.find("div.upload-iframe-render-failed").show();
+            }
+        }, 1000);
+    });
+
+    var photographer = form_details.find("input[name='photographer']");
     photographer.typeahead({
         minLength: 3,
         source: function(query, process) {
@@ -51,7 +72,7 @@ $(document).ready(function() {
         }
     });
 
-    $("div.image-details form").submit(function(e) {
+    form_details.submit(function(e) {
         if(uploadReady && userReady) {
             TagDisplay.collect();
         } else {
@@ -59,36 +80,37 @@ $(document).ready(function() {
         }
     });
 
-    $("div.image-details button[type='submit']").click(function(e) {
+    image_details.find("button[type='submit']").click(function(e) {
         userReady = true;
         if(!uploadReady) {
             e.preventDefault();
             $(this).prop('disabled', true);
-            $("div.image-details p.waiting").show();
+            image_details.find("p.waiting").show();
         }
     });
-});
 
-function iframeUploadComplete(result) {
-    $("div.uploading").hide();
-    if(result.status == 'success') {
-        $("div.upload-complete").show();
-        $("div.image-details input[name='ids']").val(JSON.stringify(result.ids));
-        uploadReady = true;
-        if(userReady) {
-            $("div.image-details form").trigger('submit');
+    iframeUploadComplete = function(result) {
+        iframeCallbackSuccess = true;
+        uploading.hide();
+        if(result.status == 'success') {
+            uploader.find("div.upload-complete").show();
+            image_details.find("input[name='ids']").val(JSON.stringify(result.ids));
+            uploadReady = true;
+            if(userReady) {
+                form_details.trigger('submit');
+            }
+        } else if(result.status == 'no_files') {
+            uploader.find("div.upload-no-files").show();
+            image_details.hide();
+            form_image.show();
+        } else if(result.status == 'parse_error') {
+            uploader.find("div.upload-failed").show();
+            image_details.hide();
+            form_image.show();
+        } else if(result.status == 'unknown_exception') {
+            uploader.find("div.upload-unknown-exception").show();
+            image_details.hide();
+            form_image.show();
         }
-    } else if(result.status == 'no_files') {
-        $("div.upload-no-files").show();
-        $("div.image-details").hide();
-        $("form.image-uploader").show();
-    } else if(result.status == 'parse_error') {
-        $("div.upload-failed").show();
-        $("div.image-details").hide();
-        $("form.image-uploader").show();
-    } else if(result.status == 'unknown_exception') {
-        $("div.upload-unknown-exception").show();
-        $("div.image-details").hide();
-        $("form.image-uploader").show();
-    }
-}
+    };
+});
