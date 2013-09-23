7889696057cbb74165fed404029b45071e55a29d
Ali Kaafarani <ali@kvikshaug.no>
2013-09-23 11:56:23 +0200

    Split connect-methods into separate views

--- apps/connect/urls.py
+++ apps/connect/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('connect.views',
-    url(r'^bounce/$', 'connect', kwargs={'method': 'bounce'}),
-    url(r'^signon/$', 'connect', kwargs={'method': 'signon'}),
+    url(r'^bounce/$', 'bounce'),
+    url(r'^signon/$', 'signon'),
     url(r'^signon/login/$', 'signon_login'),
 )
--- apps/connect/views.py
+++ apps/connect/views.py
@@ -5,25 +5,26 @@ from django.shortcuts import render, redirect
 from connect.util import get_request_data, prepare_response
 from api.util import get_member_data
 
-def connect(request, method):
+def bounce(request):
     client, request_data, redirect_url = get_request_data(request)
 
-    response_data = {}
-
-    if method == 'bounce':
-        response_data['er_autentisert'] = request.user.is_authenticated()
-
+    response_data = {'er_autentisert': request.user.is_authenticated()}
     if request.user.is_authenticated():
         response_data.update(get_member_data(request.user))
-    else:
-        if method == 'signon':
-            request.session['dntconnect'] = {
-                'client': client,
-                'redirect_url': redirect_url
-            }
-            return redirect('connect.views.signon_login')
-        # The only other method is bounce; in which case we'll just send the response as is
 
+    return prepare_response(client, response_data, redirect_url)
+
+def signon(request):
+    client, request_data, redirect_url = get_request_data(request)
+
+    if not request.user.is_authenticated():
+        request.session['dntconnect'] = {
+            'client': client,
+            'redirect_url': redirect_url
+        }
+        return redirect('connect.views.signon_login')
+
+    response_data = get_member_data(request.user)
     return prepare_response(client, response_data, redirect_url)
 
 def signon_login(request):
