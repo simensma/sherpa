4abd3fe86ff9caa1412744ffc0d33f3e2e7aaca2
Ali Kaafarani <ali@kvikshaug.no>
2013-11-01 20:05:24 +0100

    Hide the map by default

--- static/js/admin/aktiviteter/edit/position.js
+++ static/js/admin/aktiviteter/edit/position.js
@@ -5,44 +5,56 @@ $(document).ready(function() {
     var position_section = editor.find("div.section.position");
     var map_element = position_section.find("div.leaflet-map");
     var popup_content = position_section.find("div.popup-content").html();
+    var show_on_map = position_section.find("p.intro.show-on-map");
+    var show_map = position_section.find("p.intro a.show-map");
 
-    var map = L.map('map').setView([65, 12], 5);
-    L.tileLayer('http://opencache.statkart.no/gatekeeper/gk/gk.open_gmaps?layers=topo2&zoom={z}&x={x}&y={y}', {
-        attribution: 'Kartverket'
-    }).addTo(map);
     var marker;
 
-    // From js_globals
-    if(Turistforeningen.start_point_lat !== undefined && Turistforeningen.start_point_lng !== undefined) {
-        marker = new L.Marker(new L.LatLng(Turistforeningen.start_point_lat, Turistforeningen.start_point_lng), {
-            'title': 'Turen starter her'
-        }).bindPopup(popup_content).addTo(map);
-    }
+    var initiate_map = function() {
+        var map = L.map('map').setView([65, 12], 5);
+        L.tileLayer('http://opencache.statkart.no/gatekeeper/gk/gk.open_gmaps?layers=topo2&zoom={z}&x={x}&y={y}', {
+            attribution: 'Kartverket'
+        }).addTo(map);
 
-    map.addControl(new L.Control.Draw({
-        position: 'topleft',
-        polyline: false,
-        polygon: false,
-        rectangle: false,
-        circle: false,
-        marker: {
-            title: "Velg startposisjon"
+        // From js_globals
+        if(Turistforeningen.start_point_lat !== undefined && Turistforeningen.start_point_lng !== undefined) {
+            marker = new L.Marker(new L.LatLng(Turistforeningen.start_point_lat, Turistforeningen.start_point_lng), {
+                'title': 'Turen starter her'
+            }).bindPopup(popup_content).addTo(map);
         }
-    }));
 
-    map.on('draw:marker-created', function(e) {
-        if(typeof marker !== "undefined") {
-            marker.setLatLng(e.marker.getLatLng());
-            marker.update();
-        } else {
-            e.marker.bindPopup(popup_content).addTo(map);
-            marker = e.marker;
-        }
-        marker.openPopup();
-    });
+        map.addControl(new L.Control.Draw({
+            position: 'topleft',
+            polyline: false,
+            polygon: false,
+            rectangle: false,
+            circle: false,
+            marker: {
+                title: "Velg startposisjon"
+            }
+        }));
 
-    map_element.find("a.leaflet-control-draw-marker").tooltip({
-        placement: 'right'
+        map.on('draw:marker-created', function(e) {
+            if(typeof marker !== "undefined") {
+                marker.setLatLng(e.marker.getLatLng());
+                marker.update();
+            } else {
+                e.marker.bindPopup(popup_content).addTo(map);
+                marker = e.marker;
+            }
+            marker.openPopup();
+        });
+
+        map_element.find("a.leaflet-control-draw-marker").tooltip({
+            placement: 'right'
+        });
+    };
+
+    // Show the map
+    show_map.click(function() {
+        $(this).parent().remove();
+        show_on_map.show();
+        map_element.slideDown(initiate_map);
     });
 
     form.submit(function() {
