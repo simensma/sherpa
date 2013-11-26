ff4a0b2a012d7cce55ecbc195877021be5218cbc
Ali Kaafarani <ali@kvikshaug.no>
2013-11-26 15:16:29 +0100

    Add basic search to enrollment listing

--- apps/admin/enrollment/views.py
+++ apps/admin/enrollment/views.py
@@ -2,13 +2,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.paginator import Paginator, InvalidPage
+from django.db.models import Q
 
 import json
 
 from enrollment.models import State, Enrollment
 
 def index(request):
-    enrollments = Enrollment.get_active().prefetch_related('users', 'transactions', 'users__pending_user').order_by('-date_modified')
+    enrollments = Enrollment.get_active()
+
+    if 'search' in request.GET:
+        for q in request.GET['search'].split():
+            enrollments = enrollments.filter(
+                Q(address1__icontains=q) |
+                Q(address2__icontains=q) |
+                Q(address3__icontains=q) |
+                Q(zipcode__icontains=q) |
+                Q(area__icontains=q) |
+                Q(users__name__icontains=q) |
+                Q(users__phone__icontains=q) |
+                Q(users__email__icontains=q) |
+                Q(users__memberid__icontains=q) |
+                Q(transactions__transaction_id__icontains=q) |
+                Q(transactions__order_number__icontains=q)
+            )
+
+    enrollments = enrollments.prefetch_related('users', 'transactions', 'users__pending_user').order_by('-date_modified')
     paginator = Paginator(enrollments, 20)
     try:
         enrollments = paginator.page(request.GET.get('page', 1))
@@ -16,7 +35,8 @@ def index(request):
         enrollments = paginator.page(1)
 
     context = {
-        'enrollments': enrollments
+        'enrollments': enrollments,
+        'search': request.GET.get('search'),
     }
     return render(request, 'common/admin/enrollment/index.html', context)
 
