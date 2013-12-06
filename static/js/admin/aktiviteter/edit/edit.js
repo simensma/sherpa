8bd4697784e03a43220dd6986497f960f0e7b526
Ali Kaafarani <ali@kvikshaug.no>
2013-12-06 16:52:12 +0100

    Add a bunch more client-side validations

--- static/js/admin/aktiviteter/edit/edit.js
+++ static/js/admin/aktiviteter/edit/edit.js
@@ -319,6 +319,26 @@ $(document).ready(function() {
             scrollTo = scrollTo || TitleValidator.scrollTo;
         }
 
+        if(!DescriptionValidator.validate()) {
+            valid = false;
+            scrollTo = scrollTo || DescriptionValidator.scrollTo;
+        }
+
+        if(!DifficultyValidator.validate()) {
+            valid = false;
+            scrollTo = scrollTo || DifficultyValidator.scrollTo;
+        }
+
+        if(!AudienceValidator.validate()) {
+            valid = false;
+            scrollTo = scrollTo || AudienceValidator.scrollTo;
+        }
+
+        if(!CategoryValidator.validate()) {
+            valid = false;
+            scrollTo = scrollTo || CategoryValidator.scrollTo;
+        }
+
         return {
             valid: valid,
             scrollTo: scrollTo
@@ -357,5 +377,136 @@ $(document).ready(function() {
 
     }(window.TitleValidator = window.TitleValidator || {}, jQuery ));
 
+    // Require description
+    (function(DescriptionValidator, $, undefined ) {
+
+        var control_group = form.find("div.control-group.description");
+        var input = control_group.find("textarea[name='description']");
+        var error = control_group.find("div.error");
+
+        DescriptionValidator.scrollTo = control_group.parents("div.section").offset().top;
+
+        DescriptionValidator.validate = function() {
+            var valid = input.val().trim() !== '';
+            if(!valid) {
+                markError();
+            }
+            return valid;
+        };
+
+        input.focus(clearError);
+        input.focusout(DescriptionValidator.validate);
+
+        function markError() {
+            control_group.addClass('error');
+            error.show();
+        }
+
+        function clearError() {
+            control_group.removeClass('error');
+            error.hide();
+        }
+
+    }(window.DescriptionValidator = window.DescriptionValidator || {}, jQuery ));
+
+    // Require difficulty
+    (function(DifficultyValidator, $, undefined ) {
+
+        var control_group = form.find("div.control-group.difficulty");
+        var select = control_group.find("select[name='difficulty']");
+        var error = control_group.find("div.error");
+
+        DifficultyValidator.scrollTo = control_group.offset().top;
+
+        DifficultyValidator.validate = function() {
+            var valid = select.find("option:selected").val() !== '';
+            if(!valid) {
+                markError();
+            }
+            return valid;
+        };
+
+        select.change(clearError);
+
+        function markError() {
+            control_group.addClass('error');
+            error.show();
+        }
+
+        function clearError() {
+            control_group.removeClass('error');
+            error.hide();
+        }
+
+    }(window.DifficultyValidator = window.DifficultyValidator || {}, jQuery ));
+
+    // Require at least one audience
+    (function(AudienceValidator, $, undefined ) {
+
+        var control_group = form.find("div.control-group.audiences");
+        var select = control_group.find("select[name='audiences']");
+        var error = control_group.find("div.error");
+
+        AudienceValidator.scrollTo = control_group.offset().top;
+
+        AudienceValidator.validate = function() {
+            var valid = select.find("option:selected").length !== 0;
+            if(!valid) {
+                markError();
+            }
+            return valid;
+        };
+
+        select.change(function() {
+            if(AudienceValidator.validate()) {
+                clearError();
+            }
+        });
+
+        function markError() {
+            control_group.addClass('error');
+            error.show();
+        }
+
+        function clearError() {
+            control_group.removeClass('error');
+            error.hide();
+        }
+
+    }(window.AudienceValidator = window.AudienceValidator || {}, jQuery ));
+
+    // Require at least one predefined category
+    (function(CategoryValidator, $, undefined ) {
+
+        var control_group = form.find("div.control-group.category, div.control-group.subcategories");
+        var error = control_group.find("div.error");
+        var category_buttons = control_group.find("button[data-category]");
+        var subcategory_buttons = control_group.find("button.subcategory");
+
+        CategoryValidator.scrollTo = control_group.offset().top;
+
+        CategoryValidator.validate = function() {
+            var category = category_buttons.filter(".active").attr('data-category');
+            var valid = subcategory_buttons.is("." + category + ".btn-danger");
+            if(!valid) {
+                markError();
+            }
+            return valid;
+        };
+
+        category_buttons.click(clearError);
+        subcategory_buttons.click(clearError);
+
+        function markError() {
+            control_group.addClass('error');
+            error.show();
+        }
+
+        function clearError() {
+            control_group.removeClass('error');
+            error.hide();
+        }
+
+    }(window.CategoryValidator = window.CategoryValidator || {}, jQuery ));
 
 });
