abec188cac18b5aae36567f96830f9e108cb48c7
Ali Kaafarani <ali@kvikshaug.no>
2014-01-14 16:57:47 +0100

    Move polyfill scripts to common file

--- static/js/ie/contains.js
+++
@@ -1,5 +0,0 @@
-if(typeof String.prototype.contains !== 'function') {
-    String.prototype.contains = function(str, startIndex) {
-        return -1 !== String.prototype.indexOf.call(this, str, startIndex);
-    };
-}
