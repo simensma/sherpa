dd1cd9279f9fdc2b46be2453f799571844b615fd
Ali Kaafarani <ali@kvikshaug.no>
2014-01-20 18:04:43 +0100

    Put some actual numbers in the dashboard panels

--- apps/admin/views.py
+++ apps/admin/views.py
@@ -1,12 +1,51 @@
 from django.shortcuts import render
 from django.core.cache import cache
 
-from datetime import datetime
+from datetime import datetime, date
 from lxml import etree
 import requests
 import re
 
+# This ugly import hack imports the model from views because of namespace collision,
+# should be 'from aktiviteter.models import Aktivitet'
+from aktiviteter.views import Aktivitet
+from focus.models import Actor
+from page.models import Page
+from user.models import User
+
 def index(request):
+    total_membership_count = cache.get('admin.total_membership_count')
+    local_membership_count = cache.get('admin.local_membership_count')
+    if total_membership_count is None or local_membership_count is None:
+        all_members = Actor.all_members()
+        total_membership_count = all_members.count()
+        local_membership_count = all_members.filter(
+            main_association_id=request.session['active_association'].get_main_association().focus_id
+        ).count()
+        cache.set('admin.total_membership_count', total_membership_count, 60 * 60 * 12)
+        cache.set('admin.local_membership_count', local_membership_count, 60 * 60 * 12)
+
+    turledere = User.objects.filter(turledere__isnull=False).distinct().count()
+    pages = Page.objects.filter(
+        pub_date__lte=datetime.now(),
+        published=True
+    ).count()
+    print(pages)
+    aktiviteter = Aktivitet.objects.filter(
+        pub_date__lte=date.today(),
+        published=True,
+        private=False,
+    ).count()
+    dashboard_stats = {
+        'members': {
+            'total': "{:,}".format(total_membership_count),
+            'local': "{:,}".format(local_membership_count),
+        },
+        'turledere': turledere,
+        'pages': pages,
+        'aktiviteter': aktiviteter,
+    }
+
     betablog = cache.get('admin.betablog')
     if betablog is None:
         try:
@@ -33,7 +72,10 @@ def index(request):
 
         cache.set('admin.betablog', betablog, 60 * 60 * 12)
 
-    context = {'betablog': betablog}
+    context = {
+        'betablog': betablog,
+        'dashboard_stats': dashboard_stats,
+    }
     return render(request, 'common/admin/dashboard.html', context)
 
 def intro(request):
