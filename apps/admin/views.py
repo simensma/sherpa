0c79ceaba6ced7c2414ac1081459635616b82809
Ali Kaafarani <ali@kvikshaug.no>
2014-03-18 11:46:30 +0100

    Enable creating sites for active forening

--- apps/admin/views.py
+++ apps/admin/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.core.cache import cache
 from django.db.models import Q
+from django.contrib import messages
 
 from datetime import datetime, date
 from lxml import etree
@@ -13,6 +14,8 @@ from aktiviteter.views import Aktivitet
 from focus.models import Actor
 from page.models import Page
 from user.models import User
+from core.models import Site, SiteTemplate
+from foreninger.models import Forening
 
 def index(request):
     total_membership_count = cache.get('admin.total_membership_count')
@@ -82,3 +85,66 @@ def index(request):
         'dashboard_stats': dashboard_stats,
     }
     return render(request, 'common/admin/dashboard.html', context)
+
+def setup_site(request):
+    if request.session['active_forening'].site is not None:
+        return redirect('admin.cms.views.page.list')
+
+    if request.method == 'GET':
+        return render(request, 'common/admin/setup_site.html')
+    elif request.method == 'POST':
+        # Very simple syntax verification
+        domain = request.POST['domain'].strip()
+        if domain == '' or domain == 'http://' or not domain.startswith('http://') or not '.' in domain:
+            messages.error(request, 'malformed')
+            return render(request, 'common/admin/setup_site.html', {'domain': domain})
+
+        domain = domain[len('http://'):]
+        if domain.endswith('/'):
+            domain = domain[:-1]
+
+        if '/' not in domain:
+            prefix = ''
+        else:
+            try:
+                # Prefix folder specified
+                domain, prefix = domain.split('/')
+                # Only allowed for turistforeningen.no
+                if domain != 'turistforeningen.no' and domain != 'www.turistforeningen.no':
+                    messages.error(request, 'prefix_for_disallowed_domain')
+                    return render(request, 'common/admin/setup_site.html', {
+                        'domain': "http://%s/%s" % (domain, prefix)
+                    })
+            except ValueError:
+                # More than one subdir specified
+                messages.error(request, 'more_than_one_subdir')
+                return render(request, 'common/admin/setup_site.html', {
+                    'domain': "http://%s/%s" % (domain, prefix)
+                })
+
+        if domain.count('.') == 1 and not domain.startswith('www'):
+            domain = "www.%s" % domain
+
+        try:
+            existing_site = Site.objects.get(domain=domain, prefix=prefix)
+            existing_forening = Forening.objects.get(site=existing_site)
+            messages.error(request, 'site_exists')
+            return render(request, 'common/admin/setup_site.html', {
+                'domain': "http://%s/%s" % (domain, prefix),
+                'existing_forening': existing_forening
+            })
+        except Site.DoesNotExist:
+            pass
+
+        # TODO let creator choose template?
+        large_template = SiteTemplate.objects.get(name='large')
+        site = Site(
+            domain=domain,
+            prefix=prefix,
+            template=large_template
+        )
+        site.save()
+        request.session['active_forening'].site = site
+        request.session['active_forening'].save()
+        request.session.modified = True
+        return redirect('admin.cms.views.page.list')
