e859b613563bbc5ddd9a7af8fe7647abc624a951
Ali Kaafarani <ali@kvikshaug.no>
2014-03-19 16:22:48 +0100

    Add request to all template contexts

--- sherpa/settings.py
+++ sherpa/settings.py
@@ -205,6 +205,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "django.core.context_processors.i18n",
     "django.core.context_processors.media",
     "django.core.context_processors.static",
+    "django.core.context_processors.request",
     "django.contrib.messages.context_processors.messages",
     "sherpa.context_processors.menus",
     "sherpa.context_processors.current_site",
