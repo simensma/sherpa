a740768fb32ff543074b70c648e0b09924a6186d
Ali Kaafarani <ali@kvikshaug.no>
2014-03-19 11:39:24 +0100

    Include common URLs in small urlconf

--- sherpa/urls_small.py
+++ sherpa/urls_small.py
@@ -6,5 +6,7 @@ handler404 = 'page.views.page_not_found'
 handler500 = 'page.views.server_error'
 
 urlpatterns = patterns('',
+    url(r'^', include('sherpa.urls_common_pre')),
 
+    url(r'^', include('sherpa.urls_common_post')),
 )
