33f44f0f988d4962f1a5c726f516ae2cb4374361
Ali Kaafarani <ali@kvikshaug.no>
2014-03-25 17:06:43 +0100

    Update UserVoice configuration

--- static/js/uservoice.js
+++ static/js/uservoice.js
@@ -1,30 +1,28 @@
-// Include the UserVoice JavaScript SDK (only needed once on a page)
-UserVoice=window.UserVoice||[];(function(){var uv=document.createElement('script');uv.type='text/javascript';uv.async=true;uv.src='//widget.uservoice.com/sjQEGc2xV1ahxZg1IlQbQQ.js';var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(uv,s)})();
-
 //
 // UserVoice Javascript SDK developer documentation:
 // https://www.uservoice.com/o/javascript-sdk
 //
 
-// Set colors
-UserVoice.push(['set', {
-  accent_color: '#e23a39',
-  trigger_color: 'white',
-  trigger_background_color: '#e23a39'
+(function() {
+    var uv=document.createElement('script');
+    uv.type='text/javascript';
+    uv.async=true;
+    uv.src='//widget.uservoice.com/sjQEGc2xV1ahxZg1IlQbQQ.js';
+    var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(uv,s);
+})();
+
+UserVoice = window.UserVoice || [];
+UserVoice.push(['showTab', 'classic_widget', {
+    mode: 'full',
+    primary_color: '#0072b4',
+    link_color: '#d82c20',
+    default_mode: 'support',
+    forum_id: 235824,
+    tab_label: 'Spørsmål eller tilbakemeldinger?',
+    tab_color: '#d83020',
+    tab_position: 'middle-right',
+    tab_inverted: false
 }]);
 
 // Identify the user and pass traits
-// To enable, replace sample data with actual user traits and uncomment the line
 UserVoice.push(['identify', window.User]);
-
-// Add default trigger to the bottom-right corner of the window:
-UserVoice.push(['addTrigger', { mode: 'contact', trigger_position: 'bottom-right' }]);
-
-// Or, use your own custom trigger:
-//UserVoice.push(['addTrigger', '#id', { mode: 'contact' }]);
-
-// Autoprompt for Satisfaction and SmartVote (only displayed under certain conditions)
-UserVoice.push(['autoprompt', {}]);
-
-// Language
-UserVoice.push(["set", "locale", "nb"]);
