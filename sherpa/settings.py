bdf5a4db818a95482e53b2c57e731f1b2ecfe88d
Ali Kaafarani <ali@kvikshaug.no>
2014-03-31 16:42:22 +0200

    Generic unspecific bugfix

--- sherpa/settings.py
+++ sherpa/settings.py
@@ -215,6 +215,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "sherpa.context_processors.dntconnect",
     "sherpa.context_processors.membership_year_start",
     "sherpa.context_processors.do_not_track",
+    "sherpa.context_processors.april_fools",
 )
 
 MIDDLEWARE_CLASSES = (
