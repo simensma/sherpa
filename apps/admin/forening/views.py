fc603bf660a6d5b341d719705f93fa0ced9a4bac
Ali Kaafarani <ali@kvikshaug.no>
2014-04-02 19:16:14 +0200

    Add missing import

--- apps/admin/forening/views.py
+++ apps/admin/forening/views.py
@@ -8,6 +8,7 @@ from django.http import HttpResponse
 from django.db.models import Q
 from django.template import RequestContext
 from django.template.loader import render_to_string
+from django.core.exceptions import PermissionDenied
 
 import json
 
