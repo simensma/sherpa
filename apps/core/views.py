877a26c615e9bdd6a3ed9d3ed63b354196b83949
Ali Kaafarani <ali@kvikshaug.no>
2014-05-05 10:03:44 +0200

    Don't handle/log exceptions in booking_spots

--- apps/core/views.py
+++ apps/core/views.py
@@ -42,22 +42,26 @@ def doge(request):
 
 def booking_spots(request, code, date):
     """This view is used by gamle Sherpa to display available spots in a small iframe next to the signup buttons."""
-    date = datetime.strptime(date, "%Y-%m-%d")
-    r = requests.get(
-        "%s/%s/" % (settings.DNTOSLO_MONTIS_API_URL, code),
-        params={
-            'client': 'dnt',
-            'autentisering': settings.DNTOSLO_MONTIS_API_KEY,
-        },
-    )
-    for tour_date in json.loads(r.text):
-        if date == datetime.fromtimestamp(tour_date['startdato']):
-            context = {
-                'available': tour_date['plasserLedig'],
-                'total': tour_date['plasserTotalt'],
-                'waiting_list': tour_date['venteliste'],
-            }
-            return render(request, 'main/booking_spots.html', context)
+    try:
+        date = datetime.strptime(date, "%Y-%m-%d")
+        r = requests.get(
+            "%s/%s/" % (settings.DNTOSLO_MONTIS_API_URL, code),
+            params={
+                'client': 'dnt',
+                'autentisering': settings.DNTOSLO_MONTIS_API_KEY,
+            },
+        )
+        for tour_date in json.loads(r.text):
+            if date == datetime.fromtimestamp(tour_date['startdato']):
+                context = {
+                    'available': tour_date['plasserLedig'],
+                    'total': tour_date['plasserTotalt'],
+                    'waiting_list': tour_date['venteliste'],
+                }
+                return render(request, 'main/booking_spots.html', context)
 
-    # Invalid date? Ignore for now
-    return HttpResponse('')
+        # Invalid date?
+        raise Exception()
+    except Exception():
+        # Don't handle; ignore any errors for now
+        return HttpResponse('')
