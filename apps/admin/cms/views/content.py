352e8df5d7061450fa71eebb5c2c2b905b6b0a32
Ali Kaafarani <ali@kvikshaug.no>
2014-05-06 14:06:52 +0200

    Wrap page/article save in a transaction

--- apps/admin/cms/views/content.py
+++ apps/admin/cms/views/content.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import
 from django.http import HttpResponse
 from django.template import RequestContext
 from django.template.loader import render_to_string
+from django.db import transaction
 
 from page.models import Page, Version, Row, Column, Content
 from page.widgets import parse_widget
@@ -24,131 +25,134 @@ def save(request, version):
         'unexpected_content_ids': [],
     }
 
-    # Delete all existing rows - a bit scary to do this before creating the new ones in case there's some error
-    version.rows.all().delete()
+    # Wrap the entire save operation in an atomic transaction
+    with transaction.commit_on_success():
 
-    for row in json.loads(request.POST['rows']):
-        row_obj = Row(version=version, order=row['order'])
-        row_obj.save()
-        for column in row['columns']:
-            column_obj = Column(
-                row=row_obj,
-                span=column['span'],
-                offset=column['offset'],
-                order=column['order'],
-            )
-            column_obj.save()
-            for content in column['contents']:
-                content_obj = Content(
-                    column=column_obj,
-                    content=content['content'],
-                    type=content['type'],
-                    order=content['order']
+        # Delete all existing rows
+        version.rows.all().delete()
+
+        for row in json.loads(request.POST['rows']):
+            row_obj = Row(version=version, order=row['order'])
+            row_obj.save()
+            for column in row['columns']:
+                column_obj = Column(
+                    row=row_obj,
+                    span=column['span'],
+                    offset=column['offset'],
+                    order=column['order'],
                 )
-                content_obj.save()
+                column_obj.save()
+                for content in column['contents']:
+                    content_obj = Content(
+                        column=column_obj,
+                        content=content['content'],
+                        type=content['type'],
+                        order=content['order']
+                    )
+                    content_obj.save()
 
-    # Tags - common for pages and articles
-    version.tags.clear()
-    for tag in [t.lower() for t in json.loads(request.POST['tags'])]:
-        obj, created = Tag.objects.get_or_create(name=tag)
-        version.tags.add(obj)
+        # Tags - common for pages and articles
+        version.tags.clear()
+        for tag in [t.lower() for t in json.loads(request.POST['tags'])]:
+            obj, created = Tag.objects.get_or_create(name=tag)
+            version.tags.add(obj)
 
-    # Article/Page data
-    if version.variant.page is not None:
-        page = version.variant.page
+        # Article/Page data
+        if version.variant.page is not None:
+            page = version.variant.page
 
-        ### Title ###
-        page.title = request.POST['title']
+            ### Title ###
+            page.title = request.POST['title']
 
-        ### Parent page ###
-        if request.POST['parent'] == '':
-            new_parent = None
-        else:
-            new_parent = Page.on(request.active_forening.site).get(id=request.POST['parent'])
-            parent = new_parent
-            while parent is not None:
-                if parent.id == page.id:
-                    response['parent_error'] = 'parent_in_parent'
-                    break
-                parent = parent.parent
-        if 'parent_error' not in response:
-            page.parent = new_parent
-
-        ### Ads ###
-        version.ads = json.loads(request.POST['ads'])
-
-        ### Published state ###
-
-        try:
-            page.published = json.loads(request.POST['status'])
-            datetime_string = '%s %s' % (request.POST['publish_date'], request.POST['publish_time'])
-            page.pub_date = datetime.strptime(datetime_string, '%d.%m.%Y %H:%M')
-        except ValueError:
-            if page.published:
-                # We're trying to publish, and an error occured.
-                if request.POST['publish_date'] == '' and request.POST['publish_time'] == '':
-                    # Well, since we didn't specify the date, set it to now - and update it clientside
-                    now = datetime.now()
-                    response['publish_error'] = 'auto_now'
-                    response['publish_date'] = now.strftime('%d.%m.%Y')
-                    response['publish_time'] = now.strftime('%H:%M')
-                    page.pub_date = now
-                else:
-                    # Parse error - inform, and don't publish
-                    response['publish_error'] = 'unparseable_datetime'
-                    page.published = False
+            ### Parent page ###
+            if request.POST['parent'] == '':
+                new_parent = None
             else:
-                # An error occured, but we're not publishing so just nullify
-                response['publish_error'] = 'error_nullify'
-                page.pub_date = None
+                new_parent = Page.on(request.active_forening.site).get(id=request.POST['parent'])
+                parent = new_parent
+                while parent is not None:
+                    if parent.id == page.id:
+                        response['parent_error'] = 'parent_in_parent'
+                        break
+                    parent = parent.parent
+            if 'parent_error' not in response:
+                page.parent = new_parent
 
-        # Record the modification
-        page.modified_by = request.user
-        page.modified_date = datetime.now()
+            ### Ads ###
+            version.ads = json.loads(request.POST['ads'])
 
-        version.save()
-        page.save()
+            ### Published state ###
 
-    elif version.variant.article is not None:
-        article = version.variant.article
-
-        ### Authors ###
-        publisher_list = json.loads(request.POST['authors'])
-        if len(publisher_list) > 0:
-            publishers = User.get_users().filter(id__in=publisher_list)
-            version.publishers = publishers
-        else:
-            response['author_error'] = 'no_authors'
-
-        ### Published state ###
-        try:
-            article.published = json.loads(request.POST['status'])
-            datetime_string = '%s %s' % (request.POST['publish_date'], request.POST['publish_time'])
-            article.pub_date = datetime.strptime(datetime_string, '%d.%m.%Y %H:%M')
-        except ValueError:
-            if article.published:
-                # We're trying to publish, and an error occured.
-                if request.POST['publish_date'] == '' and request.POST['publish_time'] == '':
-                    # Well, since we didn't specify the date, set it to now - and update it clientside
-                    now = datetime.now()
-                    response['publish_error'] = 'auto_now'
-                    response['publish_date'] = now.strftime('%d.%m.%Y')
-                    response['publish_time'] = now.strftime('%H:%M')
-                    article.pub_date = now
+            try:
+                page.published = json.loads(request.POST['status'])
+                datetime_string = '%s %s' % (request.POST['publish_date'], request.POST['publish_time'])
+                page.pub_date = datetime.strptime(datetime_string, '%d.%m.%Y %H:%M')
+            except ValueError:
+                if page.published:
+                    # We're trying to publish, and an error occured.
+                    if request.POST['publish_date'] == '' and request.POST['publish_time'] == '':
+                        # Well, since we didn't specify the date, set it to now - and update it clientside
+                        now = datetime.now()
+                        response['publish_error'] = 'auto_now'
+                        response['publish_date'] = now.strftime('%d.%m.%Y')
+                        response['publish_time'] = now.strftime('%H:%M')
+                        page.pub_date = now
+                    else:
+                        # Parse error - inform, and don't publish
+                        response['publish_error'] = 'unparseable_datetime'
+                        page.published = False
                 else:
-                    # Parse error - inform, and don't publish
-                    response['publish_error'] = 'unparseable_datetime'
-                    article.published = False
+                    # An error occured, but we're not publishing so just nullify
+                    response['publish_error'] = 'error_nullify'
+                    page.pub_date = None
+
+            # Record the modification
+            page.modified_by = request.user
+            page.modified_date = datetime.now()
+
+            version.save()
+            page.save()
+
+        elif version.variant.article is not None:
+            article = version.variant.article
+
+            ### Authors ###
+            publisher_list = json.loads(request.POST['authors'])
+            if len(publisher_list) > 0:
+                publishers = User.get_users().filter(id__in=publisher_list)
+                version.publishers = publishers
             else:
-                # An error occured, but we're not publishing so just nullify
-                response['publish_error'] = 'error_nullify'
-                article.pub_date = None
+                response['author_error'] = 'no_authors'
 
-        # Record the modification
-        article.modified_by = request.user
-        article.modified_date = datetime.now()
+            ### Published state ###
+            try:
+                article.published = json.loads(request.POST['status'])
+                datetime_string = '%s %s' % (request.POST['publish_date'], request.POST['publish_time'])
+                article.pub_date = datetime.strptime(datetime_string, '%d.%m.%Y %H:%M')
+            except ValueError:
+                if article.published:
+                    # We're trying to publish, and an error occured.
+                    if request.POST['publish_date'] == '' and request.POST['publish_time'] == '':
+                        # Well, since we didn't specify the date, set it to now - and update it clientside
+                        now = datetime.now()
+                        response['publish_error'] = 'auto_now'
+                        response['publish_date'] = now.strftime('%d.%m.%Y')
+                        response['publish_time'] = now.strftime('%H:%M')
+                        article.pub_date = now
+                    else:
+                        # Parse error - inform, and don't publish
+                        response['publish_error'] = 'unparseable_datetime'
+                        article.published = False
+                else:
+                    # An error occured, but we're not publishing so just nullify
+                    response['publish_error'] = 'error_nullify'
+                    article.pub_date = None
 
-        version.save()
-        article.save()
+            # Record the modification
+            article.modified_by = request.user
+            article.modified_date = datetime.now()
+
+            version.save()
+            article.save()
 
     return HttpResponse(json.dumps(response))
