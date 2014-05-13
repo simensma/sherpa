bb73adc93b95edffc4d5a2fdca4cfdb7a1159d92
Ali Kaafarani <ali@kvikshaug.no>
2014-05-13 10:07:28 +0200

    Remove unused import

--- apps/admin/cache/views.py
+++ apps/admin/cache/views.py
@@ -1,6 +1,5 @@
 # encoding: utf-8
 from django.shortcuts import render, redirect
-from django.core.urlresolvers import reverse
 from django.http import HttpResponse
 from django.core.cache import cache
 
