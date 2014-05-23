44e5fc386a17df7464ef471353a1f5d7cea2fcad
Ali Kaafarani <ali@kvikshaug.no>
2014-05-23 14:47:32 +0200

    Add js wrapper

--- static/js/admin/campaigns/new.js
+++ static/js/admin/campaigns/new.js
@@ -1,6 +1,8 @@
 $(function() {
 
-    $("button.upload-image").click(function() {
+    var wrapper = $("div.new-campaign");
+
+    wrapper.find("button.upload-image").click(function() {
         ImageUploadDialog.open(function(url, description, photographer) {
             // TBD
         });
