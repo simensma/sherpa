b1e81f0312649808ca7957371b4bbedaf7d33b96
Ali Kaafarani <ali@kvikshaug.no>
2014-05-28 09:31:22 +0200

    Hide subsequent steps until previous step is filled

---
+++ static/js/admin/setup_site.js
@@ -0,0 +1,13 @@
+$(function() {
+
+    var wrapper = $("div.setup-site");
+    var step1 = wrapper.find("div.step1");
+    var step2 = wrapper.find("div.step2");
+    var domain = step2.find("input[name='domain']");
+    var submit = step2.find("button[type='submit']");
+
+    step1.find("label.btn").click(function() {
+        step2.slideDown();
+    });
+
+});
