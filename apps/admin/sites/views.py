ca48d1757827b2f9976ab520b0667634fbbabd58
Ali Kaafarani <ali@kvikshaug.no>
2014-05-28 18:43:45 +0200

    List children foreningers sites

--- apps/admin/sites/views.py
+++ apps/admin/sites/views.py
@@ -4,5 +4,11 @@ from core.models import Site
 
 def index(request, site):
     active_site = Site.objects.get(id=site)
-    context = {'active_site': active_site}
+
+    children_foreninger_with_site = [f for f in request.active_forening.get_children_deep() if f.sites.count() > 0]
+
+    context = {
+        'active_site': active_site,
+        'children_foreninger_with_site': children_foreninger_with_site,
+    }
     return render(request, 'common/admin/sites/index.html', context)
