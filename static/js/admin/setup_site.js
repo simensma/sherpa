18a4ba3b7eb89c4bdbea6ce6ce3800a8718044b8
Ali Kaafarani <ali@kvikshaug.no>
2014-05-28 10:13:27 +0200

    Make the wizard steps more prominent

--- static/js/admin/setup_site.js
+++ static/js/admin/setup_site.js
@@ -1,14 +1,8 @@
 $(function() {
 
     var wrapper = $("div.setup-site");
-    var step1 = wrapper.find("div.step1");
-    var step2 = wrapper.find("div.step2");
-    var domain = step2.find("input[name='domain']");
-    var submit = step2.find("button[type='submit']");
-
-    step1.find("label.btn").click(function() {
-        step2.slideDown();
-    });
+    var domain = wrapper.find("input[name='domain']");
+    var submit = wrapper.find("button[type='submit']");
 
     submit.click(function(e) {
         if(domain.val().trim() === 'forening' || domain.val().trim() === '') {
