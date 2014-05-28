ec1545995c682cfbc052c0088ce8526117fe7f3c
Ali Kaafarani <ali@kvikshaug.no>
2014-05-28 18:48:38 +0200

    List only children-foreninger for admins

--- apps/admin/sites/views.py
+++ apps/admin/sites/views.py
@@ -5,7 +5,18 @@ from core.models import Site
 def index(request, site):
     active_site = Site.objects.get(id=site)
 
-    children_foreninger_with_site = [f for f in request.active_forening.get_children_deep() if f.sites.count() > 0]
+    # Generate a list of children-foreninger with site to display
+    children_foreninger_with_site = []
+    for forening in request.active_forening.get_children_deep():
+        # Verify that the forening has at least one site
+        if forening.sites.count() == 0:
+            continue
+
+        # Check that the user has admin-access to the forening
+        # This would be mighty slow if user.all_foreninger wasn't cached
+        for user_forening in request.user.all_foreninger():
+            if forening == user_forening and user_forening.role == 'admin':
+                children_foreninger_with_site.append(forening)
 
     context = {
         'active_site': active_site,
