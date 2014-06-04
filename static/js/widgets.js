4b49effa60559b11c0eee7d63a90d4cd2b8c1195
Ali Kaafarani <ali@kvikshaug.no>
2014-06-04 14:47:45 +0200

    Center images vertically in fixed-height container

--- static/js/widgets.js
+++ static/js/widgets.js
@@ -16,4 +16,13 @@ $(function() {
         });
     });
 
+    // Articles (horizontal layout)
+    // Adjust the contained image so it's centered within its fixed-height container
+    $("div.widget.articles div.horizontal div.image-container").each(function() {
+        var total_height = $(this).height();
+        var image = $(this).find("img");
+        var top_offset = ((image.height() - total_height) / 2) * -1;
+        image.css('top', top_offset + 'px');
+    });
+
 });
