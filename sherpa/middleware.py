e9d6c1c68e6d41ae0661bd06ef07696351ebfc87
Ali Kaafarani <ali@kvikshaug.no>
2014-06-13 09:49:00 +0200

    Remove obsolete comment

--- sherpa/middleware.py
+++ sherpa/middleware.py
@@ -87,7 +87,6 @@ class ChangeActiveForening(object):
         if request.user.is_authenticated() and request.user.has_perm('sherpa'):
             m = re.match(r'/sherpa/aktiv-forening/(?P<forening>\d+)/', request.path)
             if m is not None:
-                # Note: this object will be copied in session for a while and will NOT get updated even if the original object is.
                 forening = Forening.objects.get(id=m.groupdict()['forening'])
                 if not forening in request.user.all_foreninger():
                     raise PermissionDenied
