ba28c0194a004db7ebd389f430037f8c96e3922a
Ali Kaafarani <ali@kvikshaug.no>
2014-07-08 18:24:26 +0200

    Display cropped image when crop selection is chosen

--- static/js/admin/sites/campaigns/new.js
+++ static/js/admin/sites/campaigns/new.js
@@ -4,6 +4,8 @@ $(function() {
     var section_progress = wrapper.find('.section-progress');
     var chosen_image = wrapper.find('img.chosen-image');
     var chosen_image_ajaxloader = wrapper.find('img.chosen-image-ajaxloader');
+    var cropped_image_container = wrapper.find('div.cropped-image-container');
+    var cropped_image = cropped_image_container.find('img.cropped-image');
     var step2 = wrapper.find('div.step2');
     var step3 = wrapper.find('div.step3');
 
@@ -58,6 +60,13 @@ $(function() {
             chosen_image.Jcrop({
                 aspectRatio: crop_ratio[0] / crop_ratio[1],
                 setSelect: [0, 0, x2, y2],
+                onSelect: function(selection) {
+                    chosen_image.data('crop', {
+                        selection: selection,
+                        width: chosen_image.width(),
+                        height: chosen_image.height(),
+                    });
+                },
             }, function() {
                 JcropApi = this;
             });
@@ -68,11 +77,47 @@ $(function() {
         chosen_image.attr('src', image_url);
     }
 
+    function cropImage() {
+        cropped_image.attr('src', chosen_image.attr('src'));
+
+        var crop = chosen_image.data('crop');
+
+        // Remove any previous cropping
+        cropped_image.removeAttr('style');
+        cropped_image_container.removeAttr('style');
+
+        // Math magics
+        var column_width = crop_ratio[0];
+        var selection_width = crop.selection.x2 - crop.selection.x;
+        var selection_height = crop.selection.y2 - crop.selection.y;
+        var scaled_width = crop.width / selection_width;
+        var scaled_height = scaled_width; // Autoscale height to the new custom ratio
+
+        // If the image is smaller than the column, Jcrop will not scale it to 100%, so factor in the difference
+        var image_to_column_ratio = column_width / crop.width;
+        scaled_width *= image_to_column_ratio;
+        scaled_height *= image_to_column_ratio;
+
+        var offset_left = crop.selection.x * scaled_width;
+        var offset_top = crop.selection.y * scaled_height;
+
+        // Now set the calculated values on the new content
+        cropped_image.css('width', crop.width * scaled_width + 'px');
+        cropped_image.css('height', crop.height * scaled_height + 'px');
+        cropped_image.css('margin-left', '-' + offset_left + 'px');
+        cropped_image.css('margin-top', '-' + offset_top + 'px');
+        cropped_image_container.css('height', selection_height * scaled_height + 'px');
+    }
+
     function enableStep(step) {
         section_progress.find('li').removeClass('active');
         section_progress.find('li').eq(step - 1).addClass('active');
         wrapper.find('div.step').hide();
         wrapper.find('div.step[data-step="' + step + '"]').show();
+
+        if(step === 3) {
+            cropImage();
+        }
     }
 
 });
