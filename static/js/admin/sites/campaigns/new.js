d745045c00c19051ef43ed742ef8f1df15bf407e
Ali Kaafarani <ali@kvikshaug.no>
2014-07-08 16:32:08 +0200

    Add hidden second step

--- static/js/admin/sites/campaigns/new.js
+++ static/js/admin/sites/campaigns/new.js
@@ -1,17 +1,25 @@
 $(function() {
 
     var wrapper = $("div.new-campaign");
+    var section_progress = wrapper.find(".section-progress");
+    var step2 = wrapper.find("div.step2");
 
     wrapper.find("button.pick-from-image-archive").click(function() {
         ImageArchivePicker.pick(function(url, description, photographer) {
-            // TBD
+            enableStep2();
         });
     });
 
     wrapper.find("button.upload-new-image").click(function() {
         ImageUploadDialog.open(function(url, description, photographer) {
-            // TBD
+            enableStep2();
         });
     });
+
+    function enableStep2() {
+        section_progress.find("li").first().removeClass('active');
+        section_progress.find("li").last().addClass('active');
+        step2.show();
+    }
 
 });
