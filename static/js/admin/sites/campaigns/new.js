7548f2f9c3088d8b4ac937d93aa9f5bfa684b3e0
Ali Kaafarani <ali@kvikshaug.no>
2014-07-08 16:39:45 +0200

    Add third step

--- static/js/admin/sites/campaigns/new.js
+++ static/js/admin/sites/campaigns/new.js
@@ -3,23 +3,37 @@ $(function() {
     var wrapper = $("div.new-campaign");
     var section_progress = wrapper.find(".section-progress");
     var step2 = wrapper.find("div.step2");
+    var step3 = wrapper.find("div.step3");
+
+    section_progress.find("a").click(function() {
+        enableStep(Number($(this).attr('data-step')));
+    });
 
     wrapper.find("button.pick-from-image-archive").click(function() {
         ImageArchivePicker.pick(function(url, description, photographer) {
-            enableStep2();
+            enableStep(2);
         });
     });
 
     wrapper.find("button.upload-new-image").click(function() {
         ImageUploadDialog.open(function(url, description, photographer) {
-            enableStep2();
+            enableStep(2);
         });
     });
 
-    function enableStep2() {
-        section_progress.find("li").first().removeClass('active');
-        section_progress.find("li").last().addClass('active');
-        step2.show();
+    function enableStep(step) {
+        section_progress.find("li").removeClass('active');
+        section_progress.find("li").eq(step - 1).addClass('active');
+        if(step === 1) {
+            wrapper.find("div.step2").hide();
+            wrapper.find("div.step3").hide();
+        } else if(step === 2) {
+            wrapper.find("div.step2").show();
+            wrapper.find("div.step3").hide();
+        } else if(step === 3) {
+            wrapper.find("div.step2").show();
+            wrapper.find("div.step3").show();
+        }
     }
 
 });
