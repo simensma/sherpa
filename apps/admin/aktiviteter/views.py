5fc53208e7fd125b80f1f4666823ff25ee93a042
Hans Kristian Flaatten <hans.kristian.flaatten@turistforeningen.no>
2014-07-08 15:24:43 +0200

    Implement SHERPA-315 - forening filter

--- apps/admin/aktiviteter/views.py
+++ apps/admin/aktiviteter/views.py
@@ -21,42 +21,32 @@ import json
 import re
 
 def index(request):
-    #aktiviteter = Aktivitet.objects.all()
-
-    #if not (request.GET.get('forening') == 'alle' and request.user.has_perm('sherpa_admin')):
-    #    aktiviteter = aktiviteter.filter(
-    #        Q(forening=request.active_forening) |
-    #        Q(co_forening=request.active_forening)
-    #    )
-    #    exclude_filter = False
-    #else:
-    #    exclude_filter = True
-
-    #aktiviteter = aktiviteter.order_by('-pub_date')
-
-    exclude_filter = True
+    try:
+        forening = Forening.objects.get(id=request.GET.get('forening'))
+    except (ValueError, Forening.DoesNotExist):
+        forening = request.active_forening
 
     datoer = AktivitetDate.objects.all()
     datoer = datoer.filter(start_date__gte=date.today())
     datoer = datoer.filter(
-        Q(aktivitet__forening=request.active_forening) |
-        Q(aktivitet__co_forening=request.active_forening) |
+        Q(aktivitet__forening=forening) |
+        Q(aktivitet__co_forening=forening) |
         Q(
-            aktivitet__forening__parents=request.active_forening,
+            aktivitet__forening__parents=forening,
             aktivitet__forening__type='turgruppe',
         ) |
         Q(
-            aktivitet__co_forening__parents=request.active_forening,
+            aktivitet__co_forening__parents=forening,
             aktivitet__co_forening__type='turgruppe',
         )
     )
     datoer = datoer.order_by('start_date')
 
     context = {
-        'forening': request.active_forening,
-        'children': request.active_forening.get_children_sorted(),
+        'active_forening': request.active_forening,
+        'active_forening_children': request.active_forening.get_children_sorted(),
+        'selected_forening': forening,
         'datoer': datoer,
-        'exclude_filter': exclude_filter,
     }
     return render(request, 'common/admin/aktiviteter/index.html', context)
 
