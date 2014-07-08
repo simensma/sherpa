f0aadb5385973824ac2836dd3c183b6cbbc0f70f
Ali Kaafarani <ali@kvikshaug.no>
2014-07-08 14:52:34 +0200

    Add pick from image archive option

--- static/js/admin/sites/campaigns/new.js
+++ static/js/admin/sites/campaigns/new.js
@@ -2,7 +2,13 @@ $(function() {
 
     var wrapper = $("div.new-campaign");
 
-    wrapper.find("button.upload-image").click(function() {
+    wrapper.find("button.pick-from-image-archive").click(function() {
+        ImageArchivePicker.pick(function(url, description, photographer) {
+            // TBD
+        });
+    });
+
+    wrapper.find("button.upload-new-image").click(function() {
         ImageUploadDialog.open(function(url, description, photographer) {
             // TBD
         });
