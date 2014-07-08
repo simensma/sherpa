1d2dcd2551bedf5e45f1b1fc70a1c19d44f114e5
Ali Kaafarani <ali@kvikshaug.no>
2014-07-08 16:40:18 +0200

    Use single quotes

--- static/js/admin/sites/campaigns/new.js
+++ static/js/admin/sites/campaigns/new.js
@@ -1,38 +1,38 @@
 $(function() {
 
-    var wrapper = $("div.new-campaign");
-    var section_progress = wrapper.find(".section-progress");
-    var step2 = wrapper.find("div.step2");
-    var step3 = wrapper.find("div.step3");
+    var wrapper = $('div.new-campaign');
+    var section_progress = wrapper.find('.section-progress');
+    var step2 = wrapper.find('div.step2');
+    var step3 = wrapper.find('div.step3');
 
-    section_progress.find("a").click(function() {
+    section_progress.find('a').click(function() {
         enableStep(Number($(this).attr('data-step')));
     });
 
-    wrapper.find("button.pick-from-image-archive").click(function() {
+    wrapper.find('button.pick-from-image-archive').click(function() {
         ImageArchivePicker.pick(function(url, description, photographer) {
             enableStep(2);
         });
     });
 
-    wrapper.find("button.upload-new-image").click(function() {
+    wrapper.find('button.upload-new-image').click(function() {
         ImageUploadDialog.open(function(url, description, photographer) {
             enableStep(2);
         });
     });
 
     function enableStep(step) {
-        section_progress.find("li").removeClass('active');
-        section_progress.find("li").eq(step - 1).addClass('active');
+        section_progress.find('li').removeClass('active');
+        section_progress.find('li').eq(step - 1).addClass('active');
         if(step === 1) {
-            wrapper.find("div.step2").hide();
-            wrapper.find("div.step3").hide();
+            wrapper.find('div.step2').hide();
+            wrapper.find('div.step3').hide();
         } else if(step === 2) {
-            wrapper.find("div.step2").show();
-            wrapper.find("div.step3").hide();
+            wrapper.find('div.step2').show();
+            wrapper.find('div.step3').hide();
         } else if(step === 3) {
-            wrapper.find("div.step2").show();
-            wrapper.find("div.step3").show();
+            wrapper.find('div.step2').show();
+            wrapper.find('div.step3').show();
         }
     }
 
