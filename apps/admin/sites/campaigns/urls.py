ba320908a73340b2500150514a8103c22236ad2c
Ali Kaafarani <ali@kvikshaug.no>
2014-07-14 16:30:43 +0200

    Update existing campaign if editing, not creating

--- apps/admin/sites/campaigns/urls.py
+++ apps/admin/sites/campaigns/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('admin.sites.campaigns.views',
     url(r'^$', 'index'),
-    url(r'^rediger/$', 'edit'),
+    url(r'^rediger/$', 'edit', {'campaign': None}),
+    url(r'^rediger/(?P<campaign>\d+)/$', 'edit'),
     url(r'^lagre/$', 'save'),
 )
--- apps/admin/sites/campaigns/views.py
+++ apps/admin/sites/campaigns/views.py
@@ -15,29 +15,38 @@ def index(request, site):
     }
     return render(request, 'common/admin/sites/campaigns/index.html', context)
 
-def edit(request, site):
+def edit(request, site, campaign):
     active_site = Site.objects.get(id=site)
     context = {
         'active_site': active_site,
         'font_sizes': range(20, 77),
     }
+    if campaign is not None:
+        context['campaign'] = Campaign.objects.get(id=campaign)
     return render(request, 'common/admin/sites/campaigns/edit.html', context)
 
 def save(request, site):
     active_site = Site.objects.get(id=site)
+
+    try:
+        campaign = Campaign.objects.get(id=request.POST['existing_campaign'])
+    except (ValueError, Campaign.DoesNotExist):
+        campaign = Campaign()
+
     post_data = json.loads(request.POST['campaign'])
-    campaign = Campaign(
-        title=post_data['title'],
-        image_url=post_data['image_url'],
-        image_crop=json.dumps(post_data['image_crop']),
-        button_enabled=post_data['button_enabled'],
-        button_label=post_data['button_label'],
-        button_anchor=post_data['button_anchor'],
-        button_large=post_data['button_large'],
-        button_position=json.dumps(post_data['button_position']),
-        site=active_site,
-    )
+    campaign.title = post_data['title']
+    campaign.image_url = post_data['image_url']
+    campaign.image_crop = json.dumps(post_data['image_crop'])
+    campaign.button_enabled = post_data['button_enabled']
+    campaign.button_label = post_data['button_label']
+    campaign.button_anchor = post_data['button_anchor']
+    campaign.button_large = post_data['button_large']
+    campaign.button_position = json.dumps(post_data['button_position'])
+    campaign.site = active_site
     campaign.save()
+
+    campaign.text.all().delete()
+
     for post_text in post_data['text']:
         campaign_text = CampaignText(
             campaign=campaign,
