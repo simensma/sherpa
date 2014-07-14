d3c71f05b49d8eadf9a1d8e3ee9ec97490acd7ca
Ali Kaafarani <ali@kvikshaug.no>
2014-07-14 14:12:42 +0200

    Save the posted campaign data

--- apps/admin/sites/campaigns/views.py
+++ apps/admin/sites/campaigns/views.py
@@ -1,8 +1,10 @@
 # encoding: utf-8
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-from admin.models import Campaign
+from admin.models import Campaign, CampaignText
 from core.models import Site
+
+import json
 
 def index(request, site):
     active_site = Site.objects.get(id=site)
@@ -22,4 +24,24 @@ def new(request, site):
     return render(request, 'common/admin/sites/campaigns/new.html', context)
 
 def save(request, site):
-    pass # TBD
+    active_site = Site.objects.get(id=site)
+    post_data = json.loads(request.POST['campaign'])
+    campaign = Campaign(
+        title=post_data['title'],
+        image_url=post_data['image_url'],
+        image_crop=json.dumps(post_data['image_crop']),
+        button_enabled=post_data['button_enabled'],
+        button_label=post_data['button_label'],
+        button_anchor=post_data['button_anchor'],
+        button_large=post_data['button_large'],
+        site=active_site,
+    )
+    campaign.save()
+    for post_text in post_data['text']:
+        campaign_text = CampaignText(
+            campaign=campaign,
+            content=post_text['content'],
+            style=json.dumps(post_text['style']),
+        )
+        campaign_text.save()
+    return redirect('admin.sites.campaigns.views.index', active_site.id)
