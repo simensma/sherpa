f8273fb3098843a2b59c3f7b4bde24246ae2f4fa
Ali Kaafarani <ali@kvikshaug.no>
2014-07-16 11:50:24 +0200

    Rename URL path

--- apps/admin/sites/campaigns/urls.py
+++ apps/admin/sites/campaigns/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('admin.sites.campaigns.views',
     url(r'^$', 'index'),
-    url(r'^rediger/$', 'edit', {'campaign': None}),
+    url(r'^ny/$', 'edit', {'campaign': None}),
     url(r'^rediger/(?P<campaign>\d+)/$', 'edit'),
     url(r'^lagre/$', 'save'),
 )
