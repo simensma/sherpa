f059949bb49d1d1965139c2f5c5330089402f854
Ali Kaafarani <ali@kvikshaug.no>
2014-07-17 17:00:53 +0200

    Replace cleanimages simples3 usage with boto

--- apps/core/management/commands/cleanimages.py
+++ apps/core/management/commands/cleanimages.py
@@ -2,7 +2,7 @@
 from django.core.management.base import BaseCommand
 from django.conf import settings
 
-import simples3 # TODO: Replace with boto
+import boto
 import re
 
 from admin.models import Image
@@ -17,30 +17,28 @@ class Command(BaseCommand):
             self.stdout.write("Avbryter.\n")
             return
 
-        s3 = simples3.S3Bucket(
-            settings.AWS_BUCKET,
-            settings.AWS_ACCESS_KEY_ID,
-            settings.AWS_SECRET_ACCESS_KEY,
-            'https://%s' % settings.AWS_BUCKET)
+        conn = boto.connect_s3(settings.AWS_ACCESS_KEY_ID, settings.AWS_SECRET_ACCESS_KEY)
+        buck = conn.get_bucket(settings.AWS_BUCKET)
 
         ghost_keys = []
 
         self.stdout.write("\n")
         self.stdout.write("Henter dirlisting fra S3, vennligst vent...\n")
 
-        for (aws_key, modify, etag, size) in s3.listdir(prefix=settings.AWS_IMAGEGALLERY_PREFIX):
-            if aws_key == settings.AWS_IMAGEGALLERY_PREFIX:
+        for key in buck.list(prefix=settings.AWS_IMAGEGALLERY_PREFIX):
+            if key.name == settings.AWS_IMAGEGALLERY_PREFIX:
                 continue
 
             # Filter out thumbs
-            filename, extension = aws_key.rsplit('.', 1)
+            filename, extension = key.name.rsplit('.', 1)
             if any(filename.endswith('-%s' % thumb) for thumb in settings.THUMB_SIZES):
                 continue
 
-            key = re.sub(settings.AWS_IMAGEGALLERY_PREFIX, '', filename)
+            image_key = re.sub(settings.AWS_IMAGEGALLERY_PREFIX, '', filename)
 
-            if not Image.objects.filter(key=key).exists():
-                ghost_keys.append((filename, extension))
+            if not Image.objects.filter(key=image_key).exists():
+                ghost_keys.append(key)
+
 
         if len(ghost_keys) == 0:
             self.stdout.write("Fant ingen ghost images!\n")
@@ -49,28 +47,28 @@ class Command(BaseCommand):
         self.stdout.write("Fant %s ghost images:\n" % len(ghost_keys))
         self.stdout.write("\n")
 
-        for key, extension in ghost_keys:
+        for key in ghost_keys:
             content_check = ''
-            if Content.objects.filter(content__icontains=key).exists():
-                list_ids = Content.objects.filter(content__icontains=key).values_list('id', flat=True)
+            if Content.objects.filter(content__icontains=key.name).exists():
+                list_ids = Content.objects.filter(content__icontains=key.name).values_list('id', flat=True)
                 list_str = ', '.join([str(id) for id in list_ids])
                 content_check = " -- ADVARSEL, bildet er brukt i Content-felter med id: %s" % list_str
-            self.stdout.write("  http://%s/%s.%s%s\n" % (settings.AWS_BUCKET, key, extension, content_check))
+            self.stdout.write("  http://%s/%s%s\n" % (settings.AWS_BUCKET, key.name, content_check))
         self.stdout.write("\n")
 
         if raw_input("Slett dem? (y/N) ") != 'y':
             self.stdout.write("Avbryter.\n")
             return
 
-        def delete_image(key, extension):
-            s3.delete("%s.%s" % (key, extension))
+        def delete_image(key):
+            key.delete()
             for size in settings.THUMB_SIZES:
-                s3.delete("%s-%s.%s" % (key, str(size), extension))
+                buck.get_key("%s-%s.%s" % (key.name, size, extension)).delete()
 
         self.stdout.write("Sletter %s bildefiler (inkl. thumbs), vennligst vent...\n" % (len(ghost_keys) * (len(settings.THUMB_SIZES) + 1)))
 
-        for key, extension in ghost_keys:
-            delete_image(key, extension)
+        for key in ghost_keys:
+            delete_image(key)
 
         self.stdout.write("\n")
         self.stdout.write("Done.\n")
