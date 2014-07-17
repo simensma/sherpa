511864d672bc91590503537ca8a5b2d75d9e8ebe
Ali Kaafarani <ali@kvikshaug.no>
2014-07-17 17:50:00 +0200

    Save cropped/resized image file on S3

--- apps/admin/sites/campaigns/views.py
+++ apps/admin/sites/campaigns/views.py
@@ -1,7 +1,14 @@
 # encoding: utf-8
 from django.shortcuts import render, redirect
+from django.conf import settings
 
 import json
+from StringIO import StringIO
+from hashlib import sha1
+
+import requests
+import boto
+from PIL import Image
 
 from admin.models import Campaign, CampaignText
 from core.models import Site
@@ -28,15 +35,45 @@ def edit(request, site, campaign):
 def save(request, site):
     active_site = Site.objects.get(id=site)
 
+    post_data = json.loads(request.POST['campaign'])
+    crop = post_data['image_crop']
+
+    # Download the supplied image, crop/resize it, and save it to S3
+    r = requests.get(post_data['image_original'])
+    image = Image.open(StringIO(r.content))
+
+    width_ratio = float(image.size[0]) / crop['width']
+    height_ratio = float(image.size[1]) / crop['height']
+
+    box = (
+        int(crop['selection']['x'] * width_ratio),
+        int(crop['selection']['y'] * height_ratio),
+        int(crop['selection']['x2'] * width_ratio),
+        int(crop['selection']['y2'] * height_ratio),
+    )
+
+    campaign_image = image.crop(box).resize((940, 480))
+    campaign_image_file = StringIO()
+    campaign_image.save(campaign_image_file, "JPEG")
+    campaign_image_file = campaign_image_file.getvalue()
+
+    hash_ = sha1(campaign_image_file).hexdigest()
+
+    conn = boto.connect_s3(settings.AWS_ACCESS_KEY_ID, settings.AWS_SECRET_ACCESS_KEY)
+    bucket = conn.get_bucket(settings.AWS_BUCKET)
+
+    key = bucket.new_key("%s/%s.jpg" % (settings.AWS_CAMPAIGNS_PREFIX, hash_))
+    key.set_contents_from_string(campaign_image_file)
+
     try:
         campaign = Campaign.objects.get(id=request.POST['existing_campaign'])
     except (ValueError, Campaign.DoesNotExist):
         campaign = Campaign()
 
-    post_data = json.loads(request.POST['campaign'])
     campaign.title = post_data['title']
     campaign.image_original = post_data['image_original']
-    campaign.image_crop = json.dumps(post_data['image_crop'])
+    campaign.image_cropped_hash = hash_
+    campaign.image_crop = json.dumps(crop)
     campaign.photographer = request.POST['photographer']
     campaign.photographer_alignment = post_data['photographer_alignment']
     campaign.photographer_color = post_data['photographer_color']
