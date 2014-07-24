6341919c590aaba68ff189736b68d7e99b481977
Ali Kaafarani <ali@kvikshaug.no>
2014-07-24 10:40:52 +0200

    Add comment

--- static/js/admin/main.js
+++ static/js/admin/main.js
@@ -1,5 +1,6 @@
 $(function() {
 
+    // Simulate anchor click when user changes active forening
     $("select[name='user_forening']").chosen().change(function() {
         var a = $('<a class="jq-hide" href="' + $(this).find('option:selected').attr('data-href') + '">s</a>').appendTo(document.body).get(0).click();
     });
