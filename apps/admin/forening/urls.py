f73fc8488e9a5e1ec0781ed2345429a0d97f316b
Ali Kaafarani <ali@kvikshaug.no>
2014-07-29 10:56:06 +0200

    Remove unused urls include import

--- apps/admin/forening/urls.py
+++ apps/admin/forening/urls.py
@@ -1,5 +1,5 @@
 # encoding: utf-8
-from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('admin.forening.views',
     url(r'^(?P<forening_id>\d+)/$', 'index'),
