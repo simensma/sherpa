e86512bb9a4f1ff2aef34125f9533f8a8c1b63d9
Håvard Ranum <havard.ranum@bouvet.no>
2014-08-18 09:49:31 +0200

    Moved search input field and other users/organizations photos selects to search tab dropdown

--- static/js/admin/images/layout.js
+++ static/js/admin/images/layout.js
@@ -1,10 +1,10 @@
 $(function() {
 
-    $("div.image-archive-quicksearch select").chosen();
+    $("div.image-archive-quicksearch select").select2();
 
-    $("select[name='user-images']").change(function() {
+    $("select[name='user-images']").on('change', function (e) {
         var selected = $(this).find("option:selected");
-        var id = selected.val();
+        var id = e.val;
         var url = selected.attr('data-href').replace(/0\//, id + '/');
         window.location = url;
     });
