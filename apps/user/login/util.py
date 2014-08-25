8d6d8e90152426ca73c51f6be82941b1ed8a93b1
Ali Kaafarani <ali@kvikshaug.no>
2014-08-25 20:15:23 +0200

    Create inactive pending user on registration if applicable
    Since pending users theoretically could exist in focus.Enrollment
    without having been created in our User DB, we should account for this
    and create an inactive pending user object if applicable.

--- apps/user/login/util.py
+++ apps/user/login/util.py
@@ -111,7 +111,7 @@ def attempt_registration(request):
             if request.POST['country'] == 'NO':
                 enrollment = enrollment.filter(zipcode=request.POST['zipcode'])
             if enrollment.exists():
-                actor = User.get_users(include_pending=True).get(memberid=request.POST['memberid']).get_actor()
+                actor = User.get_or_create_inactive(memberid=request.POST['memberid'], include_pending=True).get_actor()
             else:
                 # Give up
                 raise ObjectDoesNotExist
