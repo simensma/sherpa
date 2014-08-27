09d15cda2699d247239efe403886598683552444
Ali Kaafarani <ali@kvikshaug.no>
2014-08-27 11:33:47 +0200

    Implement callback return for email type

--- static/js/admin/util/url-picker.js
+++ static/js/admin/util/url-picker.js
@@ -5,6 +5,8 @@
     var url_types;
     var save_button;
 
+    var callback;
+
     $(function() {
 
         /* Look up elements */
@@ -17,13 +19,15 @@
         /* Define event listeners */
 
         url_type_choices.find('input').change(changeUrlType);
+        save_button.click(saveUrl);
 
     });
 
     /* Public functions */
 
-    UrlPicker.open = function() {
+    UrlPicker.open = function(_callback) {
 
+        callback = _callback;
         url_picker.modal();
 
     };
@@ -34,6 +38,19 @@
         var url_type = url_type_choices.find('input:checked').attr('value');
         url_types.hide();
         url_types.filter('[data-dnt-url-type="' + url_type + '"]').slideDown('fast');
+    }
+
+    function saveUrl() {
+        var url_type = url_type_choices.find('input:checked').attr('value');
+        var url_type_wrapper = url_types.filter('[data-dnt-url-type="' + url_type + '"]');
+
+        if(url_type === 'email') {
+            callback({
+                type: 'email',
+                url: url_type_wrapper.find('input[name="email"]').val(),
+            });
+        }
+        url_picker.modal('hide');
     }
 
 }(window.UrlPicker = window.UrlPicker || {}, jQuery ));
