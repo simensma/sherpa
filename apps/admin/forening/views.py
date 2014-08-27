a65116b64087501eeacbf2595d1010a4d59093f8
Ali Kaafarani <ali@kvikshaug.no>
2014-08-27 15:33:31 +0200

    Cache sorted foreninger

--- apps/admin/forening/views.py
+++ apps/admin/forening/views.py
@@ -158,6 +158,7 @@ def index(request, forening_id):
                 messages.info(request, 'forening_save_success')
                 cache.delete('foreninger.all.sorted_by_name')
                 cache.delete('foreninger.all.sorted_by_name.with_active_url')
+                cache.delete('foreninger.all.sorted_by_type')
                 cache.delete('forening.%s' % forening.id)
                 cache.delete('forening.main_foreninger.%s' % forening.id)
                 return redirect('admin.forening.views.index', current_forening.id)
@@ -217,6 +218,7 @@ def index(request, forening_id):
                 request.session['active_forening'] = forening.id
                 cache.delete('foreninger.all.sorted_by_name')
                 cache.delete('foreninger.all.sorted_by_name.with_active_url')
+                cache.delete('foreninger.all.sorted_by_type')
                 cache.delete('forening.%s' % forening.id)
                 cache.delete('forening.main_foreninger.%s' % forening.id)
                 # Since GET url == POST url, we need to specifically set the tab hashtag we want, or the existing
--- apps/admin/util.py
+++ apps/admin/util.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 
+from django.core.cache import cache
+
 from page.models import Page, Version
 from foreninger.models import Forening
 
@@ -13,8 +15,13 @@ def url_picker_context(active_site):
         variant__article__site=active_site,
     ).order_by('-variant__article__pub_date')
 
+    foreninger_sorted = cache.get('foreninger.all.sorted_by_type')
+    if foreninger_sorted is None:
+        foreninger_sorted = Forening.sort(Forening.objects.all())
+        cache.set('foreninger.all.sorted_by_type', foreninger_sorted, 60 * 60 * 24 * 7)
+
     return {'url_picker': {
         'pages': Page.on(active_site).order_by('title'),
         'article_versions': article_versions,
-        'foreninger': Forening.sort(Forening.objects.all()),
+        'foreninger': foreninger_sorted,
     }}
