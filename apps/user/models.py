6cc121988ef8209debd185f8050fc9154ae475b6
Ali Kaafarani <ali@kvikshaug.no>
2014-09-04 10:33:46 +0200

    Add method comment to get_payment_status

--- apps/user/models.py
+++ apps/user/models.py
@@ -281,6 +281,13 @@ class User(AbstractBaseUser):
         return self.get_actor().has_paid()
 
     def get_payment_status(self):
+        """
+        Returns a dict explaining the users' payment status.
+        'new_membership_year' will be False before årskravet, and True after.
+        'current_year' is always included and is True if the user has paid their membership for the current year.
+        'next_year' is ONLY included if 'new_membership_year' is True, and will be True if the user has paid for the
+          next years' membership.
+        """
         from core.util import membership_year_start
         start_date = membership_year_start()['actual_date']
         today = date.today()
