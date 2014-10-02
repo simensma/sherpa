d2ff546a66e8b83e7b8fae9c5be0a8c2279a64c3
Ali Kaafarani <ali@kvikshaug.no>
2014-10-02 10:54:42 +0200

    Implement aktivitet-listing

--- apps/page/widgets/aktivitet_listing.py
+++ apps/page/widgets/aktivitet_listing.py
@@ -1,5 +1,10 @@
 from page.widgets.widget import Widget
+from aktiviteter.util import filter_aktivitet_dates
 
 class AktivitetListingWidget(Widget):
     def parse(self, widget_options, site):
-        return {}
+        aktivitet_dates = filter_aktivitet_dates({
+            'page': 1,
+        })
+
+        return {'aktivitet_dates': aktivitet_dates}
