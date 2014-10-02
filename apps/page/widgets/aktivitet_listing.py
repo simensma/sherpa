9b24587b469b577901e3b1353c24e56330c62d14
Ali Kaafarani <ali@kvikshaug.no>
2014-10-02 14:01:59 +0200

    Filter widget list on chosen organizer

--- apps/page/widgets/aktivitet_listing.py
+++ apps/page/widgets/aktivitet_listing.py
@@ -1,5 +1,7 @@
 from datetime import date
 
+from django.db.models import Q
+
 from page.widgets.widget import Widget
 from aktiviteter.models import AktivitetDate
 from foreninger.models import Forening
@@ -7,6 +9,8 @@ from foreninger.models import Forening
 class AktivitetListingWidget(Widget):
     def parse(self, widget_options, site):
         aktivitet_dates = AktivitetDate.get_published().filter(
+            Q(aktivitet__forening__in=widget_options['foreninger']) |
+            Q(aktivitet__co_foreninger__in=widget_options['foreninger']),
             aktivitet__private=False,
             start_date__gte=date.today(),
         )
