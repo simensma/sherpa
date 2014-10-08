dd2fa711120690df6c6248219dd32a6af25d4f68
Ali Kaafarani <ali@kvikshaug.no>
2014-10-08 15:42:32 +0200

    Rewrite the button widget
    - Fewer available colors (red and white)
    - Fewer available sizes (large and medium)
    - Different UI controls (single preview button)

--- static/js/admin/sites/editor/widgets/button.js
+++ static/js/admin/sites/editor/widgets/button.js
@@ -1,24 +1,57 @@
 $(function() {
 
+    var preview_button;
+
     WidgetEditor.listen({
         widget_name: 'button',
 
         init: function(editor) {
-            editor.find("table.choices button").click(function() {
-                $(this).parent().prev().children("input[type='radio']").click();
+            preview_button = editor.find('[data-dnt-preview-button]');
+            preview_button.text(preview_button.attr('data-dnt-placeholder'));
+
+            editor.find('input[name="text"]').keyup(function() {
+                var text = $(this).val().trim();
+                if(text === '') {
+                    text = preview_button.attr('data-dnt-placeholder');
+                }
+                preview_button.text(text);
+            });
+
+            editor.find('input[name="color"]').change(function() {
+                preview_button.removeClass('btn-default btn-danger');
+                preview_button.addClass($(this).val());
+            });
+
+            editor.find('input[name="size"]').change(function() {
+                preview_button.removeClass('btn-lg');
+                preview_button.addClass($(this).val());
             });
         },
 
+        onNew: function(editor) {
+            editor.find("input[name='text']").val('');
+            editor.find("input[name='url']").val('http://');
+            editor.find("input[name='color'][value='btn-default']").prop('checked', true);
+            editor.find("input[name='size'][value='']").prop('checked', true);
+        },
+
         onEdit: function(editor, widget_content) {
-            // TODO: Reset all editor-controls based on the current widget state
+            editor.find("input[name='text']").val(widget_content.text);
             editor.find("input[name='url']").val(widget_content.url);
+            editor.find("input[name='color'][value='" + widget_content.color + "']").prop('checked', true);
+            editor.find("input[name='size'][value='" + widget_content.size + "']").prop('checked', true);
+
+            // Trigger preview button updates
+            editor.find("input[name='text']").keyup();
+            editor.find("input[name='color']:checked").change();
+            editor.find("input[name='size']:checked").change();
         },
 
         onSave: function(editor) {
             var text = editor.find("input[name='text']").val().trim();
             var url = editor.find("input[name='url']").val().trim();
             if(text === "") {
-                editor.find("div.alert").show();
+                alert(editor.attr('data-no-text-warning'));
                 return false;
             }
 
