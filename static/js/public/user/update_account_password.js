a50b32456e4de9430b52dc24c2dcd141ac989a1b
Ali Kaafarani <ali@kvikshaug.no>
2014-10-08 12:44:12 +0200

    Adapt user password form for BS3

--- static/js/public/user/update_account_password.js
+++ static/js/public/user/update_account_password.js
@@ -1,11 +1,11 @@
 $(function() {
 
     Validator.validatePasswords({
-        control_group: $("div.control-group.password, div.control-group.password-repeat"),
-        pass1: $("input[name='password']"),
-        pass2: $("input[name='password-repeat']"),
+        form_group: $('[data-dnt-form-group="password"], [data-dnt-form-group="password-repeat"]'),
+        pass1: $('input[name="password"]'),
+        pass2: $('input[name="password-repeat"]'),
         min_length: Turistforeningen.user_password_length,
-        hints: $("div.control-group.password div.hints.validator")
+        hints: $('[data-dnt-form-group="password"] .validator')
     });
 
     // Trigger all validations
