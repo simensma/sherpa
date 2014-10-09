d09e9fec29546409b7b49904c13c83ab194ff597
Ali Kaafarani <ali@kvikshaug.no>
2014-10-09 10:23:59 +0200

    Replace the article widget tagger with select2

--- static/js/admin/sites/editor/widgets/articles.js
+++ static/js/admin/sites/editor/widgets/articles.js
@@ -1,90 +1,85 @@
 $(function() {
 
+    var $tags_enabled;
+    var $tags_input;
+
     WidgetEditor.listen({
         widget_name: 'articles',
 
-        init: function(editor) {
-            editor.find("input[name='layout']").click(function() {
-                if(editor.find("input[name='layout']:checked").val() === 'medialist') {
-                    editor.find("div.medialist-section").show();
-                    editor.find("div.horizontal-section").hide();
+        init: function($editor) {
+            $tags_enabled = $editor.find('input[name="enable-tags"]');
+            $tags_input = $editor.find('input[name="tags"]');
+
+            $editor.find("input[name='layout']").click(function() {
+                if($editor.find("input[name='layout']:checked").val() === 'medialist') {
+                    $editor.find("div.medialist-section").show();
+                    $editor.find("div.horizontal-section").hide();
                 } else {
-                    editor.find("div.medialist-section").hide();
-                    editor.find("div.horizontal-section").show();
+                    $editor.find("div.medialist-section").hide();
+                    $editor.find("div.horizontal-section").show();
                 }
             });
 
             // Enable/disable
-            var url = editor.find("input[name='tag-link']").attr('data-tags-url');
-            editor.find("input[name='tag-link']").typeahead({
+            var url = $editor.find("input[name='tag-link']").attr('data-tags-url');
+            $editor.find("input[name='tag-link']").typeahead({
                 minLength: 3,
                 remote: url + "?q=%QUERY"
             });
-            editor.find("input[name='set-tag-link']").change(function() {
+            $editor.find("input[name='set-tag-link']").change(function() {
                 if($(this).is(':checked')) {
-                    editor.find("input[name='tag-link']").prop('disabled', false);
+                    $editor.find("input[name='tag-link']").prop('disabled', false);
                 } else {
-                    editor.find("input[name='tag-link']").prop('disabled', true).val("");
+                    $editor.find("input[name='tag-link']").prop('disabled', true).val("");
                 }
             });
 
-            editor.find("input[name='enable-tags']").change(function() {
-                if($(this).is(':checked')) {
-                    editor.find("input[name='tags']").prop('disabled', false);
-                } else {
-                    editor.find("input[name='tags']").prop('disabled', true).val("");
-                    editor.find("div.tag-box").empty();
-                }
+            $tags_enabled.change(function() {
+                $tags_input.select2('enable', $(this).is(':checked'));
             });
 
-            TagDisplay.enable({
-                ref: 'article_widget',
-                tagBox: editor.find("div.tag-box"),
-                pickerInput: editor.find("input[name='tags']")
-            });
+            Select2Tagger({$input: $tags_input});
         },
 
-        onNew: function(editor) {
-            TagDisplay.reset('article_widget');
+        onNew: function($editor) {
+            $tags_enabled.prop('checked', false);
+            $tags_input.select2('val', '').select2('enable', false);
         },
 
-        onEdit: function(editor, widget_content) {
-            editor.find("input[name='layout'][value='" + widget_content.layout + "']").click();
+        onEdit: function($editor, widget_content) {
+            $editor.find("input[name='layout'][value='" + widget_content.layout + "']").click();
             if(widget_content.layout === 'medialist') {
-                editor.find("div.medialist-section").show();
-                editor.find("div.horizontal-section").hide();
+                $editor.find("div.medialist-section").show();
+                $editor.find("div.horizontal-section").hide();
             } else {
-                editor.find("div.medialist-section").hide();
-                editor.find("div.horizontal-section").show();
+                $editor.find("div.medialist-section").hide();
+                $editor.find("div.horizontal-section").show();
             }
-            editor.find("select[name='columns'] option[value='" + widget_content.columns + "']").prop('selected', true);
-            editor.find("input[name='title']").val(widget_content.title);
-            editor.find("input[name='count']").val(widget_content.count);
-            editor.find("input[name='display-images']").prop('checked', widget_content.display_images);
+            $editor.find("select[name='columns'] option[value='" + widget_content.columns + "']").prop('selected', true);
+            $editor.find("input[name='title']").val(widget_content.title);
+            $editor.find("input[name='count']").val(widget_content.count);
+            $editor.find("input[name='display-images']").prop('checked', widget_content.display_images);
             if(widget_content.tag_link === null) {
-                editor.find("input[name='set-tag-link']").prop('checked', false);
-                editor.find("input[name='tag-link']").prop('disabled', true).val("");
+                $editor.find("input[name='set-tag-link']").prop('checked', false);
+                $editor.find("input[name='tag-link']").prop('disabled', true).val("");
             }
-            var tag_box = editor.find("div.tag-box");
-            tag_box.attr('data-predefined-tags', JSON.stringify(widget_content.tags));
-            TagDisplay.reset('article_widget');
-            if(widget_content.tags.length > 0) {
-                editor.find("input[name='enable-tags']").prop('checked', true);
-                editor.find("input[name='tags']").prop('disabled', false);
-            } else {
-                editor.find("input[name='enable-tags']").prop('checked', false);
-                editor.find("input[name='tags']").prop('disabled', true);
-            }
+
+            // Can't use empty array to clear select2, must use empty string
+            $tags_input.select2('val', widget_content.tags.length > 0 ? widget_content.tags : '');
+
+            var has_tags = widget_content.tags.length > 0;
+            $tags_enabled.prop('checked', has_tags);
+            $tags_input.select2('enable', has_tags);
         },
 
-        onSave: function(editor) {
-            var layout = editor.find("input[name='layout']:checked").val();
+        onSave: function($editor) {
+            var layout = $editor.find("input[name='layout']:checked").val();
 
             var title;
             var count;
             var columns;
             if(layout === 'medialist') {
-                count = editor.find("input[name='count']").val();
+                count = $editor.find("input[name='count']").val();
                 if(isNaN(Number(count))) {
                     alert("Du må angi et tall for antall nyheter som skal vises!");
                     return false;
@@ -92,12 +87,12 @@ $(function() {
                     alert("Du må vise minst én nyhet!");
                     return false;
                 }
-                title = editor.find("input[name='title']").val();
+                title = $editor.find("input[name='title']").val();
 
                 // Set unused default value for columns
                 columns = 2;
             } else if(layout === 'horizontal') {
-                columns = editor.find("select[name='columns'] option:selected").val();
+                columns = $editor.find("select[name='columns'] option:selected").val();
 
                 // Set unused default value for count/title
                 count = 1;
@@ -107,16 +102,16 @@ $(function() {
                 return false;
             }
 
-            var display_images = editor.find("input[name='display-images']").prop('checked');
+            var display_images = $editor.find("input[name='display-images']").prop('checked');
             var tag_link;
-            if(editor.find("input[name='set-tag-link']").is(':checked')) {
-                tag_link = editor.find("input[name='tag-link']").val();
+            if($editor.find("input[name='set-tag-link']").is(':checked')) {
+                tag_link = $editor.find("input[name='tag-link']").val();
             } else {
                 tag_link = null;
             }
             var tags;
-            if(editor.find("input[name='enable-tags']:checked").length > 0) {
-                tags = TagDisplay.getTags('article_widget');
+            if($tags_enabled.is(':checked')) {
+                tags = $tags_input.select2('val');
             } else {
                 tags = [];
             }
