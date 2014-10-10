1d0f4cc31540e8aa21d8acdf4591816d3dcd178b
Ali Kaafarani <ali@kvikshaug.no>
2014-10-10 10:12:42 +0200

    Apply the URL picker modal for the button widget

--- static/js/admin/sites/editor/widgets/button.js
+++ static/js/admin/sites/editor/widgets/button.js
@@ -1,12 +1,15 @@
 $(function() {
 
     var preview_button;
+    var url_static_input;
 
     WidgetEditor.listen({
         widget_name: 'button',
 
         init: function(editor) {
             preview_button = editor.find('[data-dnt-preview-button]');
+            url_static_input = editor.find('[data-dnt-input="url"]');
+
             preview_button.text(preview_button.attr('data-dnt-placeholder'));
 
             editor.find('input[name="text"]').keyup(function() {
@@ -15,6 +18,21 @@ $(function() {
                     text = preview_button.attr('data-dnt-placeholder');
                 }
                 preview_button.text(text);
+            });
+
+            editor.find('[data-dnt-trigger="pick-url"]').click(function() {
+                var existing_url;
+                var url = url_static_input.text().trim();
+                if(url !== url_static_input.attr('data-dnt-placeholder')) {
+                    existing_url = url;
+                }
+
+                UrlPicker.open({
+                    existing_url: existing_url,
+                    done: function(result) {
+                        url_static_input.text(result.url);
+                    }
+                });
             });
 
             editor.find('input[name="color"]').change(function() {
@@ -30,14 +48,16 @@ $(function() {
 
         onNew: function(editor) {
             editor.find("input[name='text']").val('');
+            url_static_input.text(url_static_input.attr('data-dnt-placeholder'));
             editor.find("input[name='url']").val('http://');
+            editor.find('[data-dnt-input="url"]').val('http://');
             editor.find("input[name='color'][value='btn-default']").prop('checked', true);
             editor.find("input[name='size'][value='']").prop('checked', true);
         },
 
         onEdit: function(editor, widget_content) {
             editor.find("input[name='text']").val(widget_content.text);
-            editor.find("input[name='url']").val(widget_content.url);
+            url_static_input.text(widget_content.url);
             editor.find("input[name='color'][value='" + widget_content.color + "']").prop('checked', true);
             editor.find("input[name='size'][value='" + widget_content.size + "']").prop('checked', true);
 
@@ -49,14 +69,16 @@ $(function() {
 
         onSave: function(editor) {
             var text = editor.find("input[name='text']").val().trim();
-            var url = editor.find("input[name='url']").val().trim();
+            var url = url_static_input.text().trim();
+
             if(text === "") {
                 alert(editor.attr('data-no-text-warning'));
                 return false;
             }
 
-            if(!url.match(/^https?:\/\//)) {
-                url = "http://" + url;
+            if(url === url_static_input.attr('data-dnt-placeholder')) {
+                alert(editor.attr('data-no-url-warning'));
+                return false;
             }
 
             var color = editor.find("input[name='color']:checked").val();
