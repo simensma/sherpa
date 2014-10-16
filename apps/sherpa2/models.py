e72aaa0b6e981554308fd5670def2638842f5efe
Ali Kaafarani <ali@kvikshaug.no>
2014-10-16 10:08:57 +0200

    Remove erroneous argument

--- apps/sherpa2/models.py
+++ apps/sherpa2/models.py
@@ -662,7 +662,7 @@ class Activity(models.Model):
         return category_type
 
     def convert_pub_date(self):
-        if self.pub_date.strip(0) == '':
+        if self.pub_date.strip() == '':
             return date.today()
         return self.get_pub_date()
 
