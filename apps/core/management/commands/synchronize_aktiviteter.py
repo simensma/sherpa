fec7bd742b11f6d6c1a4e8377f2becff41b2fb73
Ali Kaafarani <ali@kvikshaug.no>
2014-10-24 10:17:58 +0200

    Explicitly log unhandled exceptions during synchronization

--- apps/core/management/commands/synchronize_aktiviteter.py
+++ apps/core/management/commands/synchronize_aktiviteter.py
@@ -1,11 +1,21 @@
 # encoding: utf-8
+import logging
+import sys
+
 from django.core.management.base import BaseCommand
 
 from sherpa2.models import Activity
+
+logger = logging.getLogger('sherpa')
 
 class Command(BaseCommand):
     args = u""
     help = u"Synkroniserer alle aktiviteter fra gamle Sherpa til nye - kjøres vanligvis kun av crontab"
 
     def handle(self, *args, **options):
-        Activity.sync_all()
+        try:
+            Activity.sync_all()
+        except:
+            logger.error(u"Uhåndtert exception ved synkronisering av aktiviteter",
+                exc_info=sys.exc_info(),
+            )
