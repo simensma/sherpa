d4ac6ca843f90c65b575fcd0ef3a92b360fd76e0
Ali Kaafarani <ali@kvikshaug.no>
2014-10-30 10:43:39 +0100

    Use css with box-model to fill wrapper dimensions

--- static/js/public/user/publication.js
+++ static/js/public/user/publication.js
@@ -8,17 +8,4 @@ $(function() {
         $(this).find('.hover-content').fadeOut(200);
     });
 
-    $(window).load(function() {
-        // Set the hover-content dimensions after images have loaded
-        content_wrappers.each(function() {
-            var content = $(this).find("div.hover-content");
-            var img = $(this).find("img");
-            var contentWidthPadding = content.outerWidth() - content.width();
-            var contentHeightPadding = content.outerHeight() - content.height();
-            content.offset(img.offset());
-            content.width(img.width() - contentWidthPadding);
-            content.height(img.height() - contentHeightPadding);
-        });
-    });
-
 });
