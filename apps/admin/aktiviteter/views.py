6fc82d6193f7440fcc257f0d1bacefae542e3f1f
Ali Kaafarani <ali@kvikshaug.no>
2014-10-30 11:59:00 +0100

    Apply DISTINCT when filtering on multiple foreninger

--- apps/admin/aktiviteter/views.py
+++ apps/admin/aktiviteter/views.py
@@ -523,7 +523,7 @@ def turleder_search(request):
 def failed_imports(request):
     # Look up failed imports for all underlying foreninger as well
     foreninger = request.active_forening.get_with_children_deep()
-    failed_imports = ConversionFailure.objects.filter(foreninger__in=foreninger).order_by('-latest_date')
+    failed_imports = ConversionFailure.objects.filter(foreninger__in=foreninger).distinct().order_by('-latest_date')
 
     paginator = Paginator(failed_imports, 40)
     try:
