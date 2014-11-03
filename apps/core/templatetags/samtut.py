badfc45727d6fe97b80e8867ff748e8585aa599e
Ali Kaafarani <ali@kvikshaug.no>
2014-11-03 11:14:13 +0100

    Translate 'samt ut' text snippet

--- apps/core/templatetags/samtut.py
+++ apps/core/templatetags/samtut.py
@@ -1,4 +1,5 @@
 from django import template
+from django.utils.translation import ugettext as _
 
 register = template.Library()
 
@@ -7,9 +8,12 @@ def samtut(membership_year_date_set):
     """
     This filter is used for text formatting, which could be done with template logic but it requires
     a fair amount of logic and it's a fairly typical pattern.
-    TODO: Translate
     """
     if membership_year_date_set['has_passed']:
-        return '%s, samt ut %s' % (membership_year_date_set['date'].year + 1, membership_year_date_set['date'].year)
+        return '%s, %s %s' % (
+            membership_year_date_set['date'].year + 1,
+            _('samt ut'),
+            membership_year_date_set['date'].year,
+        )
     else:
         return '%s' % membership_year_date_set['date'].year
