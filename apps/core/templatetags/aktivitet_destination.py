cb7f7573dda897d0bb7b75bec047b38a803a2bdb
Ali Kaafarani <ali@kvikshaug.no>
2014-11-07 12:34:21 +0100

    Return the destination site, not forening

--- apps/core/templatetags/aktivitet_destination.py
+++ apps/core/templatetags/aktivitet_destination.py
@@ -14,13 +14,12 @@ def aktivitet_destination(aktivitet_date, request):
             current_forening == aktivitet_date.aktivitet.forening or
             current_forening in aktivitet_date.aktivitet.co_foreninger.all()):
         # The current site is one of the arranging foreninger, so we can stay on this site
-        return current_forening
+        return current_site
 
-    arranging_forening = aktivitet_date.aktivitet.forening
-    arranging_site = arranging_forening.get_homepage_site(prefetched=True)
+    arranging_site = aktivitet_date.aktivitet.forening.get_homepage_site(prefetched=True)
     if arranging_site is not None and arranging_site.is_published:
         # The arranging forening's site is up and live, so go there
-        return arranging_forening
+        return arranging_site
 
     # Their site isn't published, so just stay wherever we are
-    return current_forening
+    return current_site
