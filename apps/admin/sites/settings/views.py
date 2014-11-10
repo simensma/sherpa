dff2bca1bf35a6e74d930a5277b8294d5d86f079
Ali Kaafarani <ali@kvikshaug.no>
2014-11-10 14:47:29 +0100

    Add site type and title to site settings form

--- apps/admin/sites/settings/views.py
+++ apps/admin/sites/settings/views.py
@@ -1,14 +1,20 @@
-from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.core.exceptions import PermissionDenied
+from django.shortcuts import render, redirect
 
 from core.models import Site
 
 def index(request, site):
     active_site = Site.objects.get(id=site)
-    context = {'active_site': active_site}
+    context = {
+        'active_site': active_site,
+        'site_types': Site.TYPE_CHOICES,
+    }
+
     if 'message_context' in request.session:
         context['message_context'] = request.session['message_context']
         del request.session['message_context']
+
     return render(request, 'common/admin/sites/settings/index.html', context)
 
 def save(request, site):
@@ -18,6 +24,17 @@ def save(request, site):
     active_site = Site.objects.get(id=site)
     domain = request.POST['domain'].strip().lower().replace('http://', '').rstrip('/')
     errors = False
+
+    type = request.POST['type']
+    if type not in [t[0] for t in Site.TYPE_CHOICES]:
+        raise PermissionDenied
+
+    active_site.type = type
+
+    if type in ['hytte', 'kampanje']:
+        active_site.title = request.POST['title'].strip()
+    else:
+        active_site.title = ''
 
     if domain == active_site.domain:
         # Special case; the domain wasn't changed - so just pretend that it's updated
