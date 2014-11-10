be14dc452d1bea8afebad50a2a429fd0a96e3f13
Ali Kaafarani <ali@kvikshaug.no>
2014-11-10 14:05:05 +0100

    Separate return logic from domain verification

--- apps/admin/sites/settings/views.py
+++ apps/admin/sites/settings/views.py
@@ -21,18 +21,17 @@ def save(request, site):
     if domain == active_site.domain:
         # Special case; the domain wasn't changed - so just say that it worked
         messages.info(request, 'domain_updated')
-        return redirect('admin.sites.settings.views.index', active_site.id)
-
-    result = Site.verify_domain(domain)
-    if not result['valid']:
-        messages.error(request, result['error'])
-        if result['error'] == 'site_exists':
-            request.session['message_context'] = {'existing_forening': result['existing_forening']}
-        return redirect('admin.sites.settings.views.index', active_site.id)
     else:
-        messages.info(request, 'domain_updated')
-        active_site.domain = result['domain']
-        active_site.prefix = result['prefix']
-        active_site.save()
-        request.session.modified = True
-        return redirect('admin.sites.settings.views.index', active_site.id)
+        result = Site.verify_domain(domain)
+        if not result['valid']:
+            messages.error(request, result['error'])
+            if result['error'] == 'site_exists':
+                request.session['message_context'] = {'existing_forening': result['existing_forening']}
+        else:
+            messages.info(request, 'domain_updated')
+            active_site.domain = result['domain']
+            active_site.prefix = result['prefix']
+            active_site.save()
+            request.session.modified = True
+
+    return redirect('admin.sites.settings.views.index', active_site.id)
