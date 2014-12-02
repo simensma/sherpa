116511db76dd92e5def00a2cf601198e270af576
Ali Kaafarani <ali@kvikshaug.no>
2014-12-02 08:57:39 +0100

    Add spacing between operators

--- apps/aktiviteter/models.py
+++ apps/aktiviteter/models.py
@@ -297,15 +297,15 @@ class AktivitetDate(models.Model):
         return self.other_dates().order_by('-start_date')
 
     def get_duration_days(self):
-        diff = self.end_date-self.start_date
+        diff = self.end_date - self.start_date
         return diff.days
 
     def get_duration_hours(self):
-        diff = self.end_date-self.start_date
+        diff = self.end_date - self.start_date
         return diff.seconds / 3600
 
     def get_duration(self):
-        diff = self.end_date-self.start_date
+        diff = self.end_date - self.start_date
         days = diff.days
 
         if diff.total_seconds() == 0:
