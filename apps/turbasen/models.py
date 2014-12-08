8529c769805651d7a71cff84278fd371a90e039a
Ali Kaafarani <ali@kvikshaug.no>
2014-12-08 15:43:37 +0100

    Perform single object lookup when accessing missing attribute

--- apps/turbasen/models.py
+++ apps/turbasen/models.py
@@ -9,9 +9,26 @@ class NTBObject(object):
     ENDPOINT_URL = u'https://ntbprod-turistforeningen.dotcloud.com/'
     TURBASE_LOOKUP_COUNT = 50
 
+    def __getattr__(self, name):
+        """On attribute lookup failure, if the object is only partially retrieved, get the rest of its data and try
+        again"""
+        if self._is_partial:
+            self.get()
+            return getattr(self, name)
+        raise AttributeError
+
     @staticmethod
     def lookup_object(identifier):
         return NTBObject._lookup_recursively(identifier, skip=0, previous_results=[])
+
+    @staticmethod
+    def get_object(identifier, objectid):
+        return requests.get(
+            '%s%s/%s/' % (NTBObject.ENDPOINT_URL, identifier, objectid),
+            params={
+                'api_key': settings.TURBASEN_API_KEY,
+            }
+        ).json()
 
     @staticmethod
     def _lookup_recursively(identifier, skip, previous_results):
@@ -35,17 +52,31 @@ class NTBObject(object):
 class Omrade(NTBObject):
     identifier = u'områder'
 
-    def __init__(self, navn, status, endret, lisens, tilbyder, _is_partial=False):
+    def __init__(self, objectid, navn, status, endret, lisens, tilbyder, _is_partial=False):
+        self.objectid = objectid
         self.navn = navn
         self.status = status
         self.endret = endret
         self.lisens = lisens
         self.tilbyder = tilbyder
+        self._is_partial = _is_partial
+
+    def get(self):
+        document = NTBObject.get_object(self.identifier, self.objectid)
+        self.navngiving = document['navngiving']
+        self.status = document['status']
+        self.geojson = document['geojson']
+        self.kommuner = document['kommuner']
+        self.fylker = document['fylker']
+        self.beskrivelse = document['beskrivelse']
+        self.bilder = document['bilder']
+        self._partial = False
 
     @staticmethod
     def lookup():
         return [Omrade(
             _is_partial=True,
+            objectid=doc['_id'],
             navn=doc['navn'],
             status=doc['status'],
             endret=doc['endret'],
