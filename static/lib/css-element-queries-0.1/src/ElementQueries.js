a8c9ac8358c96fa2097239425949e0f96f748daa
Hans Kristian Flaatten <hans.kristian.flaatten@turistforeningen.no>
2015-01-14 12:37:46 +0100

    Supress SecurityError from ElementQueries.js
    Sentry: sherpa-js/164
    Github: marcj/css-element-queries#16
    Stack Owerflow: 21642277

--- static/lib/css-element-queries-0.1/src/ElementQueries.js
+++ static/lib/css-element-queries-0.1/src/ElementQueries.js
@@ -221,7 +221,13 @@
          */
         this.init = function() {
             for (var i = 0, j = document.styleSheets.length; i < j; i++) {
-                readRules(document.styleSheets[i].cssText || document.styleSheets[i].cssRules || document.styleSheets[i].rules);
+                try {
+                    readRules(document.styleSheets[i].cssText || document.styleSheets[i].cssRules || document.styleSheets[i].rules);
+                } catch(e) {
+                    if (e.name !== 'SecurityError') {
+                        throw e;
+                    }
+                }
             }
         };
 
