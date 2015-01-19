d9ad5ba35bf101a3b83857e2923c89f3fa544c23
Håvard Ranum <havard.ranum@me.com>
2015-01-19 12:29:29 +0100

    Added file upload button to editor toolbar

--- static/js/admin/sites/editor/toolbar.js
+++ static/js/admin/sites/editor/toolbar.js
@@ -215,6 +215,47 @@ $(function() {
             },
         });
     });
+
+    toolbar.find("a.button.attach-file").click(function(event) {
+        var selection = rangy.getSelection();
+
+        if(selection === undefined || selection.rangeCount === 0) {
+            // No selection or ranges - ignore the file upload button click
+            return;
+        }
+
+        var range = selection.getRangeAt(0);
+        var ancestor = $(range.commonAncestorContainer).parent();
+
+        if(ancestor.parents('.editable').length === 0 && !ancestor.is('.editable')) {
+            // User hasn't selected text in an editable element - ignore the file upload button click
+            return;
+        }
+
+        var existing_url;
+        var existing_text;
+        if(ancestor.is('a')) {
+            existing_url = ancestor.attr('href');
+            existing_text = ancestor.text();
+
+            // Make sure the entire anchor is included in the range
+            range.setStartBefore(range.startContainer);
+            range.setEndAfter(range.startContainer);
+        }
+        FileUploader.open({
+            existing_url: existing_url,
+            existing_text: existing_text,
+            done: function(result) {
+                // Trim the selection for whitespace (actually, just the last char, since that's most common)
+                if($(range.endContainer).text().substring(range.endOffset - 1, range.endOffset) == ' ') {
+                    range.setEnd(range.endContainer, range.endOffset - 1);
+                }
+                selection.setSingleRange(range);
+                document.execCommand('createLink', false, result.url);
+            },
+        });
+    });
+
     toolbar.find("a.anchor-remove").click(function(event) {
         document.execCommand('unlink', false, null);
     });
