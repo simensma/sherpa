b26d3a771edef8d9769837990c729d117f0c1f94
Ali Kaafarani <ali@kvikshaug.no>
2015-02-03 14:36:51 +0100

    Merge migrations

---
+++ apps/page/migrations/0004_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('page', '0003_auto_20150128_1640'),
+        ('page', '0002_auto_20150126_1754'),
+    ]
+
+    operations = [
+    ]
