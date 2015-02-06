39a7f8237b920cb42e12b7600f3309dd2d621951
Ali Kaafarani <ali@kvikshaug.no>
2015-02-06 10:43:24 +0100

    Handle household without parent in API SMS resource

--- apps/api/api.py
+++ apps/api/api.py
@@ -291,7 +291,9 @@ def memberid(request, version, format):
         # Check if any of these are related, and in that case, use the parent.
         user = None
         for user_to_check in users:
-            if user_to_check.is_household_member() and user_to_check.get_parent() in users:
+            if user_to_check.is_household_member() and \
+                    user_to_check.get_parent() is not None and \
+                    user_to_check.get_parent() in users:
                 # Ah, this parent is in the result set - probably the one we want, use it
                 user = user_to_check.get_parent()
                 break
