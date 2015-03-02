5484e20b79cb6814aab4ee4811e20d69e89c6123
Ali Kaafarani <ali@kvikshaug.no>
2015-03-02 15:12:00 +0100

    Add navigation function

--- static/polymer/src/elements/dnt-aktivitet-signup/dnt-aktivitet-signup.js
+++ static/polymer/src/elements/dnt-aktivitet-signup/dnt-aktivitet-signup.js
@@ -1,10 +1,20 @@
 (function () {
   Polymer({
+
+    /* Data model */
+
     aktivitet: {
         title: 'På ski i Huldreheimen',
     },
     state: {
         step: 'participants',
     },
+
+    /* Functions */
+
+    navigateTo: function(step) {
+        this.state.step = step;
+    }
+
   });
 })();
