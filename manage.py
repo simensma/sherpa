91c6db95a784bde2a66d82354bc8945bef0f6ba6
Ali Kaafarani <ali@kvikshaug.no>
2015-03-03 14:36:41 +0100

    Revert "Apply django-configurations to manage.py and wsgi.py"
    This reverts commit 00671c4ee0ae4f7ebcf21b11e653bf9534ad464e.

--- manage.py
+++ manage.py
@@ -5,8 +5,6 @@ import sys
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sherpa.settings")
 
-    # Note that django-configurations overrides Django's builtin function
-    # from django.core.management import execute_from_command_line
-    from configurations.management import execute_from_command_line
+    from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
--- sherpa/wsgi.py
+++ sherpa/wsgi.py
@@ -18,9 +18,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
-# Note that django-configurations overrides Django's builtin function
-# from django.core.wsgi import get_wsgi_application
-from configurations.wsgi import get_wsgi_application
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
 # Apply WSGI middleware here.
