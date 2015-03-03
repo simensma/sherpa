8c7b6ae91b6b9f6a5ddf302013a1175860f27f91
Ali Kaafarani <ali@kvikshaug.no>
2015-03-03 19:13:00 +0100

    Add Redirect middleware overlooked in merge

--- sherpa/conf/prod.py
+++ sherpa/conf/prod.py
@@ -272,6 +272,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
+    'sherpa.middleware.Redirect',
     'sherpa.middleware.DBConnection',
     'sherpa.middleware.DefaultLanguage',
     'sherpa.middleware.Sites',
