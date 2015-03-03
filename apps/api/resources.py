707882046fbf184324394b208bfb7f48b24c46b8
Ali Kaafarani <ali@kvikshaug.no>
2015-03-03 12:10:46 +0100

    Add AktivitetDate resource to the API

--- apps/api/resources.py
+++ apps/api/resources.py
@@ -1,7 +1,16 @@
+from tastypie import fields
 from tastypie.resources import ModelResource
-from aktiviteter.models import Aktivitet
+
+from aktiviteter.models import Aktivitet, AktivitetDate
 
 class AktivitetResource(ModelResource):
     class Meta:
         queryset = Aktivitet.objects.all()
         resource_name = 'aktivitet'
+
+class AktivitetDateResource(ModelResource):
+    aktivitet = fields.ForeignKey(AktivitetResource, 'aktivitet')
+
+    class Meta:
+        queryset = AktivitetDate.objects.all()
+        resource_name = 'aktivitet-date'
