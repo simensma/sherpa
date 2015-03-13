800509b847ab296c19266faa2a8a4d90ac463ba4
Håvard Ranum <havard.ranum@me.com>
2015-03-13 07:18:57 +0100

    Do not try to initialize leaflet if there is no map container

--- static/js/public/aktiviteter/show/map.js
+++ static/js/public/aktiviteter/show/map.js
@@ -2,15 +2,17 @@ $(function() {
     var aktivitet = $("div.aktivitet");
     var popup_content = aktivitet.find("div.leaflet-popup-content").html();
 
-    var map = L.map('map', {
-        scrollWheelZoom: false
-    }).setView([Turistforeningen.start_point_lat, Turistforeningen.start_point_lng], 14);
+    if (!!$('#map').length) {
+        var map = L.map('map', {
+            scrollWheelZoom: false
+        }).setView([Turistforeningen.start_point_lat, Turistforeningen.start_point_lng], 14);
 
-    L.tileLayer('http://opencache.statkart.no/gatekeeper/gk/gk.open_gmaps?layers=topo2&zoom={z}&x={x}&y={y}', {
-        attribution: 'Kartverket'
-    }).addTo(map);
+        L.tileLayer('http://opencache.statkart.no/gatekeeper/gk/gk.open_gmaps?layers=topo2&zoom={z}&x={x}&y={y}', {
+            attribution: 'Kartverket'
+        }).addTo(map);
 
-    marker = new L.Marker(new L.LatLng(Turistforeningen.start_point_lat, Turistforeningen.start_point_lng), {
-        'title': 'Turen starter her'
-    }).bindPopup(popup_content).addTo(map);
+        marker = new L.Marker(new L.LatLng(Turistforeningen.start_point_lat, Turistforeningen.start_point_lng), {
+            'title': 'Turen starter her'
+        }).bindPopup(popup_content).addTo(map);
+    }
 });
