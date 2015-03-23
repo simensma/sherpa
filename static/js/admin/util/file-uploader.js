264f46c58c5ef58d4d0b0665624347ae053c0887
Håvard Ranum <havard.ranum@me.com>
2015-03-23 16:56:09 +0100

    Use class disabled to disable button, as anchors does not support disabled attr

--- static/js/admin/util/file-uploader.js
+++ static/js/admin/util/file-uploader.js
@@ -36,7 +36,7 @@
             dataType: 'json',
             always: function (e, data) {
                 section_uploading_file.hide();
-                save_button.attr('disabled', false);
+                save_button.removeClass('disabled');
             },
             done: function (e, data) {
                 // Get file URL from arguments returned from server
@@ -69,7 +69,7 @@
         existing_msg.hide();
         success_msg.hide();
         error_msg.hide();
-        save_button.attr('disabled', true);
+        save_button.addClass('disabled');
 
         if (opts.existing_url) {
             file_uploader.find('[data-dnt-placeholder="existing-text"]').text(opts.existing_text);
