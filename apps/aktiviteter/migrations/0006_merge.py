71343444b951679ba1e490af97f78d89a44f7690
Ali Kaafarani <ali@kvikshaug.no>
2015-03-24 15:06:25 +0100

    Add merge migration

---
+++ apps/aktiviteter/migrations/0006_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('aktiviteter', '0005_participantgroup'),
+        ('aktiviteter', '0005_auto_20150318_1019'),
+    ]
+
+    operations = [
+    ]
