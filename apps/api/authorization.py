06ae04c42f8680990d9e49341e3899e423138312
Ali Kaafarani <ali@kvikshaug.no>
2015-03-25 11:43:52 +0100

    Never authorize create/update/delete on participant users

--- apps/api/authorization.py
+++ apps/api/authorization.py
@@ -1,4 +1,5 @@
 from tastypie.authorization import Authorization
+from tastypie.exceptions import Unauthorized
 
 class AuthedUserAuthorization(Authorization):
     """A custom tastypie authorization to return only objects for the logged in user. Loosely based on
@@ -16,6 +17,24 @@ class ParticipantAuthorization(Authorization):
 
     def read_detail(self, object_list, bundle):
         return bundle.obj.aktivitet_groups.select_related().filter(participants=bundle.request.user).exists()
+
+    def create_list(self, object_list, bundle):
+        raise Unauthorized("Sorry, no creates.")
+
+    def create_detail(self, object_list, bundle):
+        raise Unauthorized("Sorry, no creates.")
+
+    def update_list(self, object_list, bundle):
+        raise Unauthorized("Sorry, no updates.")
+
+    def update_detail(self, object_list, bundle):
+        raise Unauthorized("Sorry, no updates.")
+
+    def delete_list(self, object_list, bundle):
+        raise Unauthorized("Sorry, no deletes.")
+
+    def delete_detail(self, object_list, bundle):
+        raise Unauthorized("Sorry, no deletes.")
 
 class ParticipantGroupAuthorization(Authorization):
     """A custom tastypie authorization which gives access to ParticipantGroup objects the user is currently a
