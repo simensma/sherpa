49d9f443a78649f0cd63ebd327d2cc7f07374800
Ali Kaafarani <ali@kvikshaug.no>
2015-04-15 11:27:48 +0200

    Separate transaction from product logic

--- apps/payment/views.py
+++ apps/payment/views.py
@@ -5,68 +5,21 @@ import hashlib
 
 from django.conf import settings
 from django.core.urlresolvers import reverse
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseBadRequest
 from django.views.decorators.csrf import csrf_exempt
 
-from focus.models import Actor, Enrollment
-from foreninger.models import Forening, Supply
-from turbasen.models import Sted
-from user.models import User, CabinVisit, CabinVisitor
+from .products import cabin_visit
 
 @csrf_exempt
 def create_transaction(request):
     """This view is called by the phone app to initiate a new transaction"""
     transaction = json.loads(request.POST['data'])
+    product = transaction.get('product', '')
 
-    sted = Sted.get(transaction['hytte'])
-    # TODO: Handle cabins with multiple owners (defaulting to first occurrence for now)
-    forening = Forening.objects.get(turbase_object_id=sted.grupper[0])
-
-    cabin_visit = CabinVisit(
-        order_number=CabinVisit.generate_order_number(),
-    )
-    cabin_visit.save()
-
-    amount = 0
-    for gjest in transaction['gjester']:
-        cabin_visitor = CabinVisitor(
-            cabin_visit=cabin_visit,
-            protocol_number=gjest['protokollnummer'],
-        )
-        cabin_visitor.save()
-
-        if gjest['medlemsnummer'] is None:
-            is_member = False
-        else:
-            try:
-                user = User.get_or_create_inactive(memberid=gjest['medlemsnummer'], include_pending=True)
-                if not user.has_paid():
-                    # TODO: confirm in the client that one or more users aren't valid
-                    raise NotImplementedError
-                is_member = True
-
-                cabin_visitor.user = user
-                cabin_visitor.save()
-            except (Actor.DoesNotExist, Enrollment.DoesNotExist):
-                # TODO: handle invalid memberid
-                raise NotImplementedError
-
-        for losji in gjest['losji']:
-            lodging = forening.lodging_prices.get(id=losji['id'])
-            if is_member:
-                amount += lodging.price_member * losji['antall']
-            else:
-                amount += lodging.price_nonmember * losji['antall']
-
-        for proviant in gjest["proviant"]:
-            supply = Supply.objects.get(
-                id=proviant['id'],
-                supply_category__forening=forening.id,
-            )
-            if is_member:
-                amount += supply.price_member * proviant['antall']
-            else:
-                amount += supply.price_nonmember * proviant['antall']
+    if product == 'cabin_visit':
+        amount, order_number = cabin_visit(transaction)
+    else:
+        return HttpResponseBadRequest("Unknown product '%s'" % product)
 
     # The price to DIBS is provided in øre
     amount *= 100
@@ -84,7 +37,7 @@ def create_transaction(request):
         u'callbackUrl': callback_url,
         u'currency': u'NOK', # ISO 4217
         u'merchant': settings.DIBS_MERCHANT_ID,
-        u'orderId': cabin_visit.order_number,
+        u'orderId': order_number,
         u'test': 1, # Temporary; triggers test-environment at DIBS
     }
 
@@ -98,7 +51,7 @@ def create_transaction(request):
         'MAC': MAC,
         'ticket': None, # TODO: if authenticated, return user's DIBS ticket
         'callbackUrl': callback_url,
-        'orderId': cabin_visit.order_number,
+        'orderId': order_number,
         'acceptReturnUrl': accept_return_url,
         'amount': amount,
     }))
