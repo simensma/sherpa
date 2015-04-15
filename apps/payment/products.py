0ccff317e4b05dd7b577bed49be07d75e5c895cd
Ali Kaafarani <ali@kvikshaug.no>
2015-04-15 15:16:38 +0200

    Refactor cabin visit product POST structure

--- apps/payment/products.py
+++ apps/payment/products.py
@@ -2,57 +2,59 @@
 from focus.models import Actor, Enrollment
 from foreninger.models import Forening, Supply
 from turbasen.models import Sted
-from user.models import User, CabinVisit, CabinVisitor
+from user.models import User, CabinSettlement, CabinVisit, CabinVisitor
 
 def cabin_visit(product_data):
-    sted = Sted.get(product_data['hytte'])
-    # TODO: Handle cabins with multiple owners (defaulting to first occurrence for now)
-    forening = Forening.objects.get(turbase_object_id=sted.grupper[0])
-
-    cabin_visit = CabinVisit(
-        order_number=CabinVisit.generate_order_number(),
+    # Okay, initiate a settlement
+    cabin_settlement = CabinSettlement(
+        order_number=CabinSettlement.generate_order_number(),
     )
-    cabin_visit.save()
+    cabin_settlement.save()
+
+    # @TODO: OAuth
 
     amount = 0
-    for gjest in product_data['gjester']:
-        cabin_visitor = CabinVisitor(
-            cabin_visit=cabin_visit,
-            protocol_number=gjest['protokollnummer'],
+    for cabin in product_data['hytter']:
+        sted = Sted.get(cabin["id"])
+        # @TODO: Handle cabins with multiple owners (defaulting to first occurrence for now)
+        forening = Forening.objects.get(turbase_object_id=sted.grupper[0])
+
+        cabin_visit = CabinVisit(
+            cabin_settlement=cabin_settlement,
+            cabin_object_id=sted.object_id,
         )
-        cabin_visitor.save()
+        cabin_visit.save()
 
-        if gjest['medlemsnummer'] is None:
-            is_member = False
-        else:
-            try:
-                user = User.get_or_create_inactive(memberid=gjest['medlemsnummer'], include_pending=True)
-                if not user.has_paid():
-                    # TODO: confirm in the client that one or more users aren't valid
+        for losji in cabin['losji']:
+            cabin_visitor = CabinVisitor(
+                cabin_visit=cabin_visit,
+                protocol_number=losji['protokollnummer'],
+            )
+            cabin_visitor.save()
+
+            if losji['medlemsnummer'] is not None:
+                try:
+                    user = User.get_or_create_inactive(memberid=losji['medlemsnummer'], include_pending=True)
+                    if not user.has_paid():
+                        # @TODO: confirm in the client that one or more users aren't valid
+                        raise NotImplementedError
+
+                    cabin_visitor.user = user
+                    cabin_visitor.save()
+                except (Actor.DoesNotExist, Enrollment.DoesNotExist):
+                    # @TODO: handle invalid memberid
                     raise NotImplementedError
-                is_member = True
 
-                cabin_visitor.user = user
-                cabin_visitor.save()
-            except (Actor.DoesNotExist, Enrollment.DoesNotExist):
-                # TODO: handle invalid memberid
-                raise NotImplementedError
-
-        for losji in gjest['losji']:
-            lodging = forening.lodging_prices.get(id=losji['id'])
-            if is_member:
-                amount += lodging.price_member * losji['antall']
-            else:
-                amount += lodging.price_nonmember * losji['antall']
-
-        for proviant in gjest["proviant"]:
+        for proviant in cabin['proviant']:
             supply = Supply.objects.get(
                 id=proviant['id'],
                 supply_category__forening=forening.id,
             )
-            if is_member:
+
+            # Business rule: The supply price is determined by whether or not the paying user is considered a member
+            if product_data['medlemspris']:
                 amount += supply.price_member * proviant['antall']
             else:
                 amount += supply.price_nonmember * proviant['antall']
 
-    return (amount, cabin_visit.order_number)
+    return (amount, cabin_settlement.order_number)
